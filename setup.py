cb7e72ad2f489c5007e430004a9179115ad508c5
Martin Siggel <martin.siggel@dlr.de>
2018-05-17 14:07:05 +0200

    Initial directory structure and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'heat',
+  packages = ['heat'],
+  version = '0.0.0',
+  description = 'A framework for high performace data analytics and machine learning.',
+  author = 'Helmholtz Association',
+  author_email = 'martin.siggel@dlr.de',
+#  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+#  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['data', 'analytics', 'tensors', 'distributed', 'gpu'], # arbitrary keywords
+  classifiers = [],
+)
