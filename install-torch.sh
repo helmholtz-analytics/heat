1ee2d031bbb6b29506131770fdb91879693f4718
Bjoern Hagemeier <b.hagemeier@fz-juelich.de>
2018-06-21 11:52:04 +0200

    Flip logic to compile if necessary
    PyTorch should be compile when it is NOT there. Additionally, output an explicit
    message regarding the outcome of the check.

--- install-torch.sh
+++ install-torch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [[ $(pip freeze | grep torch) ]]; then
+if [[ ! $(pip freeze | grep torch) ]]; then
+    echo "Installing PyTorch from source"
     git clone https://github.com/pytorch/pytorch.git
     cd pytorch
     git submodule -q update --init
     pip install pyyaml -q
     travis_wait 32 pip install . -q
     cd -
+else
+    echo "PyTorch already installed"
 fi
