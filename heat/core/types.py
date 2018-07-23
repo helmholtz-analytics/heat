9c4f7642617d284a252b2b780783fa1bf90fbd3a
Markus Goetz <markus.goetz@kit.edu>
2018-07-23 13:51:27 +0200

    Added type hierarchy skeletton.

--- heat/core/types.py
+++ heat/core/types.py
@@ -3,22 +3,120 @@ types: Defines the basic heat data types in the hierarchy as shown below. Design
 
 As part of the type-hierarchy: xx -- is bit-width
 generic
- +-> bool_                                  (kind=b)
+ +-> bool                                   (kind=b)
  +-> number
  |   +-> integer
  |   |   +-> signedinteger     (intxx)      (kind=i)
  |   |   |     byte
  |   |   |     short
- |   |   |     int_
+ |   |   |     int
+ |   |   |     long
  |   |   |     longlong
  |   |   \\-> unsignedinteger  (uintxx)     (kind=u)
  |   |         ubyte
  |   |         ushort
- |   |         uint_
+ |   |         uint
+ |   |         ulong
  |   |         ulonglong
  |   +-> floating              (floatxx)    (kind=f)
  |   |     half
- |   |     float_
+ |   |     float
  |   |     double              (double)
  \\-> flexible (currently unused, placeholder for characters)
 """
+
+import abc
+
+# maintain references to Python's builtin types
+_bool = bool
+_float = float
+_int = int
+
+
+class generic(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def __new__(cls, *args, **kwargs):
+        pass
+
+
+class bool(generic):
+    pass
+bool_ = bool
+
+
+class number(generic):
+    pass
+
+
+class integer(number):
+    pass
+
+
+class signedinteger(integer):
+    pass
+
+
+class byte(signedinteger):
+    pass
+
+
+class short(signedinteger):
+    pass
+
+
+class int(signedinteger):
+    pass
+int_ = int
+
+
+class long(signedinteger):
+    pass
+
+
+class longlong(signedinteger):
+    pass
+
+
+class unsignedinteger(integer):
+    pass
+
+
+class ubyte(unsignedinteger):
+    pass
+
+
+class ushort(unsignedinteger):
+    pass
+
+
+class uint(unsignedinteger):
+    pass
+
+
+class ulong(unsignedinteger):
+    pass
+
+
+class ulonglong(unsignedinteger):
+    pass
+
+
+class floating(number):
+    pass
+
+
+class half(floating):
+    pass
+
+
+class float(floating):
+    pass
+float_ = float
+
+
+class double(floating):
+    pass
+
+
+class flexible(generic):
+    pass
