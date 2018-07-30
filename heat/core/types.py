a6100c30b8996c21bc8a0da67ac67b16ea273faa
Markus Goetz <markus.goetz@kit.edu>
2018-07-30 11:18:39 +0200

    First draft for HeAT's type system
    * Implements subset of numpy's type system (see numpy/core/numerictypes.py) that is also supported by PyTorch's type system
    * List of supported types
        - bool
        - int8
        - int16
        - int32
        - int64
        - uint8
        - float16
        - float32
        - float64
    * Currently untested
    * Contribution to issue #12

--- heat/core/types.py
+++ heat/core/types.py
@@ -7,43 +7,52 @@ generic
  +-> number
  |   +-> integer
  |   |   +-> signedinteger     (intxx)      (kind=i)
- |   |   |     byte
- |   |   |     short
- |   |   |     int
- |   |   |     long
- |   |   |     longlong
+ |   |   |     int8, byte
+ |   |   |     int16, short
+ |   |   |     int32, int
+ |   |   |     int64, long
  |   |   \\-> unsignedinteger  (uintxx)     (kind=u)
- |   |         ubyte
- |   |         ushort
- |   |         uint
- |   |         ulong
- |   |         ulonglong
- |   +-> floating              (floatxx)    (kind=f)
- |   |     half
- |   |     float
- |   |     double              (double)
+ |   |         uint8, ubyte
+ |   \\-> floating             (floatxx)    (kind=f)
+ |         float16, half
+ |         float32, float
+ |         float64, double     (double)
  \\-> flexible (currently unused, placeholder for characters)
 """
 
 import abc
+import builtins
+import torch
 
-# maintain references to Python's builtin types
-_bool = bool
-_float = float
-_int = int
+from .communicator import NoneCommunicator
+from .tensor import tensor
 
 
 class generic(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __new__(cls, *args, **kwargs):
+        try:
+            torch_type = cls.torch_type()
+
+            typed_tensor = tensor(Comm=NoneCommunicator)
+            typed_tensor.array = torch.zeros(1, dtype=torch_type)
+            typed_tensor._tensor__gshape = (1,)
+
+            return typed_tensor
+
+        except TypeError:
+            raise TypeError('cannot create \'{}\' instances'.format(cls))
+
+    @classmethod
+    @abc.abstractclassmethod
+    def torch_type(cls):
         pass
 
 
 class bool(generic):
-    pass
-
-
-bool_ = bool
+    @classmethod
+    def torch_type(cls):
+        return torch.uint8
 
 
 class number(generic):
@@ -59,33 +68,27 @@ class signedinteger(integer):
 
 
 class int8(signedinteger):
-    pass
-
-
-byte = int8
+    @classmethod
+    def torch_type(cls):
+        return torch.int8
 
 
 class int16(signedinteger):
-    pass
-
-
-short = int16
+    @classmethod
+    def torch_type(cls):
+        return torch.int16
 
 
 class int32(signedinteger):
-    pass
-
-
-int = int32
-int_ = int32
+    @classmethod
+    def torch_type(cls):
+        return torch.int32
 
 
 class int64(signedinteger):
-    pass
-
-
-long = int64
-longlong = int64
+    @classmethod
+    def torch_type(cls):
+        return torch.int64
 
 
 class unsignedinteger(integer):
@@ -93,32 +96,9 @@ class unsignedinteger(integer):
 
 
 class uint8(unsignedinteger):
-    pass
-
-
-ubyte = uint8
-
-
-class uint16(unsignedinteger):
-    pass
-
-
-ushort = uint16
-
-
-class uint32(unsignedinteger):
-    pass
-
-
-uint = uint32
-
-
-class uint64(unsignedinteger):
-    pass
-
-
-ulong = uint64
-ulonglong = uint64
+    @classmethod
+    def torch_type(cls):
+        return torch.uint8
 
 
 class floating(number):
@@ -126,26 +106,70 @@ class floating(number):
 
 
 class float16(floating):
-    pass
-
-
-half = float16
+    @classmethod
+    def torch_type(cls):
+        return torch.float16
 
 
 class float32(floating):
-    pass
-
-
-float = float32
-float_ = float32
+    @classmethod
+    def torch_type(cls):
+        return torch.float32
 
 
 class float64(floating):
-    pass
-
-
-double = float64
+    @classmethod
+    def torch_type(cls):
+        return torch.float64
 
 
 class flexible(generic):
     pass
+
+
+# definition of aliases
+bool_ = bool
+
+byte = int8
+short = int16
+int = int32
+int_ = int32
+long = int64
+
+ubyte = uint8
+
+half = float16
+float = float32
+float_ = float32
+double = float64
+
+# type mappings for type strings and builtins types
+__type_mappings = {
+    'b':  bool,
+    'i':  int32,
+    'i1': int8,
+    'i2': int16,
+    'i4': int32,
+    'i8': int64,
+    'u':  uint8,
+    'u1': uint8,
+    'f':  float32,
+    'f2': float16,
+    'f4': float32,
+    'f8': float64,
+    builtins.bool:  bool,
+    builtins.int:   int32,
+    builtins.float: float32,
+}
+
+# inverse type mappings for reconstructing heat from torch types
+__inverse_type_mappings = {
+    torch.int8:    int8,
+    torch.int16:   int16,
+    torch.int32:   int32,
+    torch.int64:   int64,
+    torch.uint8:   uint8,
+    torch.float16: float16,
+    torch.float32: float32,
+    torch.float64: float64,
+}
