6c5c089cc8ead6bdb7f33051890abfe34cc1c44a
Markus Goetz <markus.goetz@kit.edu>
2018-08-01 20:02:10 +0200

    * Reinstated the PyTorch multi-build guard * Reintroduced @abc.abstractclassmethod for torch_types due to failing tests

--- install-torch.sh
+++ install-torch.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#if [[ ! $(pip freeze | grep torch) ]]; then
-    pip uninstall torch
+if [[ ! $(pip freeze | grep torch) ]]; then
     echo "Installing PyTorch from source"
     git clone --branch v0.4.1 --recursive https://github.com/pytorch/pytorch.git
     cd pytorch
     pip install pyyaml typing
     pip install .
     cd -
-#else
-#    echo "PyTorch already installed"
-#fi
+else
+    echo "PyTorch already installed"
+fi
