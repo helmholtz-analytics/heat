a9f776463e58907c2c6bc8bd7f1944720c5bfa01
Markus Goetz <markus.goetz@kit.edu>
2018-08-01 18:53:46 +0200

    Trigger build

--- install-torch.sh
+++ install-torch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #if [[ ! $(pip freeze | grep torch) ]]; then
-pip uninstall -y torch
-echo "Installing PyTorch from source"
-git clone --recursive https://github.com/pytorch/pytorch.git
-cd pytorch
-pip install -q pyyaml typing
-pip install -q .
-cd -
+    pip uninstall torch
+    echo "Installing PyTorch from source"
+    git clone --recursive https://github.com/pytorch/pytorch.git
+    cd pytorch
+    pip install pyyaml typing
+    pip install .
+    cd -
 #else
 #    echo "PyTorch already installed"
 #fi
