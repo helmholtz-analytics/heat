6ff12f8609d0f00a40adb5d8a1859eb530dc2928
Claudia Comito <c.comito@fz-juelich.de>
2018-09-01 08:25:11 +0200

    Specify deployment target if on MAC OS

--- install-torch.sh
+++ install-torch.sh
@@ -5,6 +5,9 @@ if [[ ! $(pip freeze | grep torch) ]]; then
     git clone --branch v0.4.1 --recursive https://github.com/pytorch/pytorch.git
     cd pytorch
     pip install pyyaml typing
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        export MACOSX_DEPLOYMENT_TARGET=10.9
+    fi
     pip install .
     cd -
 else
