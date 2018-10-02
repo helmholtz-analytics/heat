79b0ddfc833096f1fbd46973931b794d4828cad2
Bjoern Hagemeier <b.hagemeier@fz-juelich.de>
2018-10-02 15:29:40 +0200

    Issue #56: fix project description on PyPI
    Set both description and long_description, so PyPI displays the respective
    descriptions where they are expected. Long description is a copy of README.md,
    which is quite commonly used.
    
    Closes #56

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='heat',
     packages=['heat'],
     version='0.0.1',
     description='A framework for high performance data analytics and machine learning.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Helmholtz Association',
     author_email='martin.siggel@dlr.de',
     url='https://github.com/helmholtz-analytics/heat',
-    #  download_url = 'https://github.com/helmholtz-analytics/heat/archive/0.1.tar.gz', # TBD
     keywords=['data', 'analytics', 'tensors', 'distributed', 'gpu'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering'
+    ],
     install_requires=[
         'numpy>=1.13.0',
-        # 'torch>=0.4.0'
     ],
     extras_require={
         'hdf5': ['h5py>=2.8.0']
