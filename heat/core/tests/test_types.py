db23625b16fd8dfd794020a495a7dcdc03ee90b2
Charlotte <charlotte.debus@dlr.de>
2019-03-08 16:10:27 +0100

    Adjusted Unit-tests for NaN

--- heat/core/tests/test_types.py
+++ heat/core/tests/test_types.py
@@ -195,8 +195,9 @@ class TestConstants(unittest.TestCase):
     def test_constants(self):
         self.assertTrue(float('inf') == ht.Inf)
         self.assertTrue(ht.inf == np.inf)
+        self.assertTrue(np.isnan(ht.nan))
         self.assertTrue(3<ht.inf)
-        self.assertTrue(3!=ht.NaN)
+        self.assertTrue(np.isinf(ht.inf))
         self.assertTrue(ht.pi == np.pi)
         self.assertTrue(ht.e == np.e)
 
