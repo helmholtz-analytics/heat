d1c70f686be516dcd9f29c10715317801f2be4ee
coquelin77 <d.coquelin@fz-juelich.de>
2019-03-29 08:39:12 +0100

    added cosh

--- heat/core/trigonometrics.py
+++ heat/core/trigonometrics.py
@@ -4,6 +4,7 @@ from .operations import __local_operation as local_op
 
 __all__ = [
     'cos',
+    'cosh',
     'sin',
     'tan'
 ]
@@ -16,7 +17,7 @@ def cos(x, out=None):
     Parameters
     ----------
     x : ht.tensor
-        The value for which to compute the trigonometric sine.
+        The value for which to compute the trigonometric cosine.
     out : ht.tensor or None, optional
         A location in which to store the results. If provided, it must have a broadcastable shape. If not provided
         or set to None, a fresh tensor is allocated.
@@ -33,6 +34,32 @@ def cos(x, out=None):
     tensor([0.96017029, -0.65364362, -0.41614684,  1., -0.41614684, -0.65364362,  0.96017029])
     """
     return local_op(torch.cos, x, out)
+
+
+def cosh(x, out=None):
+    """
+    Return the hyperbolic cosine, element-wise.
+
+    Parameters
+    ----------
+    x : ht.tensor
+        The value for which to compute the hyperbolic cosine.
+    out : ht.tensor or None, optional
+        A location in which to store the results. If provided, it must have a broadcastable shape. If not provided
+        or set to None, a fresh tensor is allocated.
+
+    Returns
+    -------
+    hyperbolic cosine : ht.tensor
+        A tensor of the same shape as x, containing the hyperbolic cosine of each element in this tensor.
+        Negative input elements are returned as nan. If out was provided, square_roots is a reference to it.
+
+    Examples
+    --------
+    >>> ht.cosh(ht.arange(-6, 7, 2))
+    tensor([201.7156,  27.3082,   3.7622,   1.0000,   3.7622,  27.3082, 201.7156])
+    """
+    return local_op(torch.cosh, x, out)
 
 
 def sin(x, out=None):
