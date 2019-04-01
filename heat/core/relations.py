a2672344b7ba58febbd823c8bd2cc65eb2a9934f
simon <therealslimv@yahoo.de>
2019-04-01 15:12:07 +0200

    fixed indents in relations file

--- heat/core/relations.py
+++ heat/core/relations.py
@@ -19,7 +19,8 @@ __all__ = [
     'ne'
 ]
 
-def eq(t1,t2):
+
+def eq(t1, t2):
     """
          Element-wise rich comparison of equality between values from two operands, commutative.
          Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
@@ -56,7 +57,7 @@ def eq(t1,t2):
     return binary_op(torch.eq, t1, t2)
 
 
-def equal(t1,t2):
+def equal(t1, t2):
     """
          Overall comparison of equality between two tensors. Returns True if two tensors have the same size and elements, and False otherwise.
 
@@ -134,44 +135,42 @@ def equal(t1,t2):
 
 def ge(t1, t2):
     """
-         Element-wise rich greater than or equal comparison between values from operand t1 with respect to values of
-         operand t2 (i.e. t1 >= t2), not commutative.
-         Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
+        Element-wise rich greater than or equal comparison between values from operand t1 with respect to values of
+        operand t2 (i.e. t1 >= t2), not commutative.
+        Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
 
-         Parameters
-         ----------
-         t1: tensor or scalar
-         The first operand to be compared greater than or equal to second operand
+        Parameters
+        ----------
+        t1: tensor or scalar
+            The first operand to be compared greater than or equal to second operand
 
-         t2: tensor or scalar
-         The second operand to be compared less than or equal to first operand
+        t2: tensor or scalar
+           The second operand to be compared less than or equal to first operand
 
-         Returns
-         -------
-         result: ht.tensor
-          A uint8-tensor holding 1 for all elements in which values of t1 are greater than or equal tp values of t2,
-         0 for all other elements
+        Returns
+        -------
+        result: ht.tensor
+            A uint8-tensor holding 1 for all elements in which values of t1 are greater than or equal tp values of t2,
+            0 for all other elements
 
-         Examples
-         -------
-         >>> import heat as ht
-         >>> T1 = ht.float32([[1, 2],[3, 4]])
-         >>> ht.ge(T1, 3.0)
-         tensor([[0, 0],
-                 [1, 1]], dtype=torch.uint8)
+        Examples
+        -------
+        >>> import heat as ht
+        >>> T1 = ht.float32([[1, 2],[3, 4]])
+        >>> ht.ge(T1, 3.0)
+        tensor([[0, 0],
+                [1, 1]], dtype=torch.uint8)
 
-         >>> T2 = ht.float32([[2, 2], [2, 2]])
-         >>> ht.ge(T1, T2)
-         tensor([[0, 1],
-                 [1, 1]], dtype=torch.uint8)
-
-
+        >>> T2 = ht.float32([[2, 2], [2, 2]])
+        >>> ht.ge(T1, T2)
+        tensor([[0, 1],
+                [1, 1]], dtype=torch.uint8)
     """
 
     return binary_op(torch.ge, t1, t2)
 
 
-def gt(t1,t2):
+def gt(t1, t2):
     """
          Element-wise rich greater than comparison between values from operand t1 with respect to values of
          operand t2 (i.e. t1 > t2), not commutative.
@@ -180,16 +179,16 @@ def gt(t1,t2):
          Parameters
          ----------
          t1: tensor or scalar
-         The first operand to be compared greater than second operand
+            The first operand to be compared greater than second operand
 
          t2: tensor or scalar
-         The second operand to be compared less than first operand
+            The second operand to be compared less than first operand
 
          Returns
          -------
          result: ht.tensor
-         A uint8-tensor holding 1 for all elements in which values of t1 are greater than values of t2,
-         0 for all other elements
+            A uint8-tensor holding 1 for all elements in which values of t1 are greater than values of t2,
+            0 for all other elements
 
          Examples
          -------
@@ -209,7 +208,7 @@ def gt(t1,t2):
     return binary_op(torch.gt, t1, t2)
 
 
-def le(t1,t2):
+def le(t1, t2):
     """
          Element-wise rich less than or equal comparison between values from operand t1 with respect to values of
          operand t2 (i.e. t1 <= t2), not commutative.
@@ -218,16 +217,16 @@ def le(t1,t2):
          Parameters
          ----------
          t1: tensor or scalar
-         The first operand to be compared less than or equal to second operand
+            The first operand to be compared less than or equal to second operand
 
          t2: tensor or scalar
-         The second operand to be compared greater than or equal to first operand
+            The second operand to be compared greater than or equal to first operand
 
          Returns
          -------
          result: ht.tensor
-         A uint8-tensor holding 1 for all elements in which values of t1 are less than or equal to values of t2,
-         0 for all other elements
+            A uint8-tensor holding 1 for all elements in which values of t1 are less than or equal to values of t2,
+            0 for all other elements
 
          Examples
          -------
@@ -246,25 +245,25 @@ def le(t1,t2):
     return binary_op(torch.le, t1, t2)
 
 
-def lt(t1,t2):
+def lt(t1, t2):
     """
-         Element-wise rich less than comparison between values from operand t1 with respect to values of
-         operand t2 (i.e. t1 < t2), not commutative.
-         Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
+        Element-wise rich less than comparison between values from operand t1 with respect to values of
+        operand t2 (i.e. t1 < t2), not commutative.
+        Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
 
-         Parameters
-         ----------
-         t1: tensor or scalar
-         The first operand to be compared less than second operand
+        Parameters
+        ----------
+        t1: tensor or scalar
+            The first operand to be compared less than second operand
 
-         t2: tensor or scalar
-         The second operand to be compared greater than first operand
+        t2: tensor or scalar
+            The second operand to be compared greater than first operand
 
-         Returns
-         -------
-         result: ht.tensor
-          A uint8-tensor holding 1 for all elements in which values of t1 are less than values of t2,
-         0 for all other elements
+        Returns
+        -------
+        result: ht.tensor
+            A uint8-tensor holding 1 for all elements in which values of t1 are less than values of t2,
+            0 for all other elements
 
         Examples
         -------
@@ -282,38 +281,39 @@ def lt(t1,t2):
 
     return binary_op(torch.lt, t1, t2)
 
+
 def max(x, axis=None, out=None):
     # TODO: initial : scalar, optional Issue #101
     """
-    Return the maximum along a given axis.
+        Return the maximum along a given axis.
 
-    Parameters
-    ----------
-    a : ht.tensor
-        Input data.
-    axis : None or int, optional
-        Axis or axes along which to operate. By default, flattened input is used.
-    out : ht.tensor, optional
-        Tuple of two output tensors (max, max_indices). Must be of the same shape and buffer length as the expected
-        output. The minimum value of an output element. Must be present to allow computation on empty slice.
+        Parameters
+        ----------
+        a : ht.tensor
+            Input data.
+        axis : None or int, optional
+            Axis or axes along which to operate. By default, flattened input is used.
+        out : ht.tensor, optional
+            Tuple of two output tensors (max, max_indices). Must be of the same shape and buffer length as the expected
+            output. The minimum value of an output element. Must be present to allow computation on empty slice.
 
-    Examples
-    --------
-    >>> a = ht.float32([
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [10, 11, 12]
-        ])
-    >>> ht.max(a)
-    tensor([12.])
-    >>> ht.min(a, axis=0)
-    tensor([[10., 11., 12.]])
-    >>> ht.min(a, axis=1)
-    tensor([[ 3.],
-        [ 6.],
-        [ 9.],
-        [12.]])
+        Examples
+        --------
+        >>> a = ht.float32([
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+                [10, 11, 12]
+            ])
+        >>> ht.max(a)
+        tensor([12.])
+        >>> ht.min(a, axis=0)
+        tensor([[10., 11., 12.]])
+        >>> ht.min(a, axis=1)
+        tensor([[ 3.],
+            [ 6.],
+            [ 9.],
+            [12.]])
     """
     def local_max(*args, **kwargs):
         result = torch.max(*args, **kwargs)
@@ -327,35 +327,35 @@ def max(x, axis=None, out=None):
 def min(x, axis=None, out=None):
     # TODO: initial : scalar, optional Issue #101
     """
-    Return the minimum along a given axis.
+        Return the minimum along a given axis.
 
-    Parameters
-    ----------
-    a : ht.tensor
-        Input data.
-    axis : None or int
-        Axis or axes along which to operate. By default, flattened input is used.
-    out : ht.tensor, optional
-        Tuple of two output tensors (min, min_indices). Must be of the same shape and buffer length as the expected
-        output.The maximum value of an output element. Must be present to allow computation on empty slice.
+        Parameters
+        ----------
+        a : ht.tensor
+            Input data.
+        axis : None or int
+            Axis or axes along which to operate. By default, flattened input is used.
+        out : ht.tensor, optional
+            Tuple of two output tensors (min, min_indices). Must be of the same shape and buffer length as the expected
+            output.The maximum value of an output element. Must be present to allow computation on empty slice.
 
-    Examples
-    --------
-    >>> a = ht.float32([
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [10, 11, 12]
-        ])
-    >>> ht.min(a)
-    tensor([1.])
-    >>> ht.min(a, axis=0)
-    tensor([[1., 2., 3.]])
-    >>> ht.min(a, axis=1)
-    tensor([[ 1.],
-        [ 4.],
-        [ 7.],
-        [10.]])
+        Examples
+        --------
+        >>> a = ht.float32([
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+                [10, 11, 12]
+            ])
+        >>> ht.min(a)
+        tensor([1.])
+        >>> ht.min(a, axis=0)
+        tensor([[1., 2., 3.]])
+        >>> ht.min(a, axis=1)
+        tensor([[ 1.],
+            [ 4.],
+            [ 7.],
+            [10.]])
     """
     def local_min(*args, **kwargs):
         result = torch.min(*args, **kwargs)
@@ -366,40 +366,39 @@ def min(x, axis=None, out=None):
     return reduce_op(x, local_min, MPI.MIN, axis, out)
 
 
-
-def ne(t1,t2):
+def ne(t1, t2):
     """
-         Element-wise rich comparison of non-equality between values from two operands, commutative.
-         Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
+        Element-wise rich comparison of non-equality between values from two operands, commutative.
+        Takes the first and second operand (scalar or tensor) whose elements are to be compared as argument.
 
-         Parameters
-         ----------
-         t1: tensor or scalar
-         The first operand involved in the comparison
+        Parameters
+        ----------
+        t1: tensor or scalar
+        The first operand involved in the comparison
 
-         t2: tensor or scalar
-         The second operand involved in the comparison
+        t2: tensor or scalar
+            The second operand involved in the comparison
 
-         Returns
-         -------
-         result: ht.tensor
-         A uint8-tensor holding 1 for all elements in which values of t1 are not equal to values of t2,
-         0 for all other elements
+        Returns
+        -------
+        result: ht.tensor
+            A uint8-tensor holding 1 for all elements in which values of t1 are not equal to values of t2,
+            0 for all other elements
 
-         Examples:
-         ---------
-         >>> import heat as ht
-         >>> T1 = ht.float32([[1, 2],[3, 4]])
-         >>> ht.ne(T1, 3.0)
-         tensor([[1, 1],
-                 [0, 1]])
+        Examples:
+        ---------
+        >>> import heat as ht
+        >>> T1 = ht.float32([[1, 2],[3, 4]])
+        >>> ht.ne(T1, 3.0)
+        tensor([[1, 1],
+                [0, 1]])
 
-         >>> T2 = ht.float32([[2, 2], [2, 2]])
-         >>> ht.ne(T1, T2)
-         tensor([[1, 0],
-                 [1, 1]])
+        >>> T2 = ht.float32([[2, 2], [2, 2]])
+        >>> ht.ne(T1, T2)
+        tensor([[1, 0],
+                [1, 1]])
 
-         """
+        """
 
     return binary_op(torch.ne, t1, t2)
 
