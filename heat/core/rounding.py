37daa0848fc0e43c8cacab874881dd068065d895
simon <therealslimv@yahoo.de>
2019-04-01 17:32:02 +0200

    fixed some wrong indents that were forgotten earlier

--- heat/core/rounding.py
+++ heat/core/rounding.py
@@ -70,7 +70,7 @@ def absolute(x, out=None, dtype=None):
 
 
 def ceil(x, out=None):
-    r"""
+    """
     Return the ceil of the input, element-wise.
 
     The ceil of the scalar x is the largest integer i, such that i <= x. It is often denoted as \lceil x \rceil.
@@ -98,7 +98,7 @@ def ceil(x, out=None):
 
 
 def floor(x, out=None):
-    r"""
+    """
     Return the floor of the input, element-wise.
 
     The floor of the scalar x is the largest integer i, such that i <= x. It is often denoted as \lfloor x \rfloor.
