a1f2eae2f37426cedcf472fb6e92e82901055598
simon <therealslimv@yahoo.de>
2019-04-01 15:32:19 +0200

    added more indent fixes in the arithmetics file

--- heat/core/arithmetics.py
+++ heat/core/arithmetics.py
@@ -95,41 +95,41 @@ def div(t1, t2):
 
 def mul(t1,t2):
     """
-      Element-wise multiplication (NOT matrix multiplication) of values from two operands, commutative.
-      Takes the first and second operand (scalar or tensor) whose elements are to be multiplied as argument.
+        Element-wise multiplication (NOT matrix multiplication) of values from two operands, commutative.
+        Takes the first and second operand (scalar or tensor) whose elements are to be multiplied as argument.
 
-      Parameters
-      ----------
-      t1: tensor or scalar
-        The first operand involved in the multiplication
+        Parameters
+        ----------
+        t1: tensor or scalar
+            The first operand involved in the multiplication
 
-      t2: tensor or scalar
-        The second operand involved in the multiplication
+        t2: tensor or scalar
+            The second operand involved in the multiplication
 
 
-      Returns
-      -------
-      result: ht.tensor
-        A tensor containing the results of element-wise multiplication of t1 and t2.
+        Returns
+        -------
+        result: ht.tensor
+            A tensor containing the results of element-wise multiplication of t1 and t2.
 
-     Examples:
-     ---------
-     >>> import heat as ht
-     >>> ht.mul ( 2.0, 4.0)
-     tensor([8.])
+        Examples:
+        ---------
+        >>> import heat as ht
+        >>> ht.mul ( 2.0, 4.0)
+        tensor([8.])
 
-     >>> T1 = ht.float32([[1, 2], [3, 4]])
-     >>> s = 3.0
-     >>> ht.mul(T1, s)
-     tensor([[3., 6.],
-             [9., 12.]])
+        >>> T1 = ht.float32([[1, 2], [3, 4]])
+        >>> s = 3.0
+        >>> ht.mul(T1, s)
+        tensor([[3., 6.],
+                [9., 12.]])
 
-     >>> T2 = ht.float32([[2, 2], [2, 2]])
-     >>> ht.mul(T1, T2)
-     tensor([[2., 4.],
-             [6., 8.]])
+        >>> T2 = ht.float32([[2, 2], [2, 2]])
+        >>> ht.mul(T1, T2)
+        tensor([[2., 4.],
+                [6., 8.]])
 
-    """
+        """
 
     return binary_op(torch.mul, t1, t2)
 
@@ -177,40 +177,39 @@ def pow(t1,t2):
 
 def sub(t1, t2):
     """
-      Element-wise subtraction of values of operand t2 from values of operands t1 (i.e t1 - t2), not commutative.
-      Takes the two operands (scalar or tensor) whose elements are to be subtracted (operand 2 from operand 1)
-      as argument.
+        Element-wise subtraction of values of operand t2 from values of operands t1 (i.e t1 - t2), not commutative.
+        Takes the two operands (scalar or tensor) whose elements are to be subtracted (operand 2 from operand 1)
+        as argument.
 
-      Parameters
-      ----------
-      t1: tensor or scalar
-        The first operand from which values are subtracted
+        Parameters
+        ----------
+        t1: tensor or scalar
+            The first operand from which values are subtracted
 
-      t2: tensor or scalar
-        The second operand whose values are subtracted
+        t2: tensor or scalar
+            The second operand whose values are subtracted
 
-      Returns
-      -------
-      result: ht.tensor
-        A tensor containing the results of element-wise subtraction of t1 and t2.
+        Returns
+        -------
+        result: ht.tensor
+            A tensor containing the results of element-wise subtraction of t1 and t2.
 
-      Examples:
-      ---------
-      >>> import heat as ht
-      >>> ht.sub ( 4.0, 1.0)
-      tensor([3.])
+        Examples:
+        ---------
+        >>> import heat as ht
+        >>> ht.sub ( 4.0, 1.0)
+        tensor([3.])
 
-      >>> T1 = ht.float32([[1, 2], [3, 4]])
-      >>> T2 = ht.float32([[2, 2], [2, 2]])
-      >>> ht.sub(T1, T2)
-      tensor([[-1., 0.],
-              [1., 2.]])
+        >>> T1 = ht.float32([[1, 2], [3, 4]])
+        >>> T2 = ht.float32([[2, 2], [2, 2]])
+        >>> ht.sub(T1, T2)
+        tensor([[-1., 0.],
+                [1., 2.]])
 
-      >>> s = 2.0
-      >>> ht.sub(s, T1)
-      tensor([[ 1.,  0.],
-              [-1., -2.]])
-
-      """
+        >>> s = 2.0
+        >>> ht.sub(s, T1)
+        tensor([[ 1.,  0.],
+                [-1., -2.]])
+        """
 
     return binary_op(torch.sub, t1, t2)
