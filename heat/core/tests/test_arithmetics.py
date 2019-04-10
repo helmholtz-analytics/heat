dd2f8583a56c30d52161341a562a33cfc5441844
Markus Goetz <markus.goetz@kit.edu>
2019-04-10 19:41:13 +0200

    Refactored arithmetics tests so that they are using class fixtures

--- heat/core/tests/test_arithmetics.py
+++ heat/core/tests/test_arithmetics.py
@@ -3,205 +3,217 @@ import unittest
 
 import heat as ht
 
-T = ht.float32([
-    [1, 2],
-    [3, 4]
-])
-s = 2.0
-s_int = 2
-T1 = ht.float32([
-    [2, 2],
-    [2, 2]
-])
-v = ht.float32([2, 2])
-v2 = ht.float32([2, 2, 2])
-T_s = ht.Tensor(T1._Tensor__array, T1.shape, T1.dtype, 0, T1.device, T1.comm)
-otherType = (2, 2)
-
 
 class TestArithmetics(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.a_scalar = 2.0
+        cls.an_int_scalar = 2
+
+        cls.a_vector = ht.float32([2, 2])
+        cls.another_vector = ht.float32([2, 2, 2])
+
+        cls.a_tensor = ht.array([
+            [1.0, 2.0],
+            [3.0, 4.0]
+        ])
+        cls.another_tensor = ht.array([
+            [2.0, 2.0],
+            [2.0, 2.0]
+        ])
+        cls.a_split_tensor = cls.another_tensor.copy().resplit(0)
+
+        cls.errorneous_type = (2, 2)
+
     def test_add(self):
-        T_r = ht.float32([
-            [3, 4],
-            [5, 6]
+        result = ht.array([
+            [3.0, 4.0],
+            [5.0, 6.0]
         ])
         
-        self.assertTrue(ht.equal(ht.add(s, s), ht.float32([4.0])))
-        self.assertTrue(ht.equal(ht.add(T, s), T_r))
-        self.assertTrue(ht.equal(ht.add(s, T), T_r))
-        self.assertTrue(ht.equal(ht.add(T, T1), T_r))
-        self.assertTrue(ht.equal(ht.add(T, v), T_r))
-        self.assertTrue(ht.equal(ht.add(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.add(T_s, T), T_r))
+        self.assertTrue(ht.equal(ht.add(self.a_scalar, self.a_scalar), ht.float32([4.0])))
+        self.assertTrue(ht.equal(ht.add(self.a_tensor, self.a_scalar), result))
+        self.assertTrue(ht.equal(ht.add(self.a_scalar, self.a_tensor), result))
+        self.assertTrue(ht.equal(ht.add(self.a_tensor, self.another_tensor), result))
+        self.assertTrue(ht.equal(ht.add(self.a_tensor, self.a_vector), result))
+        self.assertTrue(ht.equal(ht.add(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.add(self.a_split_tensor, self.a_tensor), result))
 
         with self.assertRaises(ValueError):
-            ht.add(T, v2)
+            ht.add(self.a_tensor, self.another_vector)
         with self.assertRaises(NotImplementedError):
-            ht.add(T, T_s)
+            ht.add(self.a_tensor, self.a_split_tensor)
         with self.assertRaises(TypeError):
-            ht.add(T, otherType)
+            ht.add(self.a_tensor, self.errorneous_type)
         with self.assertRaises(TypeError):
             ht.add('T', 's')
 
     def test_div(self):
-        T_r = ht.float32([
-            [0.5, 1],
-            [1.5, 2]
+        result = ht.array([
+            [0.5, 1.0],
+            [1.5, 2.0]
+        ])
+        commutated_result = ht.array([
+            [2.0,     1.0],
+            [2.0/3.0, 0.5]
         ])
 
-        T_inv = ht.float32([
-            [2, 1],
-            [2/3, 0.5]
-        ])
-
-        self.assertTrue(ht.equal(ht.div(s, s), ht.float32([1.0])))
-        self.assertTrue(ht.equal(ht.div(T, s),T_r))
-        self.assertTrue(ht.equal(ht.div(s, T), T_inv))
-        self.assertTrue(ht.equal(ht.div(T, T1), T_r))
-        self.assertTrue(ht.equal(ht.div(T, v), T_r))
-        self.assertTrue(ht.equal(ht.div(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.div(T_s, T), T_inv))
+        self.assertTrue(ht.equal(ht.div(self.a_scalar, self.a_scalar), ht.float32([1.0])))
+        self.assertTrue(ht.equal(ht.div(self.a_tensor, self.a_scalar), result))
+        self.assertTrue(ht.equal(ht.div(self.a_scalar, self.a_tensor), commutated_result))
+        self.assertTrue(ht.equal(ht.div(self.a_tensor, self.another_tensor), result))
+        self.assertTrue(ht.equal(ht.div(self.a_tensor, self.a_vector), result))
+        self.assertTrue(ht.equal(ht.div(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.div(self.a_split_tensor, self.a_tensor), commutated_result))
 
         with self.assertRaises(ValueError):
-            ht.div(T, v2)
+            ht.div(self.a_tensor, self.another_vector)
         with self.assertRaises(NotImplementedError):
-            ht.sub(T, T_s)
+            ht.sub(self.a_tensor, self.a_split_tensor)
         with self.assertRaises(TypeError):
-            ht.div(T, otherType)
+            ht.div(self.a_tensor, self.errorneous_type)
         with self.assertRaises(TypeError):
             ht.div('T', 's')
 
     def test_fmod(self):
-        T_r = ht.float32([
+        result = ht.array([
             [1., 0.],
             [1., 0.]
         ])
-        T_int = ht.int32([
+        an_int_tensor = ht.array([
             [5, 3],
             [4, 1]
         ])
-        T_r_int = ht.int32([
+        integer_result = ht.array([
             [1, 1],
             [0, 1]
         ])
-        T_inv = ht.float32([
+        commutated_result = ht.array([
             [0.0, 0.0],
             [2.0, 2.0]
         ])
-        T_zero = ht.float32([
-            [0.0, 0.0],
-            [0.0, 0.0]
-        ])
-        float1 = ht.float32([5.3])
-        float2 = ht.float32([1.9])
-        float_res = ht.float32([1.5])
+        zero_tensor = ht.zeros((2, 2,))
 
-        self.assertTrue(ht.equal(ht.fmod(s, s), ht.float32([0.0])))
-        self.assertTrue(ht.equal(ht.fmod(T, T), T_zero))
-        self.assertTrue(ht.equal(ht.fmod(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.fmod(T, T1), T_r))
-        self.assertTrue(ht.equal(ht.fmod(T, v), T_r))
-        self.assertTrue(ht.equal(ht.fmod(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.fmod(T_int, s_int), T_r_int))
-        self.assertTrue(ht.equal(ht.fmod(s, T), T_inv))
-        self.assertTrue(ht.equal(ht.fmod(T_s, T), T_inv))
-        self.assertTrue(ht.allclose(ht.fmod(float1, float2), float_res))
+        a_float = ht.array([5.3])
+        another_float = ht.array([1.9])
+        result_float = ht.array([1.5])
+
+        self.assertTrue(ht.equal(ht.fmod(self.a_scalar, self.a_scalar), ht.float32([0.0])))
+        self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.a_tensor), zero_tensor))
+        self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.another_tensor), result))
+        self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.a_vector), result))
+        self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.fmod(an_int_tensor, self.an_int_scalar), integer_result))
+        self.assertTrue(ht.equal(ht.fmod(self.a_scalar, self.a_tensor), commutated_result))
+        self.assertTrue(ht.equal(ht.fmod(self.a_split_tensor, self.a_tensor), commutated_result))
+        self.assertTrue(ht.allclose(ht.fmod(a_float, another_float), result_float))
 
         with self.assertRaises(ValueError):
-            ht.fmod(T, v2)
+            ht.fmod(self.a_tensor, self.another_vector)
         with self.assertRaises(NotImplementedError):
-            ht.fmod(T, T_s)
+            ht.fmod(self.a_tensor, self.a_split_tensor)
         with self.assertRaises(TypeError):
-            ht.fmod(T, otherType)
+            ht.fmod(self.a_tensor, self.errorneous_type)
         with self.assertRaises(TypeError):
             ht.fmod('T', 's')
 
     def test_mod(self):
-        T_int_1 = ht.int32([[1, 4], [2, 2]])
-        T_int_2 = ht.int32([[1, 2], [3, 4]])
-        T_int_res_1 = ht.int32([[0, 0], [2, 2]])
-        T_int_res_2 = ht.int32([[1, 0], [0, 0]])
-
-        self.assertTrue(ht.equal(ht.mod(T_int_1, T_int_2), T_int_res_1))
-        self.assertTrue(ht.equal(ht.mod(T_int_1, s_int), T_int_res_2))
-        self.assertTrue(ht.equal(ht.mod(s_int, T_int_2), T_int_res_1))
-
-    def test_mul(self):
-        T_r = ht.float32([
-            [2, 4],
-            [6, 8]
+        a_tensor = ht.array([
+            [1, 4],
+            [2, 2]
+        ])
+        another_tensor = ht.array([
+            [1, 2],
+            [3, 4]
+        ])
+        a_result = ht.array([
+            [0, 0],
+            [2, 2]
+        ])
+        another_result = ht.array([
+            [1, 0],
+            [0, 0]
         ])
 
-        self.assertTrue(ht.equal(ht.mul(s, s), ht.float32([4.0])))
-        self.assertTrue(ht.equal(ht.mul(T, s), T_r))
-        self.assertTrue(ht.equal(ht.mul(s, T), T_r))
-        self.assertTrue(ht.equal(ht.mul(T, T1), T_r))
-        self.assertTrue(ht.equal(ht.mul(T, v), T_r))
-        self.assertTrue(ht.equal(ht.mul(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.mul(T_s, T), T_r))
+        self.assertTrue(ht.equal(ht.mod(a_tensor, another_tensor), a_result))
+        self.assertTrue(ht.equal(ht.mod(a_tensor, self.an_int_scalar), another_result))
+        self.assertTrue(ht.equal(ht.mod(self.an_int_scalar, another_tensor), a_result))
+
+    def test_mul(self):
+        result = ht.array([
+            [2.0, 4.0],
+            [6.0, 8.0]
+        ])
+
+        self.assertTrue(ht.equal(ht.mul(self.a_scalar, self.a_scalar), ht.array([4.0])))
+        self.assertTrue(ht.equal(ht.mul(self.a_tensor, self.a_scalar), result))
+        self.assertTrue(ht.equal(ht.mul(self.a_scalar, self.a_tensor), result))
+        self.assertTrue(ht.equal(ht.mul(self.a_tensor, self.another_tensor), result))
+        self.assertTrue(ht.equal(ht.mul(self.a_tensor, self.a_vector), result))
+        self.assertTrue(ht.equal(ht.mul(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.mul(self.a_split_tensor, self.a_tensor), result))
 
         with self.assertRaises(ValueError):
-            ht.mul(T, v2)
+            ht.mul(self.a_tensor, self.another_vector)
         with self.assertRaises(NotImplementedError):
-            ht.mul(T, T_s)
+            ht.mul(self.a_tensor, self.a_split_tensor)
         with self.assertRaises(TypeError):
-            ht.mul(T, otherType)
+            ht.mul(self.a_tensor, self.errorneous_type)
         with self.assertRaises(TypeError):
             ht.mul('T', 's')
 
     def test_pow(self):
-        T_r = ht.float32([
-            [1, 4],
-            [9, 16]
+        result = ht.array([
+            [1.0,  4.0],
+            [9.0, 16.0]
+        ])
+        commutated_result = ht.array([
+            [2.0,  4.0],
+            [8.0, 16.0]
         ])
 
-        T_inv = ht.float32([
-            [2, 4],
-            [8, 16]
-        ])
-
-        self.assertTrue(ht.equal(ht.pow(s, s), ht.float32([4.0])))
-        self.assertTrue(ht.equal(ht.pow(T, s), T_r))
-        self.assertTrue(ht.equal(ht.pow(s, T), T_inv))
-        self.assertTrue(ht.equal(ht.pow(T, T1), T_r))
-        self.assertTrue(ht.equal(ht.pow(T, v), T_r))
-        self.assertTrue(ht.equal(ht.pow(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.pow(T_s, T), T_inv))
+        self.assertTrue(ht.equal(ht.pow(self.a_scalar, self.a_scalar), ht.array([4.0])))
+        self.assertTrue(ht.equal(ht.pow(self.a_tensor, self.a_scalar), result))
+        self.assertTrue(ht.equal(ht.pow(self.a_scalar, self.a_tensor), commutated_result))
+        self.assertTrue(ht.equal(ht.pow(self.a_tensor, self.another_tensor), result))
+        self.assertTrue(ht.equal(ht.pow(self.a_tensor, self.a_vector), result))
+        self.assertTrue(ht.equal(ht.pow(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.pow(self.a_split_tensor, self.a_tensor), commutated_result))
 
         with self.assertRaises(ValueError):
-            ht.pow(T, v2)
+            ht.pow(self.a_tensor, self.another_vector)
         with self.assertRaises(NotImplementedError):
-            ht.pow(T, T_s)
+            ht.pow(self.a_tensor, self.a_split_tensor)
         with self.assertRaises(TypeError):
-            ht.pow(T, otherType)
+            ht.pow(self.a_tensor, self.errorneous_type)
         with self.assertRaises(TypeError):
             ht.pow('T', 's')
 
     def test_sub(self):
-        T_r = ht.float32([
-            [-1, 0],
-            [1, 2]
+        result = ht.array([
+            [-1.0, 0.0],
+            [ 1.0, 2.0]
+        ])
+        minus_result = ht.array([
+            [ 1.0,  0.0],
+            [-1.0, -2.0]
         ])
 
-        T_r_minus = ht.float32([
-            [1, 0],
-            [-1, -2]
-        ])
-
-        self.assertTrue(ht.equal(ht.sub(s, s), ht.float32([0.0])))
-        self.assertTrue(ht.equal(ht.sub(T, s), T_r))
-        self.assertTrue(ht.equal(ht.sub(s, T), T_r_minus))
-        self.assertTrue(ht.equal(ht.sub(T, T1), T_r))
-        self.assertTrue(ht.equal(ht.sub(T, v), T_r))
-        self.assertTrue(ht.equal(ht.sub(T, s_int), T_r))
-        self.assertTrue(ht.equal(ht.sub(T_s, T), T_r_minus))
+        self.assertTrue(ht.equal(ht.sub(self.a_scalar, self.a_scalar), ht.array([0.0])))
+        self.assertTrue(ht.equal(ht.sub(self.a_tensor, self.a_scalar), result))
+        self.assertTrue(ht.equal(ht.sub(self.a_scalar, self.a_tensor), minus_result))
+        self.assertTrue(ht.equal(ht.sub(self.a_tensor, self.another_tensor), result))
+        self.assertTrue(ht.equal(ht.sub(self.a_tensor, self.a_vector), result))
+        self.assertTrue(ht.equal(ht.sub(self.a_tensor, self.an_int_scalar), result))
+        self.assertTrue(ht.equal(ht.sub(self.a_split_tensor, self.a_tensor), minus_result))
 
         with self.assertRaises(ValueError):
-            ht.sub(T, v2)
+            ht.sub(self.a_tensor, self.another_vector)
         with self.assertRaises(NotImplementedError):
-            ht.sub(T, T_s)
+            ht.sub(self.a_tensor, self.a_split_tensor)
         with self.assertRaises(TypeError):
-            ht.sub(T, otherType)
+            ht.sub(self.a_tensor, self.errorneous_type)
         with self.assertRaises(TypeError):
             ht.sub('T', 's')
 
@@ -222,8 +234,7 @@ class TestArithmetics(unittest.TestCase):
 
         out_noaxis = ht.zeros((1,))
         ht.sum(shape_noaxis, out=out_noaxis)
-        self.assertTrue(out_noaxis._Tensor__array ==
-                        shape_noaxis._Tensor__array.sum())
+        self.assertTrue(out_noaxis._Tensor__array == shape_noaxis._Tensor__array.sum())
 
         # check sum over all float elements of split 1d tensor
         shape_noaxis_split = ht.arange(array_len, split=0)
@@ -270,20 +281,16 @@ class TestArithmetics(unittest.TestCase):
 
         out_noaxis = ht.zeros((3, 3,))
         ht.sum(shape_noaxis, axis=0, out=out_noaxis)
-        self.assertTrue((out_noaxis._Tensor__array ==
-                         torch.full((3, 3,), 3)).all())
+        self.assertTrue((out_noaxis._Tensor__array == torch.full((3, 3,), 3)).all())
 
         # check sum over all float elements of splitted 5d tensor with negative axis
         shape_noaxis_split_axis_neg = ht.ones((1, 2, 3, 4, 5), split=1)
-        shape_noaxis_split_axis_neg_sum = shape_noaxis_split_axis_neg.sum(
-            axis=-2)
+        shape_noaxis_split_axis_neg_sum = shape_noaxis_split_axis_neg.sum(axis=-2)
 
         self.assertIsInstance(shape_noaxis_split_axis_neg_sum, ht.Tensor)
-        self.assertEqual(
-            shape_noaxis_split_axis_neg_sum.shape, (1, 2, 3, 5))
+        self.assertEqual(shape_noaxis_split_axis_neg_sum.shape, (1, 2, 3, 5))
         self.assertEqual(shape_noaxis_split_axis_neg_sum.dtype, ht.float32)
-        self.assertEqual(
-            shape_noaxis_split_axis_neg_sum._Tensor__array.dtype, torch.float32)
+        self.assertEqual(shape_noaxis_split_axis_neg_sum._Tensor__array.dtype, torch.float32)
         self.assertEqual(shape_noaxis_split_axis_neg_sum.split, 1)
 
         out_noaxis = ht.zeros((1, 2, 3, 5))
