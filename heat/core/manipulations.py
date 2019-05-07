308db784ef9a727a9c2b6a83675f3f0e005370de
Claudia Comito <c.comito@fz-juelich.de>
2019-05-07 14:50:26 +0200

    (Broken) Introducing manipulations.py, test_manipulations.py

---
+++ heat/core/manipulations.py
@@ -0,0 +1,104 @@
+import torch
+
+from . import operations
+from . import dndarray
+from . import types
+
+__all__ = [
+    'squeeze']
+
+
+def squeeze(x, axis=None):
+    """
+    Remove single-dimensional entries from the shape of a tensor.
+
+    Parameters:
+    -----------
+    x : ht.DNDarray
+        Input data.
+
+    axis : None or int or tuple of ints, optional
+
+
+    Returns:
+    --------	
+    squeezed : ht.DNDarray
+               The input tensor, but with all or a subset of the dimensions of length 1 removed. 
+
+
+    Examples:
+    >>> import heat as ht
+    >>> import torch
+    >>> torch.manual_seed(1)
+    <torch._C.Generator object at 0x115704ad0>
+    >>> a = ht.random.randn(1,3,1,5)
+    >>> a
+    tensor([[[[ 0.2673, -0.4212, -0.5107, -1.5727, -0.1232]],
+
+            [[ 3.5870, -1.8313,  1.5987, -1.2770,  0.3255]],
+
+            [[-0.4791,  1.3790,  2.5286,  0.4107, -0.9880]]]])
+    >>> a.shape
+    (1, 3, 1, 5)
+    >>> ht.squeeze(a).shape
+    (3, 5)
+    >>> ht.squeeze(a)
+    tensor([[ 0.2673, -0.4212, -0.5107, -1.5727, -0.1232],
+            [ 3.5870, -1.8313,  1.5987, -1.2770,  0.3255],
+            [-0.4791,  1.3790,  2.5286,  0.4107, -0.9880]])
+    >>> ht.squeeze(a,axis=0).shape
+    (3, 1, 5)
+    >>> ht.squeeze(a,axis=-2).shape
+    (1, 3, 5)
+    >>> ht.squeeze(a,axis=1).shape
+    Traceback (most recent call last):
+    ...
+    ValueError: Dimension along axis 1 is not 1 for shape (1, 3, 1, 5)
+    """
+
+    # Sanitize input
+    if not isinstance(x, dndarray.DNDarray):
+        raise TypeError('expected x to be a ht.DNDarray, but was {}'.format(type(x)))
+    # Sanitize axis
+    axis = stride_tricks.sanitize_axis(x.shape, axis)
+    if axis is not None:
+        if isinstance(axis, int):
+            dim_is_one = (x.shape[axis] == 1)
+        if isinstance(axis, tuple):
+            dim_is_one = bool(tensor.array(list(x.shape[dim] == 1 for dim in axis)).all()._tensor__array)
+        if not dim_is_one:
+            raise ValueError('Dimension along axis {} is not 1 for shape {}'.format(axis, x.shape))
+
+    # Local squeeze
+    if axis is None:
+        axis = tuple(i for i, dim in enumerate(x.shape) if dim == 1)
+    if isinstance(axis, int):
+        axis = (axis,)
+    out_shape = tuple(x.lshape[dim] for dim in range(len(x.lshape)) if not dim in axis)
+    x_lsqueezed = x._tensor__array.reshape(out_shape)
+
+    # Distributed squeeze
+    if x.split is not None:
+        split = None
+        if x.comm.is_distributed():
+            if x.split in axis:
+                raise ValueError('Cannot split AND squeeze along same axis. Split is {}, axis is {} for shape {}'.format(
+                    x.split, axis, x.shape))
+            out_shape = tuple(x.gshape[dim] for dim in range(len(x.gshape)) if not dim in axis)
+            x_gsqueezed = tensor.empty(out_shape, dtype=x.dtype)
+            x.comm.Allgather(x_lsqueezed, x_gsqueezed)
+            return tensor.tensor(
+                x_gsqueezed,
+                out_shape,
+                x.dtype,
+                split=split,
+                device=x.device,
+                comm=x.comm)
+
+    return tensor.tensor(
+        x_lsqueezed,
+        out_shape,
+        x.dtype,
+        split=None,
+        device=x.device,
+        comm=x.comm)
---
+++ heat/core/tests/test_manipulations.py
@@ -0,0 +1,83 @@
+import unittest
+import torch
+
+import numpy as np
+import heat as ht
+
+
+class TestManipulations(unittest.TestCase):
+    def test_squeeze(self):
+        torch.manual_seed(1)
+        data = ht.random.randn(1, 4, 5, 1)
+
+        # 4D local tensor, no axis
+        result = ht.squeeze(data)
+        self.assertIsInstance(result, ht.tensor)
+        self.assertEqual(result.dtype, ht.float32)
+        self.assertEqual(result._tensor__array.dtype, torch.float32)
+        self.assertEqual(result.shape, (4, 5))
+        self.assertEqual(result.lshape, (4, 5))
+        self.assertEqual(result.split, None)
+        self.assertTrue((result._tensor__array == data._tensor__array.squeeze()).all())
+
+        # 4D local tensor, major axis
+        result = ht.squeeze(data, axis=0)
+        self.assertIsInstance(result, ht.tensor)
+        self.assertEqual(result.dtype, ht.float32)
+        self.assertEqual(result._tensor__array.dtype, torch.float32)
+        self.assertEqual(result.shape, (4, 5, 1))
+        self.assertEqual(result.lshape, (4, 5, 1))
+        self.assertEqual(result.split, None)
+        self.assertTrue((result._tensor__array == data._tensor__array.squeeze(0)).all())
+
+        # 4D local tensor, minor axis
+        result = ht.squeeze(data, axis=-1)
+        self.assertIsInstance(result, ht.tensor)
+        self.assertEqual(result.dtype, ht.float32)
+        self.assertEqual(result._tensor__array.dtype, torch.float32)
+        self.assertEqual(result.shape, (1, 4, 5))
+        self.assertEqual(result.lshape, (1, 4, 5))
+        self.assertEqual(result.split, None)
+        self.assertTrue((result._tensor__array == data._tensor__array.squeeze(-1)).all())
+
+        # 4D local tensor, tuple axis
+        result = data.squeeze(axis=(0, -1))
+        self.assertIsInstance(result, ht.tensor)
+        self.assertEqual(result.dtype, ht.float32)
+        self.assertEqual(result._tensor__array.dtype, torch.float32)
+        self.assertEqual(result.shape, (4, 5))
+        self.assertEqual(result.lshape, (4, 5))
+        self.assertEqual(result.split, None)
+        self.assertTrue((result._tensor__array == data._tensor__array.squeeze()).all())
+
+        # 4D split tensor, along the axis
+        data = ht.array(ht.random.randn(1, 4, 5, 1), split=1)
+        result = ht.squeeze(data, axis=-1)
+        self.assertIsInstance(result, ht.tensor)
+        self.assertEqual(result.dtype, ht.float32)
+        self.assertEqual(result._tensor__array.dtype, torch.float32)
+        self.assertEqual(result.shape, (1, ht.MPI_WORLD.size * 4, 5))
+        self.assertEqual(result.lshape, (1, 4, 5))
+        self.assertEqual(result.split, 1)
+
+        # 3D split tensor, across the axis
+        size = ht.MPI_WORLD.size * 2
+        data = ht.triu(ht.ones((1, size, size), split=0), k=1)
+
+        result = ht.squeeze(data, axis=0)
+        self.assertIsInstance(result, ht.tensor)
+        self.assertEqual(result.dtype, ht.float32)
+        self.assertEqual(result._tensor__array.dtype, torch.float32)
+        self.assertEqual(result.shape, (size, size))
+        self.assertEqual(result.lshape, (size, size))
+        #self.assertEqual(result.split, None)
+
+        # check exceptions
+        with self.assertRaises(ValueError):
+            data.squeeze(axis=(0, 1))
+        with self.assertRaises(TypeError):
+            data.squeeze(axis=1.1)
+        with self.assertRaises(TypeError):
+            data.squeeze(axis='y')
+        with self.assertRaises(ValueError):
+            ht.argmin(data, axis=-4)
