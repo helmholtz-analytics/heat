Added scalar broadcast for casts, fixed unittests, amended stride_tricks to allow for 0 dimensions

heat/core/dndarray.py
@@ -461,7 +461,11 @@ class DNDarray:
         if np.prod(self.shape) == 1:
             if self.split is None:
                 return cast_function(self.__array)
-            return self.comm.bcast(cast_function(self.__array) if self.comm.rank == 0 else None, root=0)
+
+            is_empty = np.prod(self.__array.shape) == 0
+            root = self.comm.allreduce(0 if is_empty else self.comm.rank, op=MPI.SUM)
+
+            return self.comm.bcast(None if is_empty else cast_function(self.__array), root=root)
 
         raise TypeError('only size-1 arrays can be converted to Python scalars')
 
@@ -1548,7 +1552,8 @@ class DNDarray:
                 if isinstance(key[self.split], slice):
                     key = list(key)
                     overlap = list(set(range(key[self.split].start if key[self.split].start is not None else 0,
-                                             key[self.split].stop if key[self.split].stop is not None else self.gshape[self.split]))
+                                             key[self.split].stop if key[self.split].stop is not None else self.gshape[self.split],
+                                             key[self.split].step if key[self.split].step is not None else 1))
                                    & set(range(chunk_start, chunk_end)))
 
                     if overlap:

heat/core/tests/test_dndarray.py
@@ -29,7 +29,7 @@ class TestDNDarray(unittest.TestCase):
         self.assertEqual(as_float64._DNDarray__array.dtype, torch.float64)
         self.assertIs(as_float64, data)
 
-    def test_bool(self):
+    def test_bool_cast(self):
         # simple scalar tensor
         a = ht.ones(1)
         casted_a = bool(a)
@@ -59,7 +59,7 @@ class TestDNDarray(unittest.TestCase):
             with self.assertRaises(TypeError):
                 bool(ht.full((ht.MPI_WORLD.size,), 2, split=0))
 
-    def test_complex(self):
+    def test_complex_cast(self):
         # simple scalar tensor
         a = ht.ones(1)
         casted_a = complex(a)
@@ -89,7 +89,7 @@ class TestDNDarray(unittest.TestCase):
             with self.assertRaises(TypeError):
                 complex(ht.full((ht.MPI_WORLD.size,), 2, split=0))
 
-    def test_float(self):
+    def test_float_cast(self):
         # simple scalar tensor
         a = ht.ones(1)
         casted_a = float(a)
@@ -119,7 +119,7 @@ class TestDNDarray(unittest.TestCase):
             with self.assertRaises(TypeError):
                 float(ht.full((ht.MPI_WORLD.size,), 2, split=0))
 
-    def test_int(self):
+    def test_int_cast(self):
         # simple scalar tensor
         a = ht.ones(1)
         casted_a = int(a)
@@ -396,7 +396,7 @@ class TestDNDarray(unittest.TestCase):
 
         # slice in both directions
         a = ht.zeros((13, 5,), split=1)
-        a[3:13, 2:5:2] = 1
+        a[3:13, 2::2] = 1
         self.assertTrue((a[3:13, 2:5:2] == 1).all())
         self.assertEqual(a[3:13, 2:5:2].gshape, (10, 2))
         self.assertEqual(a[3:13, 2:5:2].split, 1)