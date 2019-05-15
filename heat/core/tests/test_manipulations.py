f8829653dcae74ca0c4a264a1f5bbf056c486425
Claudia Comito <c.comito@fz-juelich.de>
2019-05-15 08:57:28 +0200

    Merged manipulation.py into manipulations.py incl. unit tests.

--- heat/core/tests/test_manipulations.py
+++ heat/core/tests/test_manipulations.py
@@ -6,6 +6,123 @@ import heat as ht
 
 
 class TestManipulations(unittest.TestCase):
+    def test_expand_dims(self):
+        # vector data
+        a = ht.arange(10)
+        b = ht.expand_dims(a, 0)
+
+        self.assertIsInstance(b, ht.DNDarray)
+        self.assertEqual(len(b.shape), 2)
+
+        self.assertEqual(b.shape[0], 1)
+        self.assertEqual(b.shape[1], a.shape[0])
+
+        self.assertEqual(b.lshape[0], 1)
+        self.assertEqual(b.lshape[1], a.shape[0])
+
+        self.assertIs(b.split, None)
+
+        # vector data with out-of-bounds axis
+        a = ht.arange(12)
+        b = a.expand_dims(1)
+
+        self.assertIsInstance(b, ht.DNDarray)
+        self.assertEqual(len(b.shape), 2)
+
+        self.assertEqual(b.shape[0], a.shape[0])
+        self.assertEqual(b.shape[1], 1)
+
+        self.assertEqual(b.lshape[0], a.shape[0])
+        self.assertEqual(b.lshape[1], 1)
+
+        self.assertIs(b.split, None)
+
+        # volume with intermediate axis
+        a = ht.empty((3, 4, 5,))
+        b = a.expand_dims(1)
+
+        self.assertIsInstance(b, ht.DNDarray)
+        self.assertEqual(len(b.shape), 4)
+
+        self.assertEqual(b.shape[0], a.shape[0])
+        self.assertEqual(b.shape[1], 1)
+        self.assertEqual(b.shape[2], a.shape[1])
+        self.assertEqual(b.shape[3], a.shape[2])
+
+        self.assertEqual(b.lshape[0], a.shape[0])
+        self.assertEqual(b.lshape[1], 1)
+        self.assertEqual(b.lshape[2], a.shape[1])
+        self.assertEqual(b.lshape[3], a.shape[2])
+
+        self.assertIs(b.split, None)
+
+        # volume with negative axis
+        a = ht.empty((3, 4, 5,))
+        b = a.expand_dims(-4)
+
+        self.assertIsInstance(b, ht.DNDarray)
+        self.assertEqual(len(b.shape), 4)
+
+        self.assertEqual(b.shape[0], 1)
+        self.assertEqual(b.shape[1], a.shape[0])
+        self.assertEqual(b.shape[2], a.shape[1])
+        self.assertEqual(b.shape[3], a.shape[2])
+
+        self.assertEqual(b.lshape[0], 1)
+        self.assertEqual(b.lshape[1], a.shape[0])
+        self.assertEqual(b.lshape[2], a.shape[1])
+        self.assertEqual(b.lshape[3], a.shape[2])
+
+        self.assertIs(b.split, None)
+
+        # split volume with negative axis expansion after the split
+        a = ht.empty((3, 4, 5,), split=1)
+        b = a.expand_dims(-2)
+
+        self.assertIsInstance(b, ht.DNDarray)
+        self.assertEqual(len(b.shape), 4)
+
+        self.assertEqual(b.shape[0], a.shape[0])
+        self.assertEqual(b.shape[1], a.shape[1])
+        self.assertEqual(b.shape[2], 1)
+        self.assertEqual(b.shape[3], a.shape[2])
+
+        self.assertEqual(b.lshape[0], a.shape[0])
+        self.assertLessEqual(b.lshape[1], a.shape[1])
+        self.assertEqual(b.lshape[2], 1)
+        self.assertEqual(b.lshape[3], a.shape[2])
+
+        self.assertIs(b.split, 1)
+
+        # split volume with negative axis expansion before the split
+        a = ht.empty((3, 4, 5,), split=2)
+        b = a.expand_dims(-3)
+
+        self.assertIsInstance(b, ht.DNDarray)
+        self.assertEqual(len(b.shape), 4)
+
+        self.assertEqual(b.shape[0], a.shape[0])
+        self.assertEqual(b.shape[1], 1)
+        self.assertEqual(b.shape[2], a.shape[1])
+        self.assertEqual(b.shape[3], a.shape[2])
+
+        self.assertEqual(b.lshape[0], a.shape[0])
+        self.assertEqual(b.lshape[1], 1)
+        self.assertEqual(b.lshape[2], a.shape[1])
+        self.assertLessEqual(b.lshape[3], a.shape[2])
+
+        self.assertIs(b.split, 3)
+
+        # exceptions
+        with self.assertRaises(TypeError):
+            ht.expand_dims('(3, 4, 5,)', 1)
+        with self.assertRaises(TypeError):
+            ht.empty((3, 4, 5,)).expand_dims('1')
+        with self.assertRaises(ValueError):
+            ht.empty((3, 4, 5,)).expand_dims(4)
+        with self.assertRaises(ValueError):
+            ht.empty((3, 4, 5,)).expand_dims(-5)
+
     def test_squeeze(self):
         torch.manual_seed(1)
         data = ht.random.randn(1, 4, 5, 1)
