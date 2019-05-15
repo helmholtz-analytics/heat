02aae243d4f8797670a1183dbd6560070011f445
simon <therealslimv@yahoo.de>
2019-05-15 20:08:04 +0200

    implemented parallel sort step 1 - 4

--- heat/core/manipulation.py
+++ heat/core/manipulation.py
@@ -1,3 +1,5 @@
+import torch
+
 from . import dndarray
 from . import stride_tricks
 
@@ -62,3 +64,57 @@ def expand_dims(a, axis):
         a.device,
         a.comm
     )
+
+
+def sort(a, axis=None, descending=False, out=None):
+    # default: using last axis
+    if axis is None:
+        axis = len(a.shape) - 1
+
+    if not a.split or axis != a.split:
+        partial = torch.sort(a._DNDarray__array, dim=axis, descending=descending)
+
+    else:
+        transposed = a._DNDarray__array.transpose(0, axis)
+        local_sorted = torch.sort(a._DNDarray__array, dim=0, descending=descending)
+
+        size = a.comm.Get_size()
+        rank = a.comm.Get_rank()
+        length = local_sorted.size()[axis]
+        # Separate the sorted tensor into size + 1 equal length partitions
+        partitions = [x * length // (size + 1) for x in range(1, size + 1)]
+        local_pivots = local_sorted
+
+        # Share pivot elements with root process
+        pivot_buffer = None
+        if rank is 0:
+            pivot_buffer = torch.empty((size * size, ), dtype=a.dtype.torch_type())
+        a.comm.Gather(local_pivots, pivot_buffer, root=0)
+
+        global_pivots = torch.empty((size - 1,), dtype=a.dtype.torch_type())
+        if rank is 0:
+            # sorting the random pivots for the global partitioning
+            sorted_pivots = torch.sort(pivot_buffer, dim=0, descending=descending)
+            length = sorted_pivots.size()[axis]
+            global_partitions = [x * length // size for x in range(1, size)]
+            global_pivots = sorted_pivots[global_partitions]
+
+        a.comm.Bcast(global_pivots, root=0)
+
+        # compute starting locations
+        partition_list = [[0] * size]
+        index = 0
+        # for val in local_sorted:
+
+
+
+    if out:
+        out._DND__array = partial
+    else:
+        return dndarray.DNDarray(
+            partial,
+            a.dtype,
+            a.split,
+            a.device,
+            a.comm
+        )
