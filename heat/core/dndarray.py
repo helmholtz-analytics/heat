6fe9c523d3ea9e3db60c2438946603e0226219b2
coquelin77 <d.coquelin@fz-juelich.de>
2019-05-23 09:35:52 +0200

    fixed but in the case that the overlap list was not sorted

--- heat/core/dndarray.py
+++ heat/core/dndarray.py
@@ -1095,6 +1095,7 @@ class DNDarray:
                     key = list(key)
                     overlap = list(key_set & chunk_set)
                     if overlap:  # if the slice is requesting data on the nodes
+                        overlap.sort()
                         hold = [x - chunk_start for x in overlap]
                         key[self.split] = slice(min(hold), max(hold) + 1, key[self.split].step)
                         arr = self.__array[tuple(key)]
@@ -1127,6 +1128,7 @@ class DNDarray:
                 overlap = list(key_set & chunk_set)
 
                 if overlap:
+                    overlap.sort()
                     hold = [x - chunk_start for x in overlap]
                     key = slice(min(hold), max(hold) + 1, step)
                     arr = self.__array[key]
@@ -1779,6 +1781,7 @@ class DNDarray:
                                    & set(range(chunk_start, chunk_end)))
 
                     if overlap:
+                        overlap.sort()
                         hold = [x - chunk_start for x in overlap]
                         key[self.split] = slice(min(hold), max(hold) + 1, key[self.split].step)
                         try:
@@ -1796,6 +1799,7 @@ class DNDarray:
             elif isinstance(key, slice) and self.split == 0:
                 overlap = list(set(range(key.start, key.stop)) & set(range(chunk_start, chunk_end)))
                 if overlap:
+                    overlap.sort()
                     hold = [x - chunk_start for x in overlap]
                     key = slice(min(hold), max(hold) + 1, key.step)
                     self.__setter(key, value)
