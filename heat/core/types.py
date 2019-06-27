c9db9ceed15518100fc110a03a0f72620343bd7c
Markus Goetz <markus.goetz@kit.edu>
2019-06-27 12:29:03 +0200

    Removed abc parent classes to speed up instance checks

--- heat/core/types.py
+++ heat/core/types.py
@@ -19,7 +19,6 @@ generic
  \\-> flexible (currently unused, placeholder for characters)
 """
 
-import abc
 import builtins
 import collections
 import numpy as np
@@ -59,8 +58,7 @@ __all__ = [
 ]
 
 
-class generic(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
+class generic:
     def __new__(cls, *value, device=None, comm=None):
         try:
             torch_type = cls.torch_type()
@@ -90,14 +88,12 @@ class generic(metaclass=abc.ABCMeta):
         return dndarray.DNDarray(array, tuple(array.shape), cls, split=None, device=device, comm=comm)
 
     @classmethod
-    @abc.abstractclassmethod
     def torch_type(cls):
-        pass
+        return NotImplemented
 
     @classmethod
-    @abc.abstractclassmethod
     def char(cls):
-        pass
+        return NotImplemented
 
 
 class bool(generic):
