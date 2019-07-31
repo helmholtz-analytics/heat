ab947069d29c340bdb0f6d759b423c4f0719c0eb
coquelin77 <d.coquelin@fz-juelich.de>
2019-07-31 11:25:59 +0200

    ....wrong value in var test...again

--- heat/core/tests/test_statistics.py
+++ heat/core/tests/test_statistics.py
@@ -636,7 +636,7 @@ class TestStatistics(unittest.TestCase):
             ht.var(x, axis='01')
 
         a = ht.arange(1, 5)
-        self.assertEqual(a.var(), 1.2910)
+        self.assertEqual(a.var(), 1.6667)
 
         # ones
         dimensions = []
