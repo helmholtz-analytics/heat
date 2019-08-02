f1e70cd9f10aa920460284298a4faede485b99de
simon <ubwdr@student.kit.edu>
2019-08-02 16:40:40 +0200

    run into a bug

--- heat/core/tests/test_suites/basic_test.py
+++ heat/core/tests/test_suites/basic_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from heat.core import dndarray, MPICommunication, MPI, types, factories, random
+from heat.core import dndarray, MPICommunication, MPI, types, factories
+import heat as ht
 
 import numpy as np
 import torch
@@ -11,12 +12,12 @@ class BasicTest(TestCase):
     __device = None
 
     @property
-    def device(self):
-        return BasicTest.__device
-
-    @property
     def comm(self):
         return BasicTest.__comm
+
+    @property
+    def device(self):
+        return BasicTest.__device
 
     def get_rank(self):
         return self.comm.rank
@@ -24,7 +25,43 @@ class BasicTest(TestCase):
     def get_size(self):
         return self.comm.size
 
+    def assert_array_equal(self, heat_array: dndarray.DNDarray, numpy_array):
+        self.assertIsInstance(heat_array, dndarray.DNDarray,
+                              'The array to test was not a instance of ht.DNDarray. '
+                              'Instead got {}.'.format(type(heat_array)))
+        self.assertIsInstance(numpy_array, np.ndarray,
+                              'The array to test against was not a instance of numpy.ndarray. '
+                              'Instead got {}.'.format(type(numpy_array)))
+        # self.assertEqual(heat_array.shape, numpy_array.shape,
+        #                  'Global shapes do not match. Got {} expected {}'.format(heat_array.shape, numpy_array.shape))
+        print('heat gshape', heat_array.gshape, heat_array.shape, heat_array.lshape)
+        numpy_array = numpy_array.astype(np.float64)
+        split = heat_array.split
+        offset, local_shape, slices = heat_array.comm.chunk(heat_array.gshape, split)
+        print('slices', slices, 'counts', offset, 'local_shape', local_shape)
+        # self.assertEqual(heat_array.lshape, numpy_array[slices].shape,
+        #                  'Local shapes do not match. '
+        #                  'Got {} expected {}'.format(heat_array.lshape, numpy_array[slices].shape))
+
+        local_numpy = heat_array._DNDarray__array.numpy()
+
+        # Array is distributed correctly
+        equal_res = np.array(np.allclose(local_numpy, numpy_array[slices]))
+        print('heat', local_numpy, '\nnumpy', numpy_array[slices], '\ndiff', heat_array._DNDarray__array.numpy() - numpy_array[slices])
+        print('equal_res', equal_res)
+        self.comm.Allreduce(MPI.IN_PLACE, equal_res, MPI.LAND)
+        self.assertTrue(equal_res, 'Local tensors do not match the corresponding numpy slices.')
+        self.assertEqual(local_numpy.dtype, numpy_array.dtype,
+                         'Resulting types do not match heat: {} numpy: {}.'.format(heat_array.dtype, numpy_array.dtype))
+
+        # Combined array is correct
+        combined = heat_array.numpy()
+        print('combined', combined)
+        self.assertTrue(np.array_equal(heat_array.numpy(), numpy_array), 'Combined tensors do not match.')
+
     def assert_func_equal(self, tensor, heat_func, numpy_func, distributed_result=True, heat_args=None, numpy_args=None):
+        self.assertTrue(callable(heat_func))
+        self.assertTrue(callable(numpy_func))
         print('rank', self.get_rank())
         if isinstance(tensor, tuple) or isinstance(tensor, list):
             tensor = self._create_random_array(tensor)
@@ -62,33 +99,6 @@ class BasicTest(TestCase):
                 self.assert_array_equal(ht_res, np_res)
             else:
                 self.assertTrue(np.array_equal(ht_res._DNDarray__array.numpy(), np_res))
-
-    def assert_array_equal(self, heat_array: dndarray.DNDarray, numpy_array):
-        self.assertIsInstance(heat_array, dndarray.DNDarray,
-                              'The array to test was not a instance of ht.DNDarray. '
-                              'Instead got {}.'.format(type(heat_array)))
-        self.assertIsInstance(numpy_array, np.ndarray,
-                              'The array to test against was not a instance of numpy.ndarray. '
-                              'Instead got {}.'.format(type(numpy_array)))
-
-        numpy_array = numpy_array.astype(np.float64)
-        split = heat_array.split
-        offset, local_shape, slices = heat_array.comm.chunk(heat_array.gshape, split)
-
-        local_numpy = heat_array._DNDarray__array.numpy()
-
-        # Array is distributed correctly
-        equal_res = np.array(np.array_equal(local_numpy, numpy_array[slices]))
-        print('heat', local_numpy, '\nnumpy', numpy_array[slices], '\ndiff', heat_array._DNDarray__array.numpy() - numpy_array[slices])
-        self.comm.Allreduce(MPI.IN_PLACE, equal_res, MPI.LAND)
-        self.assertTrue(equal_res, 'Local tensors do not match the corresponding numpy slices.')
-        self.assertEqual(local_numpy.dtype, numpy_array.dtype,
-                         'Resulting types do not match heat: {} numpy: {}.'.format(heat_array.dtype, numpy_array.dtype))
-
-        # Combined array is correct
-        combined = heat_array.numpy()
-        print('combined', combined)
-        self.assertTrue(np.array_equal(heat_array.numpy(), numpy_array), 'Combined tensors do not match.')
 
     def _create_random_array(self, shape):
         # Ensure all processes have the same initial array
--- heat/core/tests/test_suites/test_basic_test.py
+++ heat/core/tests/test_suites/test_basic_test.py
@@ -16,22 +16,23 @@ class TestBasicTest(BasicTest):
             self.assert_array_equal(heat_array, np_array)
 
     def test_assert_func_equal(self):
-        array = np.ones((self.get_size(), 20), dtype=np.int8)
-        ht_func = ht.any
-        np_func = np.any
-        self.assert_func_equal(array, ht_func, np_func, distributed_result=False)
-
-        array = np.array([[1, 2, 4, 1, 3], [1, 4, 7, 5, 1]], dtype=np.int8)
-        ht_func = ht.unique
-        np_func = np.unique
-        ht_args = {'sorted': True, 'axis': 0}
-        np_args = {'axis': 0}
-        self.assert_func_equal(array, ht_func, np_func, heat_args=ht_args, numpy_args=np_args)
-
-        # Testing with random values
-        # shape = (5, 2, 6)
+        # array = np.ones((self.get_size(), 20), dtype=np.int8)
+        # ht_func = ht.any
+        # np_func = np.any
+        # self.assert_func_equal(array, ht_func, np_func, distributed_result=False)
+        #
+        # array = np.array([[1, 2, 4, 1, 3], [1, 4, 7, 5, 1]], dtype=np.int8)
         # ht_func = ht.unique
         # np_func = np.unique
-        # ht_args = {'sorted': True, 'axis': 1}
-        # np_args = {'axis': 1}
-        # self.assert_func_equal(shape, heat_func=ht_func, numpy_func=np_func, heat_args=ht_args, numpy_args=np_args)
+        # ht_args = {'sorted': True, 'axis': 0}
+        # np_args = {'axis': 0}
+        # self.assert_func_equal(array, ht_func, np_func, heat_args=ht_args, numpy_args=np_args)
+
+        # Testing with random values
+        a = ht.ones((4, ), split=0)
+        a = a.exp()
+        print('shape', a.gshape)
+        shape = (5, )
+        ht_func = ht.exp
+        np_func = np.exp
+        self.assert_func_equal(shape, heat_func=ht_func, numpy_func=np_func)
