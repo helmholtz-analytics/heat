483eff767cb24ba14314f8cdd9c241b7ec88ea8e
simon <ubwdr@student.kit.edu>
2019-10-15 11:44:06 +0200

    changed code to be split invariant

--- heat/core/tests/test_exp.py
+++ heat/core/tests/test_exp.py
@@ -2,7 +2,7 @@ import unittest
 import heat as ht
 
 
-class TestTravis(unittest.TestCase):
+class TestExp(unittest.TestCase):
     def test_one_failed(self):
         size = ht.get_comm().size
         print("size", size)
