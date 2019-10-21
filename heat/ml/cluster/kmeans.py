434a2dc04b63e23f9a19daf8f68b1bbe8f811231
Debus, Charlotte <charlotte.debus@dlr.de>
2019-10-21 15:05:06 +0200

    Formatting

--- heat/ml/cluster/kmeans.py
+++ heat/ml/cluster/kmeans.py
@@ -113,7 +113,9 @@ class KMeans:
             # Samples will be equally distributed drawn from all involved processes
 
             _, displ, _ = X.comm.counts_displs_shape(shape=X.shape, axis=0)
-            centroids = ht.empty((X.shape[1], self.n_clusters), split=None, device=X.device, comm=X.comm)
+            centroids = ht.empty(
+                (X.shape[1], self.n_clusters), split=None, device=X.device, comm=X.comm
+            )
 
             if (X.split is None) or (X.split == 0):
                 for i in range(self.n_clusters):
@@ -153,7 +155,9 @@ class KMeans:
         # kmeans++, smart centroid guessing
         elif self.init == "kmeans++":
             if (X.split is None) or (X.split == 0):
-                centroids = ht.empty((X.shape[1], self.n_clusters), split=None, device=X.device, comm=X.comm)
+                centroids = ht.empty(
+                    (X.shape[1], self.n_clusters), split=None, device=X.device, comm=X.comm
+                )
                 sample = ht.random.randint(0, X.shape[0] - 1).item()
                 _, displ, _ = X.comm.counts_displs_shape(shape=X.shape, axis=0)
                 proc = 0
