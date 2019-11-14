7d9b477d1f9ae998ff9aa412f742d4c181e3ebe6
simon <ubwdr@student.kit.edu>
2019-11-14 13:34:34 +0100

    fixed black issues

--- heat/core/tests/test_suites/test_basic_test.py
+++ heat/core/tests/test_suites/test_basic_test.py
@@ -6,7 +6,6 @@ from heat.core.tests.test_suites.basic_test import BasicTest
 
 
 class TestBasicTest(BasicTest):
-
     def test_assert_array_equal(self):
         heat_array = ht.ones((self.get_size(), 10, 10), dtype=ht.int32, split=1)
         np_array = np.ones((self.get_size(), 10, 10), dtype=np.int32)
@@ -17,8 +16,11 @@ class TestBasicTest(BasicTest):
             self.assert_array_equal(heat_array, np_array)
 
         heat_array = ht.zeros((25, 13, self.get_size(), 20), dtype=ht.float32, split=2)
-        expected_array = torch.zeros((25, 13, self.get_size(), 20),
-                                     dtype=torch.float32, device=heat_array.device.torch_device)
+        expected_array = torch.zeros(
+            (25, 13, self.get_size(), 20),
+            dtype=torch.float32,
+            device=heat_array.device.torch_device,
+        )
         self.assert_array_equal(heat_array, expected_array)
 
     def test_assert_func_equal(self):
@@ -30,8 +32,8 @@ class TestBasicTest(BasicTest):
         array = np.array([[1, 2, 4, 1, 3], [1, 4, 7, 5, 1]], dtype=np.int8)
         ht_func = ht.expand_dims
         np_func = np.expand_dims
-        ht_args = {'axis': 1}
-        np_args = {'axis': 1}
+        ht_args = {"axis": 1}
+        np_args = {"axis": 1}
         self.assert_func_equal(array, ht_func, np_func, heat_args=ht_args, numpy_args=np_args)
 
         # Testing with random values
