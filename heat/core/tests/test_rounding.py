342e661d7b49d4cfd10953ba707fae97dfdee252
Claudia Comito <c.comito@fz-juelich.de>
2019-11-25 13:14:28 +0100

    test_modf(), test_round(): defining test tensors so that they are always distributed evenly (shape is a multiple of number of ranks).

--- heat/core/tests/test_rounding.py
+++ heat/core/tests/test_rounding.py
@@ -163,7 +163,9 @@ class TestRounding(unittest.TestCase):
             ht.floor(object())
 
     def test_modf(self):
-        start, end, step = -5.0, 5.0, 1.4
+        size = ht.communication.MPI_WORLD.size
+        start, end = -5.0, 5.0
+        step = (end - start) / (2 * size)
         comparison = np.modf(np.arange(start, end, step, np.float32))
 
         # exponential of float32
@@ -242,13 +244,13 @@ class TestRounding(unittest.TestCase):
         self.assertAlmostEqual(ht.all(float64_modf_distrbd[1]), ht.all(ht.array(comparison[1])))
 
     def test_round(self):
-        print("DEBUG: test_round, size = ")
-        start, end, step = -5.0, 5.0, 1.4
+        size = ht.communication.MPI_WORLD.size
+        start, end = -5.0, 5.0
+        step = (end - start) / (2 * size)
         comparison = torch.arange(start, end, step, dtype=torch.float32).round()
 
         # exponential of float32
         float32_tensor = ht.arange(start, end, step, dtype=ht.float32)
-        print("DEBUG: test_round. split, size = ", float32_tensor.split, float32_tensor.comm.size)
         float32_round = float32_tensor.round()
         self.assertIsInstance(float32_round, ht.DNDarray)
         self.assertEqual(float32_round.dtype, ht.float32)
@@ -278,12 +280,6 @@ class TestRounding(unittest.TestCase):
         comparison = torch.arange(start, end, step, dtype=torch.float32).round()
         float32_tensor_distrbd = ht.arange(start, end, step, dtype=ht.float32, split=0)
         float32_round_distrbd = float32_tensor_distrbd.round()
-        print(
-            "DEBUG: test_round. split, dtype, size = ",
-            float32_tensor_distrbd.split,
-            float32_tensor_distrbd.dtype,
-            float32_tensor_distrbd.comm.size,
-        )
         self.assertIsInstance(float32_round_distrbd, ht.DNDarray)
         self.assertEqual(float32_round_distrbd.dtype, ht.float32)
         self.assertEqual(float32_round_distrbd.dtype, ht.float32)
@@ -292,12 +288,6 @@ class TestRounding(unittest.TestCase):
         # exponential of float64
         comparison = torch.arange(start, end, step, dtype=torch.float64).round()
         float64_tensor_distrbd = ht.arange(start, end, step, dtype=ht.float64, split=0)
-        print(
-            "DEBUG: test_round. split, dtype, size = ",
-            float64_tensor_distrbd.split,
-            float64_tensor_distrbd.dtype,
-            float64_tensor_distrbd.comm.size,
-        )
         float64_round_distrbd = float64_tensor_distrbd.round()
         self.assertIsInstance(float64_round_distrbd, ht.DNDarray)
         self.assertEqual(float64_round_distrbd.dtype, ht.float64)
