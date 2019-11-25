89caeaa7268b8b3a79b7f52852fb08d85f378fe6
Claudia Comito <c.comito@fz-juelich.de>
2019-11-25 11:32:03 +0100

    sanitize_memory_layout(), moved tests for least likely occurrencies to the bottom

--- heat/core/memory.py
+++ heat/core/memory.py
@@ -33,9 +33,17 @@ def sanitize_memory_layout(x, order="C"):
     """
     dims = list(range(x.ndim))
     shape = x.shape
-    # assess current layout
     row_major = bool(x.stride()[i] > x.stride()[i + 1] for i in dims)
     column_major = False if row_major else bool(x.stride()[i] < x.stride()[i + 1] for i in dims)
+    if (order == "C" and column_major) or (order == "F" and row_major):
+        if column_major:
+            new_stride = tuple(np.prod(shape[i + 1 :]) for i in dims[:-1]) + (1,)
+        elif row_major:
+            new_stride = (1,) + tuple(np.prod(shape[-x.ndim : -x.ndim + i]) for i in dims[1:])
+        dims[0], dims[-1] = dims[-1], dims[0]
+        permutation = tuple(dims)
+        x = x.permute(permutation).contiguous()
+        x = x.set_(x.storage(), x.storage_offset(), shape, new_stride)
     if not row_major and not column_major:
         raise NotImplementedError(
             "Expecting row/major or column-major memory layout, not implemented for alternative layouts."
@@ -44,12 +52,4 @@ def sanitize_memory_layout(x, order="C"):
         raise NotImplementedError(
             "Internal usage of torch.clone() means losing original memory layout for now. \n Please specify order='C' for row-major, order='F' for column-major layout."
         )
-    if order == "C" and not row_major:
-        new_stride = tuple(np.prod(shape[i + 1 :]) for i in dims[:-1]) + (1,)
-    elif order == "F" and not column_major:
-        new_stride = (1,) + tuple(np.prod(shape[-x.ndim : -x.ndim + i]) for i in dims[1:])
-    dims[0], dims[-1] = dims[-1], dims[0]
-    permutation = tuple(dims)
-    x = x.permute(permutation).contiguous()
-    x = x.set_(x.storage(), x.storage_offset(), shape, new_stride)
     return x
