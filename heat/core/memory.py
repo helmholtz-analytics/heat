1da8a74e801793a32ff3c5bcf7bb2ae7046f6c5c
Claudia Comito <c.comito@fz-juelich.de>
2019-11-28 14:36:53 +0100

    Modified sanitize_memory_layout() to address tensors with unknown/wrong layout (e.g. from tensor creation with ndmin > tensor.shape).

--- heat/core/memory.py
+++ heat/core/memory.py
@@ -39,23 +39,24 @@ def sanitize_memory_layout(x, order="C"):
         Default is 'C' as in C-like (row-major) memory layout. The array is stored in memory rows first.
         Alternative is 'F', as in Fortran-like (column-major) memory layout. The array is stored columns first.
     """
+    if x.ndim < 2:
+        return x
     dims = list(range(x.ndim))
     shape = x.shape
-    row_major = bool(x.stride()[i] > x.stride()[i + 1] for i in dims)
-    column_major = False if row_major else bool(x.stride()[i] < x.stride()[i + 1] for i in dims)
+    stride = x.stride()
+    row_major = all(np.diff(list(stride)) < 0)
+    column_major = all(np.diff(list(stride)) > 0)
+    if order == "C":
+        if not row_major:
+            stride = tuple(np.prod(shape[i + 1 :]) for i in dims[:-1]) + (1,)
+    if order == "F":
+        if not column_major:
+            stride = (1,) + tuple(np.prod(shape[-x.ndim : -x.ndim + i]) for i in dims[1:])
     if (order == "C" and column_major) or (order == "F" and row_major):
-        if column_major:
-            new_stride = tuple(np.prod(shape[i + 1 :]) for i in dims[:-1]) + (1,)
-        elif row_major:
-            new_stride = (1,) + tuple(np.prod(shape[-x.ndim : -x.ndim + i]) for i in dims[1:])
         dims[0], dims[-1] = dims[-1], dims[0]
-        permutation = tuple(dims)
-        x = x.permute(permutation).contiguous()
-        x = x.set_(x.storage(), x.storage_offset(), shape, new_stride)
-    if not row_major and not column_major:
-        raise NotImplementedError(
-            "Expecting row-major or column-major memory layout, not implemented for alternative layouts."
-        )
+    permutation = tuple(dims)
+    x = x.permute(permutation).contiguous()
+    x = x.set_(x.storage(), x.storage_offset(), shape, stride)
     if order == "K":
         raise NotImplementedError(
             "Internal usage of torch.clone() means losing original memory layout for now. \n Please specify order='C' for row-major, order='F' for column-major layout."
