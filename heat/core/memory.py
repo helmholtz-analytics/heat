e47ac209670f2b0fdd03ba1b985f807c0107558c
Claudia Comito <c.comito@fz-juelich.de>
2019-12-02 14:24:52 +0100

    Fixed error that messed up column-major memory layout for ndim>3

--- heat/core/memory.py
+++ heat/core/memory.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from . import dndarray
 
 __all__ = ["copy", "sanitize_memory_layout"]
@@ -35,7 +36,7 @@ def sanitize_memory_layout(x, order="C"):
     x: torch.tensor
         Input data
 
-    order: str, optional. 
+    order: str, optional.
         Default is 'C' as in C-like (row-major) memory layout. The array is stored in memory rows first.
         Alternative is 'F', as in Fortran-like (column-major) memory layout. The array is stored columns first.
     """
@@ -43,24 +44,24 @@ def sanitize_memory_layout(x, order="C"):
         # do nothing
         return x
     dims = list(range(x.ndim))
-    shape = x.shape
-    stride = x.stride()
-    row_major = all(np.diff(list(stride)) <= 0)
-    column_major = all(np.diff(list(stride)) >= 0)
+    stride = list(x.stride())
+    row_major = all(np.diff(stride) <= 0)
+    column_major = all(np.diff(stride) >= 0)
     if (order == "C" and row_major) or (order == "F" and column_major):
         # do nothing
         return x
-    if order == "C":
-        stride = tuple(np.prod(shape[i + 1 :]) for i in dims[:-1]) + (1,)
-    if order == "F":
-        stride = (1,) + tuple(np.prod(shape[-x.ndim : -x.ndim + i]) for i in dims[1:])
     if (order == "C" and column_major) or (order == "F" and row_major):
-        dims[0], dims[-1] = dims[-1], dims[0]
-    permutation = tuple(dims)
-    x = x.permute(permutation).contiguous()
-    x = x.set_(x.storage(), x.storage_offset(), shape, stride)
+        dims = tuple(reversed(dims))
+        y = torch.empty_like(x)
+        permutation = x.permute(dims).contiguous()
+        y = y.set_(
+            permutation.storage(),
+            x.storage_offset(),
+            x.shape,
+            tuple(reversed(permutation.stride())),
+        )
     if order == "K":
         raise NotImplementedError(
             "Internal usage of torch.clone() means losing original memory layout for now. \n Please specify order='C' for row-major, order='F' for column-major layout."
         )
-    return x
+    return y
