48e109e671a3a9b516ce4244f31742cd01a0737e
coquelin77 <d.coquelin@fz-juelich.de>
2019-12-09 16:52:47 +0100

    added setter and getter tests to tiling

--- heat/core/tests/test_tiling.py
+++ heat/core/tests/test_tiling.py
@@ -19,11 +19,11 @@ class TestTiling(unittest.TestCase):
         with self.assertRaises(ValueError):
             ht.tiling.SquareDiagTiles(ht.arange(2), tiles_per_proc=1)
 
-    def test_properties_global_set_get(self):
+    def test_properties(self):
+        # ---- m = n ------------- properties ------ s0 -----------
         m_eq_n_s0 = ht.random.randn(47, 47, split=0)
         m_eq_n_s0_t1 = ht.tiling.SquareDiagTiles(m_eq_n_s0, tiles_per_proc=1)
         m_eq_n_s0_t2 = ht.tiling.SquareDiagTiles(m_eq_n_s0, tiles_per_proc=2)
-        # ---- m = n ------------- properties ------ s0 -----------
         # arr
         self.assertTrue(ht.equal(m_eq_n_s0_t1.arr, m_eq_n_s0))
         self.assertTrue(ht.equal(m_eq_n_s0_t2.arr, m_eq_n_s0))
@@ -44,7 +44,6 @@ class TestTiling(unittest.TestCase):
             # tile rows per proc
             self.assertEqual(m_eq_n_s0_t1.tile_rows_per_process, [1, 1, 1])
             self.assertEqual(m_eq_n_s0_t2.tile_rows_per_process, [2, 2, 2])
-
         # last diag pr
         self.assertEqual(m_eq_n_s0_t1.last_diagonal_process, m_eq_n_s0.comm.size - 1)
         self.assertEqual(m_eq_n_s0_t2.last_diagonal_process, m_eq_n_s0.comm.size - 1)
@@ -76,7 +75,6 @@ class TestTiling(unittest.TestCase):
             # tile rows per proc
             self.assertEqual(m_eq_n_s1_t1.tile_rows_per_process, [3, 3, 3])
             self.assertEqual(m_eq_n_s1_t2.tile_rows_per_process, [6, 6, 6])
-
         # last diag pr
         self.assertEqual(m_eq_n_s1_t1.last_diagonal_process, m_eq_n_s1.comm.size - 1)
         self.assertEqual(m_eq_n_s1_t2.last_diagonal_process, m_eq_n_s1.comm.size - 1)
@@ -104,7 +102,6 @@ class TestTiling(unittest.TestCase):
             # tile rows per proc
             self.assertEqual(m_gr_n_s0_t1.tile_rows_per_process, [1, 1, 1])
             self.assertEqual(m_gr_n_s0_t2.tile_rows_per_process, [2, 2, 2])
-
         # last diag pr
         self.assertEqual(m_gr_n_s0_t1.last_diagonal_process, m_eq_n_s1.comm.size - 1)
         self.assertEqual(m_gr_n_s0_t2.last_diagonal_process, m_eq_n_s1.comm.size - 1)
@@ -115,7 +112,7 @@ class TestTiling(unittest.TestCase):
         self.assertEqual(m_gr_n_s0_t1.tile_rows, m_eq_n_s1.comm.size)
         self.assertEqual(m_gr_n_s0_t2.tile_rows, m_eq_n_s1.comm.size * 2)
 
-        # ---- m > n ------------- properties ------ s0 -----------
+        # ---- m > n ------------- properties ------ s1 -----------
         m_gr_n_s1 = ht.random.randn(38, 128, split=1)
         m_gr_n_s1_t1 = ht.tiling.SquareDiagTiles(m_gr_n_s1, tiles_per_proc=1)
         m_gr_n_s1_t2 = ht.tiling.SquareDiagTiles(m_gr_n_s1, tiles_per_proc=2)
@@ -132,7 +129,6 @@ class TestTiling(unittest.TestCase):
             # tile rows per proc
             self.assertEqual(m_gr_n_s1_t1.tile_rows_per_process, [1, 1, 1])
             self.assertEqual(m_gr_n_s1_t2.tile_rows_per_process, [2, 2, 2])
-
             # last diag pr
             self.assertEqual(m_gr_n_s1_t1.last_diagonal_process, 0)
             self.assertEqual(m_gr_n_s1_t2.last_diagonal_process, 0)
@@ -143,17 +139,192 @@ class TestTiling(unittest.TestCase):
             self.assertEqual(m_gr_n_s1_t1.tile_rows, 1)
             self.assertEqual(m_gr_n_s1_t2.tile_rows, 2)
 
-        # m_ls_n_s0 = ht.random.randn(323, 49, split=0)
-        # m_ls_n_s0_t1 = ht.tiling.SquareDiagTiles(m_ls_n_s0, tiles_per_proc=1)
-        # m_ls_n_s0_t2 = ht.tiling.SquareDiagTiles(m_ls_n_s0, tiles_per_proc=2)
-        #
-        # m_ls_n_s1 = ht.random.randn(323, 49, split=1)
-        # m_ls_n_s1_t1 = ht.tiling.SquareDiagTiles(m_ls_n_s1, tiles_per_proc=1)
-        # m_ls_n_s1_t2 = ht.tiling.SquareDiagTiles(m_ls_n_s1, tiles_per_proc=2)
+        # ---- m < n ------------- properties ------ s0 -----------
+        m_ls_n_s0 = ht.random.randn(323, 49, split=0)
+        m_ls_n_s0_t1 = ht.tiling.SquareDiagTiles(m_ls_n_s0, tiles_per_proc=1)
+        m_ls_n_s0_t2 = ht.tiling.SquareDiagTiles(m_ls_n_s0, tiles_per_proc=2)
+        if m_eq_n_s1.comm.size == 3:
+            # col_inds
+            self.assertEqual(m_ls_n_s0_t1.col_indices, [0])
+            self.assertEqual(m_ls_n_s0_t2.col_indices, [0, 25])
+            # row inds
+            self.assertEqual(m_ls_n_s0_t1.row_indices, [0, 49, 109, 216])
+            self.assertEqual(m_ls_n_s0_t2.row_indices, [0, 25, 49, 110, 163, 216, 270])
+            # tile cols per proc
+            self.assertEqual(m_ls_n_s0_t1.tile_columns_per_process, [1])
+            self.assertEqual(m_ls_n_s0_t2.tile_columns_per_process, [2])
+            # tile rows per proc
+            self.assertEqual(m_ls_n_s0_t1.tile_rows_per_process, [2, 1, 1])
+            self.assertEqual(m_ls_n_s0_t2.tile_rows_per_process, [3, 2, 2])
+            # last diag pr
+            self.assertEqual(m_ls_n_s0_t1.last_diagonal_process, 0)
+            self.assertEqual(m_ls_n_s0_t2.last_diagonal_process, 0)
+            # tile cols
+            self.assertEqual(m_ls_n_s0_t1.tile_columns, 1)
+            self.assertEqual(m_ls_n_s0_t2.tile_columns, 2)
+            # tile rows
+            self.assertEqual(m_ls_n_s0_t1.tile_rows, 4)
+            self.assertEqual(m_ls_n_s0_t2.tile_rows, 7)
 
-        # last diag pr
-        # tile cols per pr
-        # tile rows per pr
-        # lshape map
-        #
-        pass
+        # ---- m < n ------------- properties ------ s1 -----------
+        m_ls_n_s1 = ht.random.randn(323, 49, split=1)
+        m_ls_n_s1_t1 = ht.tiling.SquareDiagTiles(m_ls_n_s1, tiles_per_proc=1)
+        m_ls_n_s1_t2 = ht.tiling.SquareDiagTiles(m_ls_n_s1, tiles_per_proc=2)
+        if m_eq_n_s1.comm.size == 3:
+            # col_inds
+            self.assertEqual(m_ls_n_s1_t1.col_indices, [0, 17, 33])
+            self.assertEqual(m_ls_n_s1_t2.col_indices, [0, 9, 17, 25, 33, 41])
+            # row inds
+            self.assertEqual(m_ls_n_s1_t1.row_indices, [0, 17, 33, 49])
+            self.assertEqual(m_ls_n_s1_t2.row_indices, [0, 9, 17, 25, 33, 41, 49])
+            # tile cols per proc
+            self.assertEqual(m_ls_n_s1_t1.tile_columns_per_process, [1, 1, 1])
+            self.assertEqual(m_ls_n_s1_t2.tile_columns_per_process, [2, 2, 2])
+            # tile rows per proc
+            self.assertEqual(m_ls_n_s1_t1.tile_rows_per_process, [4, 4, 4])
+            self.assertEqual(m_ls_n_s1_t2.tile_rows_per_process, [7, 7, 7])
+            # last diag pr
+            self.assertEqual(m_ls_n_s1_t1.last_diagonal_process, 2)
+            self.assertEqual(m_ls_n_s1_t2.last_diagonal_process, 2)
+            # tile cols
+            self.assertEqual(m_ls_n_s1_t1.tile_columns, 3)
+            self.assertEqual(m_ls_n_s1_t2.tile_columns, 6)
+            # tile rows
+            self.assertEqual(m_ls_n_s1_t1.tile_rows, 4)
+            self.assertEqual(m_ls_n_s1_t2.tile_rows, 7)
+
+    def test_local_set_get(self):
+        # this function will also test all of the start_stop functions and the
+        # --------------------- local ----------- s0 ----------------
+        # (int), (int, int), (slice, int), (slice, slice), (int, slice)
+        m_eq_n_s0 = ht.zeros((25, 25), split=0)
+        m_eq_n_s0_t2 = ht.tiling.SquareDiagTiles(m_eq_n_s0, tiles_per_proc=2)
+        k = (slice(0, 10), slice(2, None))
+        m_eq_n_s0_t2.local_set(key=k, value=1)
+        lcl_key = m_eq_n_s0_t2.local_to_global(key=k, rank=m_eq_n_s0.comm.rank)
+        st_sp = m_eq_n_s0_t2.get_start_stop(key=lcl_key)
+        sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+        lcl_slice = m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+        self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+        # reset base
+        m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = (1, 1)
+        m_eq_n_s0_t2.local_set(key=k, value=1)
+        lcl_key = m_eq_n_s0_t2.local_to_global(key=k, rank=m_eq_n_s0.comm.rank)
+        st_sp = m_eq_n_s0_t2.get_start_stop(key=lcl_key)
+        sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+        lcl_slice = m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+        self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+        # reset base
+        m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = 1
+        m_eq_n_s0_t2.local_set(key=k, value=1)
+        lcl_key = m_eq_n_s0_t2.local_to_global(key=k, rank=m_eq_n_s0.comm.rank)
+        st_sp = m_eq_n_s0_t2.get_start_stop(key=lcl_key)
+        sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+        lcl_slice = m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+        self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+        # reset base
+        m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        # --------------------- local ----------- s1 ----------------
+        m_eq_n_s1 = ht.zeros((25, 25), split=1)
+        m_eq_n_s1_t2 = ht.tiling.SquareDiagTiles(m_eq_n_s1, tiles_per_proc=2)
+        k = (slice(0, 2), slice(0, None))
+        m_eq_n_s1_t2.local_set(key=k, value=1)
+        lcl_key = m_eq_n_s1_t2.local_to_global(key=k, rank=m_eq_n_s1.comm.rank)
+        st_sp = m_eq_n_s1_t2.get_start_stop(key=lcl_key)
+        sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+        lcl_slice = m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+        self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+        # reset base
+        m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = (5, 1)
+        m_eq_n_s1_t2.local_set(key=k, value=1)
+        lcl_key = m_eq_n_s1_t2.local_to_global(key=k, rank=m_eq_n_s1.comm.rank)
+        st_sp = m_eq_n_s1_t2.get_start_stop(key=lcl_key)
+        sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+        lcl_slice = m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+        self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+        # reset base
+        m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = 2
+        m_eq_n_s1_t2.local_set(key=k, value=1)
+        lcl_key = m_eq_n_s1_t2.local_to_global(key=k, rank=m_eq_n_s1.comm.rank)
+        st_sp = m_eq_n_s1_t2.get_start_stop(key=lcl_key)
+        sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+        lcl_slice = m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+        self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+        # reset base
+        m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        # --------------------- global ---------- s0 ----------------
+        m_eq_n_s0 = ht.zeros((25, 25), split=0)
+        m_eq_n_s0_t2 = ht.tiling.SquareDiagTiles(m_eq_n_s0, tiles_per_proc=2)
+        k = 2
+        m_eq_n_s0_t2[k] = 1
+        if m_eq_n_s0_t2[k] is not None:
+            st_sp = m_eq_n_s0_t2.get_start_stop(key=k)
+            sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+            lcl_slice = m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+            self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+            # reset base
+            m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = (5, 5)
+        m_eq_n_s0_t2[k] = 1
+        if m_eq_n_s0_t2[k] is not None:
+            st_sp = m_eq_n_s0_t2.get_start_stop(key=k)
+            sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+            lcl_slice = m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+            self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+            # reset base
+            m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = (slice(0, 2), slice(1, 5))
+        m_eq_n_s0_t2[k] = 1
+        if m_eq_n_s0_t2[k] is not None:
+            st_sp = m_eq_n_s0_t2.get_start_stop(key=k)
+            sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+            lcl_slice = m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+            self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+            # reset base
+            m_eq_n_s0._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        # --------------------- global ---------- s1 ----------------
+        m_eq_n_s1 = ht.zeros((25, 25), split=1)
+        m_eq_n_s1_t2 = ht.tiling.SquareDiagTiles(m_eq_n_s1, tiles_per_proc=2)
+        k = (slice(0, 3), slice(0, 2))
+        m_eq_n_s1_t2[k] = 1
+        if m_eq_n_s1_t2[k] is not None:
+            st_sp = m_eq_n_s1_t2.get_start_stop(key=k)
+            sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+            lcl_slice = m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+            self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+            # reset base
+            m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        # k = (slice(0, 3), slice(0, 2))
+        k = (5, 5)
+        m_eq_n_s1_t2[k] = 1
+        if m_eq_n_s1_t2[k] is not None:
+            st_sp = m_eq_n_s1_t2.get_start_stop(key=k)
+            sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+            lcl_slice = m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+            self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+            # reset base
+            m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
+
+        k = (slice(0, 3), 3)
+        m_eq_n_s1_t2[k] = 1
+        if m_eq_n_s1_t2[k] is not None:
+            st_sp = m_eq_n_s1_t2.get_start_stop(key=k)
+            sz = st_sp[1] - st_sp[0], st_sp[3] - st_sp[2]
+            lcl_slice = m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]]
+            self.assertTrue(torch.all(lcl_slice - torch.ones(sz) == 0))
+            # reset base
+            m_eq_n_s1._DNDarray__array[st_sp[0] : st_sp[1], st_sp[2] : st_sp[3]] = 0
