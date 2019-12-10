9dcc46fc904d09b0df0fa38c5ad2536e2064d3a7
Claudia Comito <c.comito@fz-juelich.de>
2019-12-10 14:08:45 +0100

    Moving cluster and regression to heat/core from heat/core/ml

---
+++ heat/core/cluster/tests/test_kmeans.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+import heat as ht
+
+
+class TestKMeans(unittest.TestCase):
+    def test_fit_iris(self):
+        # get some test data
+        iris = ht.load_hdf5(os.path.join(os.getcwd(), "heat/datasets/data/iris.h5"), "data")
+
+        # fit the clusters
+        k = 3
+        kmeans = ht.ml.cluster.KMeans(n_clusters=k)
+        kmeans.fit(iris)
+
+        # check whether the results are correct
+        self.assertIsInstance(kmeans.cluster_centers_, ht.DNDarray)
+        self.assertEqual(kmeans.cluster_centers_.shape, (k, iris.shape[1]))
+        # same test with init=kmeans++
+        kmeans = ht.ml.cluster.KMeans(n_clusters=k, init="kmeans++")
+        kmeans.fit(iris)
+
+        # check whether the results are correct
+        self.assertIsInstance(kmeans.cluster_centers_, ht.DNDarray)
+        self.assertEqual(kmeans.cluster_centers_.shape, (k, iris.shape[1]))
