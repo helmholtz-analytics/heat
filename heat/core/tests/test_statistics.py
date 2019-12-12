0cc44c08deaad4e7119e23753c8ac6de8b481b46
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2019-12-12 10:07:03 +0100

    check for load_hdf5 beforehand

--- heat/core/tests/test_statistics.py
+++ heat/core/tests/test_statistics.py
@@ -590,12 +590,13 @@ class TestStatistics(unittest.TestCase):
                         else:
                             self.assertEqual(res.split, z.split)
 
-        # values for the iris dataset mean measured by libreoffice calc
-        ax0 = ht.array([5.84333333333333, 3.054, 3.75866666666667, 1.19866666666667])
-        for sp in [None, 0, 1]:
-            iris = ht.load("heat/datasets/data/iris.h5", "data", split=sp)
-            self.assertTrue(ht.allclose(ht.mean(iris), 3.46366666666667))
-            self.assertTrue(ht.allclose(ht.mean(iris, axis=0), ax0))
+        if "load_hdf5" in dir(ht):
+            # values for the iris dataset mean measured by libreoffice calc
+            ax0 = ht.array([5.84333333333333, 3.054, 3.75866666666667, 1.19866666666667])
+            for sp in [None, 0, 1]:
+                iris = ht.load("heat/datasets/data/iris.h5", "data", split=sp)
+                self.assertTrue(ht.allclose(ht.mean(iris), 3.46366666666667))
+                self.assertTrue(ht.allclose(ht.mean(iris, axis=0), ax0))
 
     def test_min(self):
         data = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
@@ -841,7 +842,8 @@ class TestStatistics(unittest.TestCase):
                 res = z.var(bessel=False)
                 self.assertTrue((res == 0).all())
 
-        # values for the iris dataset var measured by libreoffice calc
-        for sp in [None, 0, 1]:
-            iris = ht.load_hdf5("heat/datasets/data/iris.h5", "data", split=sp)
-            self.assertTrue(ht.allclose(ht.var(iris, bessel=True), 3.90318519755147))
+        if "load_hdf5" in dir(ht):
+            # values for the iris dataset var measured by libreoffice calc
+            for sp in [None, 0, 1]:
+                iris = ht.load_hdf5("heat/datasets/data/iris.h5", "data", split=sp)
+                self.assertTrue(ht.allclose(ht.var(iris, bessel=True), 3.90318519755147))
--- heat/ml/cluster/tests/test_kmeans.py
+++ heat/ml/cluster/tests/test_kmeans.py
@@ -3,24 +3,25 @@ import unittest
 
 import heat as ht
 
+if "load_hdf5" in dir(ht):
 
-class TestKMeans(unittest.TestCase):
-    def test_fit_iris(self):
-        # get some test data
-        iris = ht.load_hdf5(os.path.join(os.getcwd(), "heat/datasets/data/iris.h5"), "data")
+    class TestKMeans(unittest.TestCase):
+        def test_fit_iris(self):
+            # get some test data
+            iris = ht.load_hdf5(os.path.join(os.getcwd(), "heat/datasets/data/iris.h5"), "data")
 
-        # fit the clusters
-        k = 3
-        kmeans = ht.ml.cluster.KMeans(n_clusters=k)
-        kmeans.fit(iris)
+            # fit the clusters
+            k = 3
+            kmeans = ht.ml.cluster.KMeans(n_clusters=k)
+            kmeans.fit(iris)
 
-        # check whether the results are correct
-        self.assertIsInstance(kmeans.cluster_centers_, ht.DNDarray)
-        self.assertEqual(kmeans.cluster_centers_.shape, (k, iris.shape[1]))
-        # same test with init=kmeans++
-        kmeans = ht.ml.cluster.KMeans(n_clusters=k, init="kmeans++")
-        kmeans.fit(iris)
+            # check whether the results are correct
+            self.assertIsInstance(kmeans.cluster_centers_, ht.DNDarray)
+            self.assertEqual(kmeans.cluster_centers_.shape, (k, iris.shape[1]))
+            # same test with init=kmeans++
+            kmeans = ht.ml.cluster.KMeans(n_clusters=k, init="kmeans++")
+            kmeans.fit(iris)
 
-        # check whether the results are correct
-        self.assertIsInstance(kmeans.cluster_centers_, ht.DNDarray)
-        self.assertEqual(kmeans.cluster_centers_.shape, (k, iris.shape[1]))
+            # check whether the results are correct
+            self.assertIsInstance(kmeans.cluster_centers_, ht.DNDarray)
+            self.assertEqual(kmeans.cluster_centers_.shape, (k, iris.shape[1]))
