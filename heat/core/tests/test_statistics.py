494d5c51d55e01b39b74a66ceeb88fc435769c52
coquelin77 <d.coquelin@fz-juelich.de>
2020-01-15 17:00:28 +0100

    increasing coverage

--- heat/core/tests/test_statistics.py
+++ heat/core/tests/test_statistics.py
@@ -598,6 +598,8 @@ class TestStatistics(unittest.TestCase):
             ht.mean(x, axis=(0, "10"))
         with self.assertRaises(ValueError):
             ht.mean(x, axis=(0, 0))
+        with self.assertRaises(ValueError):
+            ht.mean(x, axis=torch.Tensor([0, 0]))
 
         a = ht.arange(1, 5, device=ht_device)
         self.assertEqual(a.mean(), 2.5)
@@ -887,6 +889,8 @@ class TestStatistics(unittest.TestCase):
             ht.var(x, axis=(0, "10"))
         with self.assertRaises(ValueError):
             ht.var(x, axis=(0, 0))
+        with self.assertRaises(ValueError):
+            ht.mean(x, axis=torch.Tensor([0, 0]))
 
         a = ht.arange(1, 5, device=ht_device)
         self.assertEqual(a.var(), 1.666666666666666)
