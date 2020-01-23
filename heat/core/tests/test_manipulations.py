919e3090078f56a91c917f20895c8ceb9080a297
Claudia Comito <c.comito@fz-juelich.de>
2020-01-23 15:31:34 +0100

    Increasing test coverage

--- heat/core/tests/test_manipulations.py
+++ heat/core/tests/test_manipulations.py
@@ -1069,8 +1069,10 @@ class TestManipulations(BasicTest):
         exp_res, exp_inv = torch_array.unique(return_inverse=True, sorted=True)
 
         data_split_none = ht.array(torch_array, device=ht_device)
-        res, inv = ht.unique(data_split_none, return_inverse=True, sorted=True)
+        res = ht.unique(data_split_none, sorted=True)
         self.assertIsInstance(res, ht.DNDarray)
+        res, inv = ht.unique(data_split_none, return_inverse=True, sorted=True)
+        self.assertIsInstance(inv, ht.DNDarray)
         self.assertTrue(torch.equal(inv._DNDarray__array, exp_inv))
 
         data_split_zero = ht.array(torch_array, split=0, device=ht_device)
