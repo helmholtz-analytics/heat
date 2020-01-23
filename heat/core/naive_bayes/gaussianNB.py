e1e8542300e037f5e345cfe72bc0a5e50ab3149d
Claudia Comito <c.comito@fz-juelich.de>
2020-01-23 11:14:35 +0100

    Formatting

--- heat/core/naive_bayes/gaussianNB.py
+++ heat/core/naive_bayes/gaussianNB.py
@@ -4,7 +4,7 @@ import heat as ht
 
 
 class GaussianNB():
-        """
+    """
     Gaussian Naive Bayes (GaussianNB)
     Can perform online updates to model parameters via :meth:`partial_fit`.
     For details on algorithm used to update feature means and variance online,
@@ -87,35 +87,35 @@ class GaussianNB():
 
     @staticmethod
     def _check_partial_fit_first_call(clf, classes=None):
-    """Private helper function for factorizing common classes param logic
-    Estimators that implement the ``partial_fit`` API need to be provided with
-    the list of possible classes at the first call to partial_fit.
-    Subsequent calls to partial_fit should check that ``classes`` is still
-    consistent with a previous value of ``clf.classes_`` when provided.
-    This function returns True if it detects that this was the first call to
-    ``partial_fit`` on ``clf``. In that case the ``classes_`` attribute is also
-    set on ``clf``.
-    """
-    if getattr(clf, 'classes_', None) is None and classes is None:
-        raise ValueError("classes must be passed on the first call "
-                         "to partial_fit.")
+        """Private helper function for factorizing common classes param logic
+        Estimators that implement the ``partial_fit`` API need to be provided with
+        the list of possible classes at the first call to partial_fit.
+        Subsequent calls to partial_fit should check that ``classes`` is still
+        consistent with a previous value of ``clf.classes_`` when provided.
+        This function returns True if it detects that this was the first call to
+        ``partial_fit`` on ``clf``. In that case the ``classes_`` attribute is also
+        set on ``clf``.
+        """
+        if getattr(clf, 'classes_', None) is None and classes is None:
+            raise ValueError("classes must be passed on the first call "
+                            "to partial_fit.")
 
-    elif classes is not None:
-        unique_labels = ht.sort(classes)[0]
-        if getattr(clf, 'classes_', None) is not None:
-            if not ht.equal(clf.classes_, unique_labels): 
-                raise ValueError(
-                    "`classes=%r` is not the same as on last call "
-                    "to partial_fit, was: %r" % (classes, clf.classes_))
+        elif classes is not None:
+            unique_labels = ht.sort(classes)[0]
+            if getattr(clf, 'classes_', None) is not None:
+                if not ht.equal(clf.classes_, unique_labels): 
+                    raise ValueError(
+                        "`classes=%r` is not the same as on last call "
+                        "to partial_fit, was: %r" % (classes, clf.classes_))
 
-        else:
-            # This is the first call to partial_fit
-            clf.classes_ = unique_labels
-            return True
+            else:
+                # This is the first call to partial_fit
+                clf.classes_ = unique_labels
+                return True
 
-    # classes is None and clf.classes_ has already previously been set:
-    # nothing to do
-    return False
+        # classes is None and clf.classes_ has already previously been set:
+        # nothing to do
+        return False
 
     @staticmethod
     def _update_mean_variance(n_past, mu, var, X, sample_weight=None):
