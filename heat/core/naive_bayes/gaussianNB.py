5459a19cc710cd170b4f5bb76289a9cbb58bc5ca
Claudia Comito <c.comito@fz-juelich.de>
2020-01-23 09:38:21 +0100

    Moved relevant _BaseNB methods to GaussianNB class, removed _BaseNB for now.

--- heat/core/naive_bayes/gaussianNB.py
+++ heat/core/naive_bayes/gaussianNB.py
@@ -1,81 +1,9 @@
 import sys
-
+import numpy as np 
 import heat as ht
 
-class _BaseNB(ClassifierMixin, BaseEstimator, metaclass=ABCMeta):
-    """Abstract base class for naive Bayes estimators"""
 
-    @abstractmethod
-    def _joint_log_likelihood(self, X):
-        """Compute the unnormalized posterior log probability of X
-        I.e. ``log P(c) + log P(x|c)`` for all rows x of X, as an array-like of
-        shape [n_classes, n_samples].
-        Input is passed to _joint_log_likelihood as-is by predict,
-        predict_proba and predict_log_proba.
-        """
-
-    def _check_X(self, X):
-        """To be overridden in subclasses with the actual checks."""
-        # Note that this is not marked @abstractmethod as long as the
-        # deprecated public alias sklearn.naive_bayes.BayesNB exists
-        # (until 0.24) to preserve backward compat for 3rd party projects
-        # with existing derived classes.
-        return X
-
-    def predict(self, X):
-        """
-        Perform classification on an array of test vectors X.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-        Returns
-        -------
-        C : ndarray of shape (n_samples,)
-            Predicted target values for X
-        """
-        check_is_fitted(self)
-        X = self._check_X(X)
-        jll = self._joint_log_likelihood(X)
-        return self.classes_[np.argmax(jll, axis=1)]
-
-    def predict_log_proba(self, X):
-        """
-        Return log-probability estimates for the test vector X.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-        Returns
-        -------
-        C : array-like of shape (n_samples, n_classes)
-            Returns the log-probability of the samples for each class in
-            the model. The columns correspond to the classes in sorted
-            order, as they appear in the attribute :term:`classes_`.
-        """
-        check_is_fitted(self)
-        X = self._check_X(X)
-        jll = self._joint_log_likelihood(X)
-        # normalize by P(x) = P(f_1, ..., f_n)
-        log_prob_x = logsumexp(jll, axis=1)
-        return jll - np.atleast_2d(log_prob_x).T
-
-    def predict_proba(self, X):
-        """
-        Return probability estimates for the test vector X.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-        Returns
-        -------
-        C : array-like of shape (n_samples, n_classes)
-            Returns the probability of the samples for each class in
-           
- the model. The columns correspond to the classes in sorted
-            order, as they appear in the attribute :term:`classes_`.
-        """
-        return np.exp(self.predict_log_proba(X))
-
-
-class GaussianNB(_BaseNB):
+class GaussianNB():
         """
     Gaussian Naive Bayes (GaussianNB)
     Can perform online updates to model parameters via :meth:`partial_fit`.
@@ -225,7 +153,7 @@ class GaussianNB(_BaseNB):
         # Compute (potentially weighted) mean and variance of new datapoints
         if sample_weight is not None:
             n_new = float(sample_weight.sum())
-            new_mu = ht.average(X, axis=0, weights=sample_weight) #TODO: check out Issue #351
+            new_mu = ht.average(X, axis=0, weights=sample_weight) #TODO:Issue #351
             new_var = ht.average((X - new_mu) ** 2, axis=0,
                                  weights=sample_weight)
         else:
@@ -421,4 +349,56 @@ class GaussianNB(_BaseNB):
         joint_log_likelihood = ht.array(joint_log_likelihood).T
         return joint_log_likelihood
 
+    def predict(self, X):
+        """
+        Perform classification on an array of test vectors X.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+        Returns
+        -------
+        C : ndarray of shape (n_samples,)
+            Predicted target values for X
+        """
+        check_is_fitted(self) #TODO
+        X = self._check_X(X) #TODO
+        jll = self._joint_log_likelihood(X)
+        return self.classes_[ht.argmax(jll, axis=1)]
+
+    def predict_log_proba(self, X):
+        """
+        Return log-probability estimates for the test vector X.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+        Returns
+        -------
+        C : array-like of shape (n_samples, n_classes)
+            Returns the log-probability of the samples for each class in
+            the model. The columns correspond to the classes in sorted
+            order, as they appear in the attribute :term:`classes_`.
+        """
+        check_is_fitted(self) #TODO
+        X = self._check_X(X) #TODO
+        jll = self._joint_log_likelihood(X)
+        # normalize by P(x) = P(f_1, ..., f_n)
+        #TODO np.log(np.sum(b*np.exp(a))) https://github.com/scipy/scipy/blob/master/scipy/special/_logsumexp.py 
+        log_prob_x = logsumexp(jll, axis=1) 
+        return jll - np.atleast_2d(log_prob_x).T #TODO np.atleast_2d or ensure that log_prob_x is at least a 2D tensor
+
+    def predict_proba(self, X):
+        """
+        Return probability estimates for the test vector X.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+        Returns
+        -------
+        C : array-like of shape (n_samples, n_classes)
+            Returns the probability of the samples for each class in
+           
+        the model. The columns correspond to the classes in sorted
+            order, as they appear in the attribute :term:`classes_`.
+        """
+        return ht.exp(self.predict_log_proba(X))
 
