868c01e8863513eeb4200e29b01edb5e7c0a1efe
simon <ubwdr@student.kit.edu>
2020-01-28 13:22:58 +0100

    creating local envs now

--- .github/workflows/gpu_test_run.sh
+++ .github/workflows/gpu_test_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-echo "GITHUB var ${GITHUB_REF}"
-echo "secret var ${secrets.TEST}"
-curl -H "Content-Type: application/json" -H "Authorization: token ${secrets.STATUS_TOKEN}" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${GITHUB_SHA}
+echo "GITHUB var ${SHA}"
+echo "secret var ${TEST}"
+curl -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${SHA}
 {
 source ~/.virtualenvs/heat/bin/activate
 module load Python/3.6.8
@@ -12,7 +12,7 @@ python -m pip install --user codecov pytest coverage
 python -m pip install --user -e .
 python -m coverage run --source=heat -m pytest
 python -m coverage report
-curl -H "Content-Type: application/json" -H "Authorization: token ${secrets.STATUS_TOKEN}" -X POST -d '{"state": "success", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${GITHUB_SHA}
+curl -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}" -X POST -d '{"state": "success", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${SHA}
 } || {
-curl -H "Content-Type: application/json" -H "Authorization: token ${secrets.STATUS_TOKEN}" -X POST -d '{"state": "failure", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${GITHUB_SHA}
+curl -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}" -X POST -d '{"state": "failure", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${SHA}
 }
