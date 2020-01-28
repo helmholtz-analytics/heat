701616e8009568258f24564e8aa3993a57948ca8
simon <ubwdr@student.kit.edu>
2020-01-28 15:37:27 +0100

    changed the way the final status is retrieved

--- .github/workflows/gpu_test_run.sh
+++ .github/workflows/gpu_test_run.sh
@@ -6,22 +6,23 @@
 #SBATCH --time=00:05:000
 
 curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
-{
-source ~/.virtualenvs/heat/bin/activate &&
-module load Python/3.6.8 &&
-module load GCC &&
-module load Intel &&
-module load ParaStationMPI &&
-DEVICE="gpu" &&
-export DEVICE &&
-python -m pip install --user codecov pytest coverage &&
-python -m pip install --user -e . &&
-srun -n $NUM_PROC python -m coverage run --source=heat --parallel-mode -m pytest &&
-python -m coverage combine &&
-python -m coverage report &&
-curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "success", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
-} || {
-# One of the commands failed, reporting failure
-curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "failure", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
-}
+
+STATUS="success"
+
+source ~/.virtualenvs/heat/bin/activate
+module load Python/3.6.8
+module load GCC
+module load Intel
+module load ParaStationMPI
+DEVICE="gpu"
+export DEVICE
+python -m pip install --user codecov pytest coverage
+python -m pip install --user -e .
+srun -n $NUM_PROC python -m coverage run --source=heat --parallel-mode -m pytest || STATUS="failure"
+python -m coverage combine
+python -m coverage report
+
+
+curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d "{\"state\": \"$STATUS\", \"description\": \"GPU Test Status\", \"context\": \"continuous-integration/gpu\"}" https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
+
 cd ~
