d6eaadadfb63d5cf5f6f31164966fcfd48c66e31
simon <ubwdr@student.kit.edu>
2020-01-28 15:43:01 +0100

    fixed failing test

--- heat/core/tests/test_arithmetics.py
+++ heat/core/tests/test_arithmetics.py
@@ -45,8 +45,6 @@ class TestArithmetics(unittest.TestCase):
         self.assertTrue(ht.equal(ht.add(self.a_tensor, self.a_vector), result))
         self.assertTrue(ht.equal(ht.add(self.a_tensor, self.an_int_scalar), result))
         self.assertTrue(ht.equal(ht.add(self.a_split_tensor, self.a_tensor), result))
-        print("DEVICE LOG", result._DNDarray__array.device)
-        self.fail()
 
         with self.assertRaises(ValueError):
             ht.add(self.a_tensor, self.another_vector)
