3e9cb4c4668d3a34b6b838cf82e32370d7dea11a
simon <ubwdr@student.kit.edu>
2020-01-28 16:47:50 +0100

    building test script in workflow to allow customization of the batch job settings

--- .github/workflows/gpu_test_template.sh
+++ .github/workflows/gpu_test_template.sh
@@ -1,9 +1,8 @@
-#!/bin/bash -x
-#SBATCH --account=haf
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=1
-#SBATCH --time=00:05:000
+# This script will be submitted as a batch job
+# `STATUS_TOKEN` and `CODECOV_TOKEN` are set in the `.bashrc` file on the machine
+# The first one is used to authorize on GitHub to submit to the status API
+# The second one is used for uploading the coverage report to codecov
+# `NUM_PROC` is set by the GitHub workflow file and can be customized
 
 curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
 
@@ -27,3 +26,4 @@ python -m codecov -t $CODECOV_TOKEN
 curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d "{\"state\": \"$STATUS\", \"description\": \"GPU Test Status\", \"context\": \"continuous-integration/gpu\"}" https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
 
 cd ~
+rm -rf tmp:
