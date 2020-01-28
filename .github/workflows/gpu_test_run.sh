00217cac33d126c37c57e074d670d5cc3d7b57a1
simon <ubwdr@student.kit.edu>
2020-01-28 13:40:27 +0100

    created batch script for gpu execution

--- .github/workflows/gpu_test_run.sh
+++ .github/workflows/gpu_test_run.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash
-echo "GITHUB var ${SHA}"
-echo "secret var ${TEST}"
+#!/bin/bash -x
+#SBATCH --account=haf
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:05:000
+#SBATCH --gres=gpu:2 --partition=gpus
+
+
+
 curl -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${SHA}
 {
 source ~/.virtualenvs/heat/bin/activate &&
@@ -8,11 +15,15 @@ module load Python/3.6.8 &&
 module load GCC &&
 module load Intel &&
 module load ParaStationMPI &&
+DEVICE="gpu" &&
+export DEVICE &&
 python -m pip install --user codecov pytest coverage &&
 python -m pip install --user -e . &&
-python -m coverage run --source=heat -m pytest &&
+srun -n 8 python -m coverage run --source=heat --parallel-mode -m pytest &&
+python -m coverage combine &&
 python -m coverage report &&
 curl -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}" -X POST -d '{"state": "success", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${SHA}
 } || {
+# One of the commands failed, reporting failure
 curl -H "Content-Type: application/json" -H "Authorization: token ${TOKEN}" -X POST -d '{"state": "failure", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/${SHA}
 }
