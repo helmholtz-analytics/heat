cd70d90fa9d9022bb5f6e6db12c57c0304723014
Simon <therealslimv@yahoo.de>
2020-02-02 10:48:14 +0100

    included the build matrix

--- .github/workflows/gpu_test_template.sh
+++ .github/workflows/gpu_test_template.sh
@@ -2,12 +2,11 @@
 # `STATUS_TOKEN` and `CODECOV_TOKEN` are set in the `.bashrc` file on the machine
 # The first one is used to authorize on GitHub to submit to the status API
 # The second one is used for uploading the coverage report to codecov
-# `NUM_PROC` is set by the GitHub workflow file and can be customized
+# `NUM_PROC` and `NUM_GPU` is set by the GitHub workflow file and can be customized
 
 curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
 
 STATUS="success"
-echo "GPUS $SLURM_GPUS"
 
 source ~/.virtualenvs/heat/bin/activate
 module load Python/3.6.8
@@ -19,7 +18,6 @@ export DEVICE
 python -m pip install --user codecov pytest coverage
 python -m pip install --user -e .
 srun -n $NUM_PROC -G=$NUM_GPU python -m coverage run --source=heat --parallel-mode -m pytest || STATUS="failure"
-srun -n $NUM_PROC -G=$NUM_GPU echo "GPUS $SLURM_GPUS"
 python -m coverage combine
 python -m coverage report
 python -m coverage xml
