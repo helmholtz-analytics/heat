ecfc9c1454bb1edb6eb708420919a3d25fb04556
Charlotte Debus <charlotte.debus@dlr.de>
2020-02-05 15:50:17 +0100

    Testing for CG Algorithm

--- heat/core/tests/test_linalg.py
+++ heat/core/tests/test_linalg.py
@@ -18,6 +18,28 @@ if os.environ.get("DEVICE") == "lgpu" and torch.cuda.is_available():
 
 
 class TestLinalg(unittest.TestCase):
+    def test_cg(self):
+        size = ht.communication.MPI_WORLD.size * 3
+        b = ht.arange(1, size + 1, dtype=ht.float32, split=0)
+        A = ht.manipulations.diag(b)
+        x0 = ht.random.rand(size, dtype=b.dtype, split=b.split)
+
+        x = ht.ones(b.shape, dtype=b.dtype, split=b.split)
+
+        res = ht.linalg.cg(A, b, x0)
+        self.assertTrue(ht.allclose(x, res, atol=1e-3))
+
+        b_np = np.arange(1, size + 1)
+        with self.assertRaises(TypeError):
+            res = ht.linalg.cg(A, b_np, x0)
+
+        with self.assertRaises(RuntimeError):
+            res = ht.linalg.cg(A, A, x0)
+        with self.assertRaises(RuntimeError):
+            res = ht.linalg.cg(b, b, x0)
+        with self.assertRaises(RuntimeError):
+            res = ht.linalg.cg(A, b, A)
+
     def test_dot(self):
         # ONLY TESTING CORRECTNESS! ALL CALLS IN DOT ARE PREVIOUSLY TESTED
         # cases to test:
