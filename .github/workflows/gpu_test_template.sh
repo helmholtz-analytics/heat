96e0cbbd00f189e8bfd99337527aa6f63694509b
Simon <therealslimv@yahoo.de>
2020-02-07 11:40:58 +0100

    changed the way values are exported

--- .github/workflows/gpu_test_template.sh
+++ .github/workflows/gpu_test_template.sh
@@ -2,7 +2,10 @@
 # `STATUS_TOKEN` and `CODECOV_TOKEN` are set in the `.bashrc` file on the machine
 # The first one is used to authorize on GitHub to submit to the status API
 # The second one is used for uploading the coverage report to codecov
-# `NUM_PROC` is set by the GitHub workflow file and can be customized
+# $SHA, $1 and $2 are by the GitHub workflow file and can be customized
+# $1 defines the number of processes that will be spawned by srun
+# $2 defines the number of GPUS that will be used in this job
+# $SHA is the SHA of the current commit to allow to post messages to the GitHub status API
 
 curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
 
@@ -13,12 +16,13 @@ module load Python/3.6.8
 module load GCC
 module load Intel
 module load ParaStationMPI
-echo "NUMBER GPUS $CUDA_VISIBLE_DEVICES"
-# This will make heat run the unittests on GPU
-export DEVICE="gpu"
 python -m pip install --user codecov pytest coverage
 python -m pip install --user -e .
-srun -n $NUM_PROC python -m coverage run --source=heat --parallel-mode -m pytest || STATUS="failure"
+export CUDA_VISIBLE_DEVICES=$2
+echo "NUMBER GPUS $CUDA_VISIBLE_DEVICES AND $1 AND $2"
+# This will make heat run the unittests on GPU
+export DEVICE="gpu"
+srun -n $1 python -m coverage run --source=heat --parallel-mode -m pytest || STATUS="failure"
 python -m coverage combine
 python -m coverage report
 python -m coverage xml
