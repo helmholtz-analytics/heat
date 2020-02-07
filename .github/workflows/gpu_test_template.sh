6f0ca7de4c2c5c4cc30a7be8fda914a74b8b4e3c
Simon <therealslimv@yahoo.de>
2020-02-07 11:12:11 +0100

    added CUDA_VISIBLE_DEVICES variable to define how many gpus are used

--- .github/workflows/gpu_test_template.sh
+++ .github/workflows/gpu_test_template.sh
@@ -2,7 +2,7 @@
 # `STATUS_TOKEN` and `CODECOV_TOKEN` are set in the `.bashrc` file on the machine
 # The first one is used to authorize on GitHub to submit to the status API
 # The second one is used for uploading the coverage report to codecov
-# `NUM_PROC` and `NUM_GPU` is set by the GitHub workflow file and can be customized
+# `NUM_PROC` is set by the GitHub workflow file and can be customized
 
 curl -H "Content-Type: application/json" -H "Authorization: token $STATUS_TOKEN" -X POST -d '{"state": "pending", "description": "GPU Test Status", "context": "continuous-integration/gpu"}' https://api.github.com/repos/helmholtz-analytics/heat/statuses/$SHA
 
@@ -17,7 +17,7 @@ DEVICE="gpu"
 export DEVICE
 python -m pip install --user codecov pytest coverage
 python -m pip install --user -e .
-srun -n $NUM_PROC -G=$NUM_GPU python -m coverage run --source=heat --parallel-mode -m pytest || STATUS="failure"
+srun -n $NUM_PROC python -m coverage run --source=heat --parallel-mode -m pytest || STATUS="failure"
 python -m coverage combine
 python -m coverage report
 python -m coverage xml
