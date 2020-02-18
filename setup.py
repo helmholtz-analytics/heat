f5a1c855064d8f78c0ca67dacc4b1e605f9ac65c
mtar <m.tarnawa@fz-juelich.de>
2020-02-18 15:19:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
     ],
-    install_requires=["mpi4py>=3.0.0", "numpy>=1.13.0", "torch>=1.3.0"],
+    install_requires=["mpi4py>=3.0.0", "numpy>=1.13.0", "torch>=1.3.1"],
     extras_require={
         "hdf5": ["h5py>=2.8.0"],
         "netcdf": ["netCDF4>=1.4.0,<=1.5.2"],
