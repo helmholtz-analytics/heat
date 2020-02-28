92d4a48c352bcbe6f1495fd8cd8e3d01d2a46e63
Claudia Comito <c.comito@fz-juelich.de>
2020-02-28 10:15:08 +0100

    Added tests for gnb.predict_proba()

--- heat/naive_bayes/tests/test_gaussiannb.py
+++ heat/naive_bayes/tests/test_gaussiannb.py
@@ -21,10 +21,22 @@ if os.environ.get("DEVICE") == "lgpu" and ht.torch.cuda.is_available():
 
 class TestGaussianNB(BasicTest):
     def test_fit_iris(self):
-        X_train = ht.load("heat/datasets/data/iris_X_train.csv", sep=";")
-        X_test = ht.load("heat/datasets/data/iris_X_test.csv", sep=";")
-        y_train = ht.load("heat/datasets/data/iris_y_train.csv", sep=";", dtype=ht.int32).squeeze()
-        y_test = ht.load("heat/datasets/data/iris_y_test.csv", sep=";", dtype=ht.int32).squeeze()
+        # load sklearn train/test sets and resulting probabilities
+        X_train = ht.load(
+            "heat/datasets/data/iris_X_train.csv", sep=";", dtype=ht.float32, device=ht_device
+        )
+        X_test = ht.load(
+            "heat/datasets/data/iris_X_test.csv", sep=";", dtype=ht.float32, device=ht_device
+        )
+        y_train = ht.load(
+            "heat/datasets/data/iris_y_train.csv", sep=";", dtype=ht.int32, device=ht_device
+        ).squeeze()
+        y_test = ht.load(
+            "heat/datasets/data/iris_y_test.csv", sep=";", dtype=ht.int32, device=ht_device
+        ).squeeze()
+        y_pred_proba_sklearn = ht.load(
+            "heat/datasets/data/iris_y_pred_proba.csv", sep=";", dtype=ht.float32, device=ht_device
+        )
 
         # test ht.GaussianNB
         from heat.naive_bayes import GaussianNB
@@ -32,34 +44,40 @@ class TestGaussianNB(BasicTest):
         gnb_heat = GaussianNB()
 
         # test GaussianNB locally
-        y_pred_local = gnb_heat.fit(X_train, y_train).predict(X_test)
+        local_fit = gnb_heat.fit(X_train, y_train)
+        y_pred_local = local_fit.predict(X_test)
+        y_pred_proba_local = local_fit.predict_proba(X_test)
         self.assertIsInstance(y_pred_local, ht.DNDarray)
         self.assertEqual((y_pred_local != y_test).sum(), ht.array(4))
+        self.assertTrue(ht.isclose(y_pred_proba_sklearn, y_pred_proba_local, atol=1e-1).all())
 
         # test GaussianNB when sample_weight is not None, sample_weight not distributed
         sample_weight = ht.ones((y_train.gshape[0]), dtype=ht.float32, split=None)
-        y_pred_local_weight = gnb_heat.fit(X_train, y_train, sample_weight=sample_weight).predict(
-            X_test
-        )
+        local_fit_weight = gnb_heat.fit(X_train, y_train, sample_weight=sample_weight)
+        y_pred_local_weight = local_fit_weight.predict(X_test)
+        y_pred_proba_local_weight = local_fit_weight.predict_proba(X_test)
         self.assertIsInstance(y_pred_local_weight, ht.DNDarray)
         self.assert_array_equal(y_pred_local_weight, y_pred_local.numpy())
+        self.assertTrue(ht.isclose(y_pred_proba_sklearn, y_pred_proba_local_weight).all())
 
         # test GaussianNB, data and labels distributed along split axis 0
-        # size = ht.MPI_WORLD.size
-        # if size in range(7):
-        X_train_split = ht.resplit(X_train, axis=0)
-        X_test_split = ht.resplit(X_test, axis=0)
-        y_train_split = ht.resplit(y_train, axis=0)
-        y_test_split = ht.resplit(y_test, axis=0)
-        y_pred_split = gnb_heat.fit(X_train_split, y_train_split).predict(X_test_split)
-        self.assert_array_equal(y_pred_split, y_pred_local.numpy())
-        self.assertEqual((y_pred_split != y_test_split).sum(), ht.array(4))
-        sample_weight_split = ht.ones(y_train_split.gshape[0], dtype=ht.float32, split=0)
-        y_pred_split_weight = gnb_heat.fit(
-            X_train_split, y_train_split, sample_weight=sample_weight_split
-        ).predict(X_test_split)
-        self.assertIsInstance(y_pred_split_weight, ht.DNDarray)
-        self.assert_array_equal(y_pred_split_weight, y_pred_split.numpy())
+        size = ht.MPI_WORLD.size
+        if size in range(7):
+            X_train_split = ht.resplit(X_train, axis=0)
+            X_test_split = ht.resplit(X_test, axis=0)
+            y_train_split = ht.resplit(y_train, axis=0)
+            y_test_split = ht.resplit(y_test, axis=0)
+            y_pred_split = gnb_heat.fit(X_train_split, y_train_split).predict(X_test_split)
+            self.assert_array_equal(y_pred_split, y_pred_local.numpy())
+            self.assertEqual((y_pred_split != y_test_split).sum(), ht.array(4))
+            sample_weight_split = ht.ones(y_train_split.gshape[0], dtype=ht.float32, split=0)
+            y_pred_split_weight = gnb_heat.fit(
+                X_train_split, y_train_split, sample_weight=sample_weight_split
+            ).predict(X_test_split)
+            self.assertIsInstance(y_pred_split_weight, ht.DNDarray)
+            self.assert_array_equal(y_pred_split_weight, y_pred_split.numpy())
+
+        # test GaussianNB internal functions
 
         # test exceptions
         X_torch = torch.ones(75, 4)
