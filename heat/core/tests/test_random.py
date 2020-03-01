96da6dfdbee455f3cffcfcf93dac38a2058361fa
Simon <therealslimv@yahoo.de>
2020-03-01 15:41:46 +0200

    removed the failing unit test

--- heat/core/tests/test_random.py
+++ heat/core/tests/test_random.py
@@ -21,9 +21,6 @@ if os.environ.get("DEVICE") == "lgpu" and torch.cuda.is_available():
 
 class TestRandom(unittest.TestCase):
     def test_rand(self):
-        print('GPU DEVICES', torch.cuda.device_count(), os.environ.get("CUDA_VISIBLE_DEVICES"))
-        self.fail()
-
         # int64 tests
 
         # Resetting seed works
