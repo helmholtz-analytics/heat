50c113a370b82fcae6c3309c88f14f95b2cdcb36
Claudia Comito <c.comito@fz-juelich.de>
2020-03-02 13:00:54 +0100

    Extending test coverage to gnb attributes

--- heat/naive_bayes/tests/test_gaussiannb.py
+++ heat/naive_bayes/tests/test_gaussiannb.py
@@ -23,32 +23,65 @@ class TestGaussianNB(BasicTest):
     def test_fit_iris(self):
         # load sklearn train/test sets and resulting probabilities
         X_train = ht.load(
-            "heat/datasets/data/iris_X_train.csv", sep=";", dtype=ht.float32, device=ht_device
+            "heat/datasets/data/iris_X_train.csv", sep=";", dtype=ht.float64, device=ht_device
         )
         X_test = ht.load(
-            "heat/datasets/data/iris_X_test.csv", sep=";", dtype=ht.float32, device=ht_device
+            "heat/datasets/data/iris_X_test.csv", sep=";", dtype=ht.float64, device=ht_device
         )
         y_train = ht.load(
-            "heat/datasets/data/iris_y_train.csv", sep=";", dtype=ht.int32, device=ht_device
+            "heat/datasets/data/iris_y_train.csv", sep=";", dtype=ht.int64, device=ht_device
         ).squeeze()
         y_test = ht.load(
-            "heat/datasets/data/iris_y_test.csv", sep=";", dtype=ht.int32, device=ht_device
+            "heat/datasets/data/iris_y_test.csv", sep=";", dtype=ht.int64, device=ht_device
         ).squeeze()
         y_pred_proba_sklearn = ht.load(
-            "heat/datasets/data/iris_y_pred_proba.csv", sep=";", dtype=ht.float32, device=ht_device
+            "heat/datasets/data/iris_y_pred_proba.csv", sep=";", dtype=ht.float64, device=ht_device
         )
 
         # test ht.GaussianNB
         from heat.naive_bayes import GaussianNB
 
         gnb_heat = GaussianNB()
+        self.assertEqual(gnb_heat.priors, None)
+        with self.assertRaises(AttributeError):
+            gnb_heat.classes_
+        with self.assertRaises(AttributeError):
+            gnb_heat.class_prior_
+        with self.assertRaises(AttributeError):
+            gnb_heat.epsilon_
 
-        # test GaussianNB locally
+        # test GaussianNB locally, no weights
         local_fit = gnb_heat.fit(X_train, y_train)
+        self.assert_array_equal(gnb_heat.classes_, np.array([0, 1, 2]))
+
         y_pred_local = local_fit.predict(X_test)
         y_pred_proba_local = local_fit.predict_proba(X_test)
+        sklearn_class_prior = np.array([0.38666667, 0.26666667, 0.34666667])
+        sklearn_epsilon = np.array([3.6399040000000003e-09])
+        sklearn_theta = ht.array(
+            [
+                [4.97586207, 3.35862069, 1.44827586, 0.23448276],
+                [5.935, 2.71, 4.185, 1.3],
+                [6.77692308, 3.09230769, 5.73461538, 2.10769231],
+            ],
+            dtype=X_train.dtype,
+            device=ht_device,
+        )
+        sklearn_sigma = ht.array(
+            [
+                [0.10321047, 0.13208086, 0.01629013, 0.00846612],
+                [0.256275, 0.0829, 0.255275, 0.046],
+                [0.38869823, 0.10147929, 0.31303255, 0.04763314],
+            ],
+            dtype=X_train.dtype,
+            device=ht_device,
+        )
         self.assertIsInstance(y_pred_local, ht.DNDarray)
         self.assertEqual((y_pred_local != y_test).sum(), ht.array(4))
+        self.assert_array_equal(gnb_heat.class_prior_, sklearn_class_prior)
+        self.assert_array_equal(gnb_heat.epsilon_, sklearn_epsilon)
+        self.assertTrue(ht.isclose(gnb_heat.theta_, sklearn_theta).all())
+        self.assertTrue(ht.isclose(gnb_heat.sigma_, sklearn_sigma, atol=1e-1).all())
         self.assertTrue(ht.isclose(y_pred_proba_sklearn, y_pred_proba_local, atol=1e-1).all())
 
         # test GaussianNB when sample_weight is not None, sample_weight not distributed
@@ -57,6 +90,10 @@ class TestGaussianNB(BasicTest):
         y_pred_local_weight = local_fit_weight.predict(X_test)
         y_pred_proba_local_weight = local_fit_weight.predict_proba(X_test)
         self.assertIsInstance(y_pred_local_weight, ht.DNDarray)
+        self.assert_array_equal(gnb_heat.class_prior_, sklearn_class_prior)
+        self.assert_array_equal(gnb_heat.epsilon_, sklearn_epsilon)
+        self.assertTrue(ht.isclose(gnb_heat.theta_, sklearn_theta).all())
+        self.assertTrue(ht.isclose(gnb_heat.sigma_, sklearn_sigma, atol=1e-1).all())
         self.assert_array_equal(y_pred_local_weight, y_pred_local.numpy())
         self.assertTrue(ht.isclose(y_pred_proba_sklearn, y_pred_proba_local_weight).all())
 
@@ -68,6 +105,10 @@ class TestGaussianNB(BasicTest):
             y_train_split = ht.resplit(y_train, axis=0)
             y_test_split = ht.resplit(y_test, axis=0)
             y_pred_split = gnb_heat.fit(X_train_split, y_train_split).predict(X_test_split)
+            self.assert_array_equal(gnb_heat.class_prior_, sklearn_class_prior)
+            self.assert_array_equal(gnb_heat.epsilon_, sklearn_epsilon)
+            self.assertTrue(ht.isclose(gnb_heat.theta_, sklearn_theta).all())
+            self.assertTrue(ht.isclose(gnb_heat.sigma_, sklearn_sigma, atol=1e-1).all())
             self.assert_array_equal(y_pred_split, y_pred_local.numpy())
             self.assertEqual((y_pred_split != y_test_split).sum(), ht.array(4))
             sample_weight_split = ht.ones(y_train_split.gshape[0], dtype=ht.float32, split=0)
@@ -76,8 +117,6 @@ class TestGaussianNB(BasicTest):
             ).predict(X_test_split)
             self.assertIsInstance(y_pred_split_weight, ht.DNDarray)
             self.assert_array_equal(y_pred_split_weight, y_pred_split.numpy())
-
-        # test GaussianNB internal functions
 
         # test exceptions
         X_torch = torch.ones(75, 4)
