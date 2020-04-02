1de3ef211e23afc82a58568cb6f13fcda3fa1709
Markus Goetz <markus.goetz@kit.edu>
2020-04-02 15:53:52 +0200

    Update communication.py

--- heat/core/communication.py
+++ heat/core/communication.py
@@ -13,11 +13,11 @@ if "openmpi" in os.environ.get("MPI_SUFFIX", "").lower():
     buffer = subprocess.check_output(["ompi_info", "--parsable", "--all"])
     CUDA_AWARE_MPI = b"mpi_built_with_cuda_support:value:true" in buffer
 # MVAPICH
-CUDA_AWARE_MPI = CUDA_AWARE_MPI and os.environ.get("MV2_USE_CUDA") == "1"
+CUDA_AWARE_MPI = CUDA_AWARE_MPI or os.environ.get("MV2_USE_CUDA") == "1"
 # MPICH
-CUDA_AWARE_MPI = CUDA_AWARE_MPI and os.environ.get("MPIR_CVAR_ENABLE_HCOLL") == "1"
+CUDA_AWARE_MPI = CUDA_AWARE_MPI or os.environ.get("MPIR_CVAR_ENABLE_HCOLL") == "1"
 # ParaStationMPI
-CUDA_AWARE_MPI = CUDA_AWARE_MPI and os.environ.get("PSP_CUDA") == "1"
+CUDA_AWARE_MPI = CUDA_AWARE_MPI or os.environ.get("PSP_CUDA") == "1"
 
 
 class Communication:
