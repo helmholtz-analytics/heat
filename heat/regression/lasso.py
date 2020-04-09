18ee89df63a04ebb360cc2e02cbcec1e24fea96c
kk <kkrajsek@gmx.de>
2020-04-10 00:02:23 +0200

    Revert "adapt label dimensions"
    This reverts commit eaf45ecc33436bdeb16170ecea32ad643af27f88.

--- heat/regression/lasso.py
+++ heat/regression/lasso.py
@@ -116,34 +116,24 @@ class Lasso(ht.RegressionMixin, ht.BaseEstimator):
         # Get number of model parameters
         _, n = X.shape
 
-        if len(y.shape)==1:
-            y = ht.expand_dims(y, axis=1) 
-       
         # Initialize model parameters
         theta = ht.zeros((n, 1), dtype=float, device=X.device)
-        
+
         # Looping until max number of iterations or convergence
         for i in range(self.max_iter):
-
             theta_old = theta.copy()
-            
+
             # Looping through each coordinate
             for j in range(n):
-
-                X_j = ht.array(X._DNDarray__array[:, j:j+1], is_split=0)
-               
-                y_est = (X @ theta)
-             
-                theta_j = theta._DNDarray__array[j].item() 
-
-                rho = (X_j * (y - y_est + theta_j * X_j)).mean()
+                y_est = (X @ theta)[:, 0]
+                rho = (X[:, j] * (y - y_est + theta[j].copy() * X[:, j])).mean()
 
                 # Intercept parameter theta[0] not be regularized
                 if j == 0:
                     theta[j] = rho
                 else:
                     theta[j] = self.soft_threshold(rho)
-        
+
             diff = self.rmse(theta, theta_old)
             if self.tol is not None:
                 if diff < self.tol:
@@ -152,8 +142,7 @@ class Lasso(ht.RegressionMixin, ht.BaseEstimator):
                     break
 
         self.n_iter = i + 1
-        self.__theta = theta 
-
+        self.__theta = theta
 
     def predict(self, X):
         """
@@ -164,4 +153,4 @@ class Lasso(ht.RegressionMixin, ht.BaseEstimator):
         X : HeAT tensor, shape (n_samples, n_features)
             Input data.
         """
-        return (X @ self.__theta)
+        return (X @ self.__theta)[:, 0]
