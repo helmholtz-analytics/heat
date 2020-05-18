7530c3edfe6f56f227b3c4d84e29fc1856e278ef
Claudia Comito <c.comito@fz-juelich.de>
2020-05-18 12:52:26 +0200

    Dtype checks

--- heat/core/statistics.py
+++ heat/core/statistics.py
@@ -1264,6 +1264,16 @@ def percentile(x, q, axis=None, interpolation="linear", keepdim=False):
         indices_map = []
         perc_map = []
         perc_map_start = perc_map_end = 0
+
+        # calculate dimension along which local percentile chunks will be joined
+        if axis == split:
+            join = 0
+        elif axis > split:
+            join = split
+        elif axis < split:
+            join = split - 1
+
+        # calculate percentage map across ranks
         for r in range(size):
             if split == axis:
                 offset, _, chunk = x.comm.chunk(x.gshape, split, rank=r, w_size=size)
@@ -1273,7 +1283,7 @@ def percentile(x, q, axis=None, interpolation="linear", keepdim=False):
                 offset = chunk_start = 0
                 chunk_stop = length
                 # fictitious chunks of percentile - will be filled with local_p below
-                _, _, fchunk = x.comm.chunk(output_shape, split, rank=r, w_size=size)
+                _, _, fchunk = x.comm.chunk(output_shape, join, rank=r, w_size=size)
             # map of indices on all ranks
             indices_map.append(indices[(indices < chunk_stop) & (indices >= chunk_start)] - offset)
             indices_on_rank = indices_map[r].numel()
@@ -1283,38 +1293,32 @@ def percentile(x, q, axis=None, interpolation="linear", keepdim=False):
                     perc_map.append([slice(perc_map_start, perc_map_end, None), r])
                     perc_map_start = perc_map_end
                 else:
-                    perc_map.append([slice(fchunk[split].start, fchunk[split].stop, None), r])
+                    perc_map.append([slice(fchunk[join].start, fchunk[join].stop, None), r])
             offset_map.append(offset)
     else:
         offset_map = [0]
         chunk_stop = length
 
     # sort data
-    data = manipulations.sort(x, axis=axis)[0].astype(types.canonical_heat_type(float))
+    data = manipulations.sort(x, axis=axis)[0].astype(types.promote_types(x.dtype, q.dtype))
 
+    # allocate memory on all nodes
+    percentile = factories.empty(output_shape, dtype=data.dtype, split=None, device=x.device)
     if x.comm.is_distributed() and split is not None:
-        # allocate memory on all nodes
-        percentile = factories.empty(
-            output_shape, dtype=types.canonical_heat_type(q.dtype), split=None, device=x.device
-        )
         if axis == split:
             data.get_halo(1)
         # fill out percentile
         for i in range(len(perc_map)):
             indices = indices_map[rank]
             perc_slice = percentile.numdims * (slice(None, None, None),)
-            if q.numel() > 1:
-                if axis == split:
-                    new_split = 0
-                elif axis > split:
-                    new_split = split + 1
-                else:
-                    new_split = split
-                perc_slice = (
-                    perc_slice[:new_split] + (perc_map[i][0],) + perc_slice[new_split + 1 :]
-                )
-            print("DEBUGGING: rank, perc_slice = ", i, rank, perc_slice)
-            local_p = factories.zeros(percentile[perc_slice].shape, comm=x.comm)
+            if q.numel() == 1 and axis == split:
+                pass
+            else:
+                perc_slice = perc_slice[:join] + (perc_map[i][0],) + perc_slice[join + 1 :]
+            print("DEBUGGING: rank, perc_slice = ", i, rank, perc_slice, perc_map)
+            local_p = factories.zeros(
+                percentile[perc_slice].shape, dtype=percentile.dtype, comm=x.comm
+            )
             print("DEBUGGING: rank, local_p.shape 1 = ", i, rank, local_p.shape)
             proc = perc_map[i][1]
             if indices.numel() and rank == proc:
@@ -1324,8 +1328,11 @@ def percentile(x, q, axis=None, interpolation="linear", keepdim=False):
             print("DEBUGGING: rank, local_p.shape 3 = ", i, rank, local_p.shape)
             percentile[perc_slice] = local_p
     else:
-        local_p = local_percentile(data, axis, indices)
-        percentile = factories.array(local_p)  # TODO: split, device, dtype etc.
+        local_p = factories.array(local_percentile(data, axis, indices))
+        percentile = local_p  # TODO: split, device, dtype etc.
+
+    if percentile.shape[0] == 1:
+        percentile = manipulations.squeeze(percentile, axis=0)
 
     return percentile
 
