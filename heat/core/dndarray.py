d54d1efa03ccb0a90b3d5f8d18a7df2295ff6dd1
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2020-06-04 10:02:43 +0200

    replace method calls

--- heat/core/dndarray.py
+++ heat/core/dndarray.py
@@ -1447,7 +1447,7 @@ class DNDarray:
             if isinstance(key, int):
                 gout = [0] * (len(self.gshape) - 1)
                 if key < 0:
-                    key += self.numdims
+                    key += self.ndim
                 # handle the reduction of the split to accommodate for the reduced dimension
                 if self.split >= len(gout):
                     new_split = len(gout) - 1 if len(gout) - 1 > 0 else 0
@@ -2604,8 +2604,8 @@ class DNDarray:
         None
         """
         rank = self.comm.rank
-        send_slice = [slice(None)] * self.numdims
-        keep_slice = [slice(None)] * self.numdims
+        send_slice = [slice(None)] * self.ndim
+        keep_slice = [slice(None)] * self.ndim
         if rank == snd_pr:
             if snd_pr < rcv_pr:  # data passed to a higher rank (off the bottom)
                 send_slice[self.split] = slice(
@@ -2737,7 +2737,7 @@ class DNDarray:
             # need to get where the tiles are on the new one first
             # rpr is the destination
             new_locs = torch.where(new_tile_locs == rpr)
-            new_locs = torch.stack([new_locs[i] for i in range(self.numdims)], dim=1)
+            new_locs = torch.stack([new_locs[i] for i in range(self.ndim)], dim=1)
             for i in range(new_locs.shape[0]):
                 key = tuple(new_locs[i].tolist())
                 spr = tiles.tile_locations[key].item()
@@ -2754,7 +2754,7 @@ class DNDarray:
                     )
                     w = self.comm.Irecv(buf=buf, source=spr, tag=spr)
                     rcv[key] = [w, buf]
-        dims = list(range(self.numdims))
+        dims = list(range(self.ndim))
         del dims[axis]
         sorted_keys = sorted(rcv.keys())
         # todo: reduce the problem to 1D cats for each dimension, then work up
@@ -3107,7 +3107,7 @@ class DNDarray:
 
             if isinstance(key, int):
                 if key < 0:
-                    key += self.numdims
+                    key += self.ndim
                 if self.split == 0:
                     if key in range(chunk_start, chunk_end):
                         self.__setter(key - chunk_start, value)
