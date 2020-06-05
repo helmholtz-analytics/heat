089beaa8386ffe6011ab297061b906e087cdfb53
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2020-06-05 10:47:10 +0200

    add tests

--- heat/core/manipulations.py
+++ heat/core/manipulations.py
@@ -9,7 +9,7 @@ from . import factories
 from . import stride_tricks
 from . import tiling
 from . import types
-from. import linalg
+from . import linalg
 
 
 __all__ = [
@@ -938,7 +938,7 @@ def reshape(a, shape, axis=None):
     return factories.array(data, dtype=a.dtype, is_split=axis, device=a.device, comm=a.comm)
 
 
-def rot90(m, k=1, axes=(0,1)):
+def rot90(m, k=1, axes=(0, 1)):
     """
     Rotate an array by 90 degrees in the plane specified by axes.
     Rotation direction is from the first towards the second axis.
@@ -948,10 +948,10 @@ def rot90(m, k=1, axes=(0,1)):
         Array of two or more dimensions.
     k : integer
         Number of times the array is rotated by 90 degrees.
-    axes: (2,) integer
+    axes: (2,) int list or tuple
         The array is rotated in the plane defined by the axes.
         Axes must be different.
-    
+
     Returns
     -------
     y : DNDarray
@@ -961,7 +961,9 @@ def rot90(m, k=1, axes=(0,1)):
     -----
     rot90(m, k=1, axes=(1,0)) is the reverse of rot90(m, k=1, axes=(0,1))
     rot90(m, k=1, axes=(1,0)) is equivalent to rot90(m, k=-1, axes=(0,1))
-    
+
+    May change the split axis on distributed tensors
+
     Examples
     --------
     >>> m = ht.array([[1,2],[3,4]], dtype=ht.int)
@@ -991,10 +993,13 @@ def rot90(m, k=1, axes=(0,1)):
     if axes[0] == axes[1] or np.absolute(axes[0] - axes[1]) == m.ndim:
         raise ValueError("Axes must be different.")
 
-    if (axes[0] >= m.ndim or axes[0] < -m.ndim
-        or axes[1] >= m.ndim or axes[1] < -m.ndim):
-        raise ValueError("Axes={} out of range for array of ndim={}."
-            .format(axes, m.ndim))
+    if axes[0] >= m.ndim or axes[0] < -m.ndim or axes[1] >= m.ndim or axes[1] < -m.ndim:
+        raise ValueError("Axes={} out of range for array of ndim={}.".format(axes, m.ndim))
+
+    if m.split is None:
+        return factories.array(
+            torch.rot90(m._DNDarray__array, k, axes), dtype=m.dtype, device=m.device, comm=m.comm
+        )
 
     k %= 4
 
@@ -1004,11 +1009,10 @@ def rot90(m, k=1, axes=(0,1)):
         return flip(flip(m, axes[0]), axes[1])
 
     axes_list = np.arange(0, m.ndim).tolist()
-    (axes_list[axes[0]], axes_list[axes[1]]) = (axes_list[axes[1]],
-                                                axes_list[axes[0]])
+    (axes_list[axes[0]], axes_list[axes[1]]) = (axes_list[axes[1]], axes_list[axes[0]])
 
     if k == 1:
-        return linalg.transpose(flip(m,axes[1]), axes_list)
+        return linalg.transpose(flip(m, axes[1]), axes_list)
     else:
         # k == 3
         return flip(linalg.transpose(m, axes_list), axes[1])
