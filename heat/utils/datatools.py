4e73b3370081273fa8fa83433afb1e866933316c
coquelin77 <daniel.coquelin@gmail.com>
2020-06-23 16:57:23 +0200

    updated tfrecord loader, more changes coming from test system later

--- heat/utils/datatools.py
+++ heat/utils/datatools.py
@@ -1,8 +1,7 @@
-from ..core import types
-from ..core.communication import MPI_WORLD
-
 import h5py
 import numpy as np
+import torch
+import tarfile
 import os
 
 __all__ = ["merge_files_imagenet_tfrecord"]
@@ -46,67 +45,36 @@ def merge_files_imagenet_tfrecord(folder_name, output_name=None):
                 layer. The label ranges from [1, 1000] where 0 is not used. Note this is
                 always identical to the image label."""
 
-    # create output file
-    if isinstance(output_name, str) and output_name[:-3] != ".h5":
-        output_name += ".h5"
-    elif not isinstance(output_name, str) and output_name is not None:
-        raise TypeError("output_name must be a string")
-    else:
-        output_name = "imagenet_merged.h5"
-
     # todo: get the number of files, either from the filenames or the contents of the folder
     if folder_name is not None:
-        train_names = [f for f in os.listdir(folder_name) if f.startswith("train")]
-        val_names = [f for f in os.listdir(folder_name) if f.startswith("val")]
+        train_names = [folder_name + f for f in os.listdir(folder_name) if f.startswith("train")]
+        val_names = [folder_name + f for f in os.listdir(folder_name) if f.startswith("val")]
+    train_names.sort()
+    val_names.sort()
     num_train = len(train_names)
     num_val = len(val_names)
     # todo: split the files between the available processes
-    rank = MPI_WORLD.rank
 
-    def _find_output_name_and_stsp(num_names, output_name=output_name):
-        if MPI_WORLD.size > 1:
-            files_per_pr = num_names // MPI_WORLD.size
-            rem = num_names % MPI_WORLD.size
-            start = MPI_WORLD.rank * files_per_pr
-            stop = (MPI_WORLD.rank + 1) * files_per_pr + 1
-            if rank == MPI_WORLD.size - 1:
-                # number of ranks is likely small so this is okay
-                start += rem
-            stop += rem
-            # create multiple h5 files for each process
-            output_name_lcl = str(rank) + output_name if rank > 0 else output_name
-        else:
-            start = 0
-            stop = num_train + 1
-            output_name_lcl = output_name
+    def _find_output_name_and_stsp(num_names):
+        start = 0
+        stop = num_names + 1
+        output_name_lcl = output_name + "imagenet_merged.h5"
         return start, stop, output_name_lcl
 
     train_start, train_stop, output_name_lcl_train = _find_output_name_and_stsp(num_train)
     val_start, val_stop, output_name_lcl_val = _find_output_name_and_stsp(num_val)
-    output_name_lcl_val = output_name_lcl_val[:-3] + "_val.h5"
+    output_name_lcl_val = output_name_lcl_val[:-3] + "_validation.h5"
 
     # create the output files
     train_lcl_file = h5py.File(output_name_lcl_train, "w")
-    train_lcl_file.create_dataset(
-        "images", (1282048 // MPI_WORLD.size, 179729), maxshape=(None, 179729), dtype="ui8"
-    )
-    train_lcl_file.create_dataset(
-        "metadata", (1282048 // MPI_WORLD.size, 9), maxshape=(None, 9), dtype="f64"
-    )
-    train_lcl_file.create_dataset(
-        "file_info", (1282048 // MPI_WORLD.size, 4), maxshape=(None, 4), dtype="s"
-    )
+    train_lcl_file.create_dataset("images", (1282048, 1), maxshape=(None, 179729), dtype="i8")
+    train_lcl_file.create_dataset("metadata", (1282048, 9), maxshape=(None, 9))
+    train_lcl_file.create_dataset("file_info", (1282048, 4), maxshape=(None, 4), dtype="S10")
 
     val_lcl_file = h5py.File(output_name_lcl_val, "w")
-    val_lcl_file.create_dataset(
-        "images", (50000 // MPI_WORLD.size, 179729), maxshape=(None, 179729), dtype="ui8"
-    )
-    val_lcl_file.create_dataset(
-        "metadata", (50000 // MPI_WORLD.size, 9), maxshape=(None, 9), dtype="f64"
-    )
-    val_lcl_file.create_dataset(
-        "file_info", (50000 // MPI_WORLD.size, 4), maxshape=(None, 4), dtype="s"
-    )
+    val_lcl_file.create_dataset("images", (50000, 1), maxshape=(None, 179729), dtype="i8")
+    val_lcl_file.create_dataset("metadata", (50000, 9), maxshape=(None, 9))
+    val_lcl_file.create_dataset("file_info", (50000, 4), maxshape=(None, 4), dtype="S10")
 
     def __single_file_load(src):
         import tensorflow as tf
@@ -117,14 +85,11 @@ def merge_files_imagenet_tfrecord(folder_name, output_name=None):
         imgs = []
         img_meta = [[] for _ in range(9)]
         file_arr = [[] for _ in range(4)]
-        for c, _ in enumerate(dataset):
-            raw_exmaple = next(iter(dataset))
+        for raw_exmaple in iter(dataset):
             parsed = tf.train.Example.FromString(raw_exmaple.numpy())
-            imgs.append(
-                tf.io.decode_raw(
-                    parsed.features.feature["image/encoded"].bytes_list.value[0], tf.uint8
-                ).numpy()
-            )
+            img_str = parsed.features.feature["image/encoded"].bytes_list.value[0]
+            img = tf.image.decode_jpeg(img_str, channels=3).numpy()
+            imgs.append(img)
             img_meta[0].append(
                 tf.cast(
                     parsed.features.feature["image/height"].int64_list.value[0], tf.float32
@@ -141,33 +106,24 @@ def merge_files_imagenet_tfrecord(folder_name, output_name=None):
                 ).numpy()
             )
             img_meta[3].append(parsed.features.feature["image/class/label"].int64_list.value[0])
-            img_meta[4].append(
-                tf.cast(
-                    parsed.features.feature["image/object/bbox/xmin"].float_list.value[0],
-                    tf.float32,
-                ).numpy()
-            )
-            img_meta[5].append(
-                tf.cast(
-                    parsed.features.feature["image/object/bbox/xmax"].float_list.value[0],
-                    tf.float32,
-                ).numpy()
-            )
-            img_meta[6].append(
-                tf.cast(
-                    parsed.features.feature["image/object/bbox/ymin"].float_list.value[0],
-                    tf.float32,
-                ).numpy()
-            )
-            img_meta[7].append(
-                tf.cast(
-                    parsed.features.feature["image/object/bbox/ymax"].float_list.value[0],
-                    tf.float32,
-                ).numpy()
-            )
-            img_meta[8].append(
-                parsed.features.feature["image/object/bbox/label"].int64_list.value[0]
-            )
+            try:
+                bbxmin = parsed.features.feature["image/object/bbox/xmin"].float_list.value[0]
+                bbxmax = parsed.features.feature["image/object/bbox/xmax"].float_list.value[0]
+                bbymin = parsed.features.feature["image/object/bbox/ymin"].float_list.value[0]
+                bbymax = parsed.features.feature["image/object/bbox/ymax"].float_list.value[0]
+                bblabel = parsed.features.feature["image/object/bbox/label"].int64_list.value[0]
+            except IndexError:
+                bbxmin = 0.0
+                bbxmax = img_meta[1][-1]
+                bbymin = 0.0
+                bbymax = img_meta[0][-1]
+                bblabel = -1
+
+            img_meta[4].append(np.float(bbxmin))
+            img_meta[5].append(np.float(bbxmax))
+            img_meta[6].append(np.float(bbymin))
+            img_meta[7].append(np.float(bbymax))
+            img_meta[8].append(bblabel)
 
             file_arr[0].append(parsed.features.feature["image/format"].bytes_list.value[0])
             file_arr[1].append(parsed.features.feature["image/filename"].bytes_list.value[0])
@@ -183,7 +139,7 @@ def merge_files_imagenet_tfrecord(folder_name, output_name=None):
         return out_images, out_img_meta, out_file_array
 
     def __write_datasets(img_outl, img_metal, file_arrl, past_sizel, file):
-        file["images"].resize((past_sizel + img_outl.shape[0], 179729))
+        file["images"].resize((past_sizel + img_outl.shape[0], 1))
         file["images"][past_sizel : img_outl.shape[0]] = img_outl
         file["metadata"].resize((past_sizel + img_metal.shape[0], 9))
         file["metadata"][past_sizel : img_metal.shape[0]] = img_metal
@@ -198,9 +154,9 @@ def merge_files_imagenet_tfrecord(folder_name, output_name=None):
             # this is where the data is created for
             imgs, img_metaf, file_arrf = __single_file_load(f)
             # create a larger ndarray with the results
-            img_out = np.vstack(img_out, imgs) if img_out is not None else imgs
-            img_meta = np.vstack(img_meta, img_metaf) if img_meta is not None else img_metaf
-            file_arr = np.vstack(file_arr, file_arrf) if file_arr is not None else file_arrf
+            img_out = np.vstack((img_out, imgs)) if img_out is not None else imgs
+            img_meta = np.vstack((img_meta, img_metaf)) if img_meta is not None else img_metaf
+            file_arr = np.vstack((file_arr, file_arrf)) if file_arr is not None else file_arrf
             # when 16 files are read then write to the lcl file
             i += 1
             if i % 16 == 15:
@@ -213,52 +169,6 @@ def merge_files_imagenet_tfrecord(folder_name, output_name=None):
 
     __load_multiple_files(train_names, train_start, train_stop, train_lcl_file)
     __load_multiple_files(val_names, val_start, val_stop, val_lcl_file)
-
-    if MPI_WORLD.size > 1 and rank == 0:
-        # todo: merge the files together at the end
-
-        for r in range(1, MPI_WORLD.size):
-            rank_lcl_name = str(r) + output_name_lcl_train
-            # get the size of the data, load half, write, load other half, write
-            r_file = h5py.File(rank_lcl_name, "r")
-            sz = r_file["images"].shape[0] // 2
-            current_end = train_lcl_file["images"].shape[0]
-            __write_datasets(
-                r_file["images"][:sz],
-                r_file["metadata"][:sz],
-                r_file["file_info"][:sz],
-                current_end,
-                train_lcl_file,
-            )
-            current_end += sz
-            __write_datasets(
-                r_file["images"][sz:],
-                r_file["metadata"][sz:],
-                r_file["file_info"][sz:],
-                current_end,
-                train_lcl_file,
-            )
-
-            rank_lcl_name = str(r) + output_name_lcl_val
-            # get the size of the data, load half, write, load other half, write
-            r_file = h5py.File(rank_lcl_name, "r")
-            sz = r_file["images"].shape[0] // 2
-            current_end = train_lcl_file["images"].shape[0]
-            __write_datasets(
-                r_file["images"][:sz],
-                r_file["metadata"][:sz],
-                r_file["file_info"][:sz],
-                current_end,
-                val_lcl_file,
-            )
-            current_end += sz
-            __write_datasets(
-                r_file["images"][sz:],
-                r_file["metadata"][sz:],
-                r_file["file_info"][sz:],
-                current_end,
-                val_lcl_file,
-            )
 
     #  add the column names to the datasets
     # labels:
