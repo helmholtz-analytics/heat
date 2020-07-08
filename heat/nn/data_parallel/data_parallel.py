0eb4ca50e0a1dcc91fbbefb3dc1adf1ed205e52c
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2020-07-08 17:06:22 +0200

    Merged docs with recent fixes

--- heat/nn/data_parallel/data_parallel.py
+++ heat/nn/data_parallel/data_parallel.py
@@ -48,8 +48,8 @@ class DataParallel(tnn.Module):
         the local module
     comm : heat.MPICommunicator
         HeAT communicator to use
-    nonblocking : bool
-        if true, use non-blocking communications, else blocking communications will be used for the parameter updates
+    optimizer : torch.optim.Optimizer (optional)
+        Optimizer used for parameter updates. If given, synchronization is non-blocking, else blocking.
     """
 
     def __init__(self, module: torch.nn.Module, comm: ht.MPICommunication, optimizer=None):
@@ -125,7 +125,45 @@ class DataParallel(tnn.Module):
         # remove forward hooks (dynamic computation graph)
         for hook_handle in self.fwd_hook_handles:
             hook_handle.remove()
+        self.fwd_hook_handles.clear()
         return ret
+
+    def async_update(self, param_slice=None, layer_names=None):
+        """
+        Update parameters asynchronously via wait handles.
+
+        Parameters
+        ----------
+        param_slice : slice (optional)
+            Slice object for creating a view onto optimizer's params list. If None, the whole params list is taken.
+        layer_names : list(str) (optional)
+            List of layer names, whose parameters are to be updated. Has to match param_slice. If None, all layers are
+            taken.
+        """
+        # perform update on the whole model
+        if param_slice is None:
+            param_slice = slice(len(self.params_ref))
+        if layer_names is None:
+            layer_names = list(self.wait_handles.keys())
+
+        # update params that are visible for the optimizer
+        self.optimizer.param_groups[0]["params"] = self.params_ref[param_slice]
+
+        # iterate over layers
+        for layer_name in layer_names:
+            # iterate over layer's parameters/associated wait handles
+            for _, param_name, wait_handle in self.wait_handles[layer_name]:
+                # get internal index of selected parameter
+                param_idx = self.param_indices[param_name]
+                # synchronize, get parameter's global gradient
+                wait_handle.wait()
+                # check if shapes are matching
+                if self.params_ref[param_idx].grad.data.shape != wait_handle.tensor.shape:
+                    raise ValueError("Shapes must be equal.")
+                # set parameter's global gradient
+                self.params_ref[param_idx].grad.data = wait_handle.tensor
+        # perform actual parameter update
+        self.optimizer.step()
 
     def blocking_hook(self, grad_loc: torch.Tensor) -> torch.Tensor:
         """
@@ -140,12 +178,12 @@ class DataParallel(tnn.Module):
         ----------
         - (cf. https://pytorch.org/docs/stable/tensors.html#torch.Tensor.register_hook).
         """
-        grad_loc_cpy = grad_loc.clone()
+        # grad_loc_cpy = grad_loc
         # average local gradients
-        grad_loc_cpy *= 1 / float(self.comm.size)
+        grad_loc *= 1 / float(self.comm.size)
         # perform MPI Allreduce to compute global gradient
-        self.comm.Allreduce(ht.MPI.IN_PLACE, grad_loc_cpy, ht.MPI.SUM)
-        return grad_loc_cpy
+        self.comm.Allreduce(ht.MPI.IN_PLACE, grad_loc, ht.MPI.SUM)
+        return grad_loc
 
     def blocking_grad_update(self, learning_rate: float):
         """
@@ -164,7 +202,7 @@ class DataParallel(tnn.Module):
             f.grad.data = c
             f.data.sub_(f.grad.data * learning_rate)
 
-    def nonblocking_hook(self, layer_name: str):
+    def nonblocking_hook(self, layer_name: str, param_name: str):
         """
         Add a nonblocking hook to send and receive the averaged parameters after the backwards step
 
@@ -172,24 +210,41 @@ class DataParallel(tnn.Module):
         ----------
         layer_name : str
             name of the layer
+        param_name : str
+            name of the parameter
         """
         # hook function for blocking gradient data exchange
         def _hook(grad_loc):
-            # Pytorch Doc says, :attr:`grad` may not be modified itself, so it has to be cloned
-            # (cf. https://pytorch.org/docs/stable/tensors.html#torch.Tensor.register_hook).
-            # Seems to be true, since otherwise a Runtime Error is thrown when working on it
-            grad_loc_cpy = grad_loc.clone()
             # counterbalance local gradient averaging
-            grad_loc_cpy *= 1 / float(self.comm.size)
+            grad_loc *= 1 / float(self.comm.size)
             # perform MPI IAllreduce to compute global gradient, returns wait handle
-            wait_handle = self.comm.Iallreduce(ht.MPI.IN_PLACE, grad_loc_cpy, ht.MPI.SUM)
+            wait_handle = self.comm.Iallreduce(ht.MPI.IN_PLACE, grad_loc, ht.MPI.SUM)
             # if wait handle dict does not contain the layer, add it -> automatically tracks reversed layer order
             if layer_name not in self.wait_handles:
                 self.wait_handles[layer_name] = list()
             # get size of flattened tensor
             size1D = functools.reduce(operator.mul, grad_loc.shape, 1)
             # assign wait handle to its layer, layer-internal sorting by size
-            bisect.insort(self.wait_handles[layer_name], (size1D, wait_handle))
+            bisect.insort(self.wait_handles[layer_name], (size1D, param_name, wait_handle))
             return grad_loc
+
+        return _hook
+
+    def forward_hook(self, layer_name: str):
+        """
+        Add a forward hook to update parameters during the forward step
+
+        Parameters
+        ----------
+        layer_name : str
+            name of the layer
+        """
+        # hook function for non-blocking parameter update
+        def _hook(_, input_):
+            # update parameters of given layer
+            param_slice = self.param_slices[layer_name]
+            self.async_update(param_slice, [layer_name])
+
+            return input_
 
         return _hook
