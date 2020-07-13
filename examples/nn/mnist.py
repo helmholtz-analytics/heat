ee39caee7a2476822e0c17b831cc2586d4404b58
coquelin77 <daniel.coquelin@gmail.com>
2020-07-13 16:10:34 +0200

    uncommented print functions from nn/mnist example

--- examples/nn/mnist.py
+++ examples/nn/mnist.py
@@ -59,15 +59,15 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss.backward()
         model.update()
         if batch_idx % args.log_interval == 0:
-            # print(
-            #     "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-            #         epoch,
-            #         batch_idx * len(data),
-            #         len(train_loader.dataset),
-            #         100.0 * batch_idx / len(train_loader),
-            #         loss.item(),
-            #     )
-            # )
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
             if args.dry_run:
                 break
 
@@ -86,11 +86,11 @@ def test(model, device, test_loader):
 
     test_loss /= len(test_loader.dataset)
 
-    # print(
-    #     "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-    #         test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
-    #     )
-    # )
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
 
 
 def main():
