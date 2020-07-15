451edbd4ea9c30fa75116b2108c465ff2765c400
Luca Spataro <luca@spataro.de>
2020-07-15 17:21:53 +0200

    add different data type test

--- heat/core/tests/test_convolve.py
+++ heat/core/tests/test_convolve.py
@@ -15,14 +15,14 @@ class TestConvolve(TestCase):
     def test_convolve1D(self):
         full_odd = ht.array(
             [0, 1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 29, 15]
-        ).astype(ht.float)
+        ).astype(ht.int)
         full_even = ht.array(
             [0, 1, 3, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 42, 29, 15]
-        ).astype(ht.float)
+        ).astype(ht.int)
 
-        signal = ht.arange(0, 16, split=0).astype(ht.float)
-        kernal_odd = ht.ones(3).astype(ht.float)
-        kernal_even = ht.ones(4).astype(ht.float)
+        signal = ht.arange(0, 16, split=0).astype(ht.int)
+        kernal_odd = ht.ones(3).astype(ht.int)
+        kernal_even = ht.ones(4).astype(ht.int)
 
         with self.assertRaises(TypeError):
             ht.convolve1D(signal, [0, 1, 2, 3])
@@ -37,14 +37,15 @@ class TestConvolve(TestCase):
         with self.assertRaises(ValueError):
             ht.convolve1D(kernal_even, full_even)
         with self.assertRaises(TypeError):
-            k = ht.ones(3).astype(ht.int)
+            k = ht.ones(3).astype(ht.float)
             ht.convolve1D(signal, k)
         with self.assertRaises(ValueError):
             ht.convolve1D(signal, kernal_even, mode="same")
         with self.assertRaises(TypeError):
-            k = ht.ones(4, split=0).astype(ht.float)
+            k = ht.ones(4, split=0).astype(ht.int)
             ht.convolve1D(signal, k)
 
+        # test modes
         modes = ["full", "same", "valid"]
         for i, mode in enumerate(modes):
             # odd kernal size
@@ -61,6 +62,14 @@ class TestConvolve(TestCase):
                 gathered = manipulations.resplit(conv, axis=None)
 
                 if mode == "full":
+                    print(conv)
+                    print(gathered)
                     self.assertTrue(ht.equal(full_even, gathered))
                 else:
                     self.assertTrue(ht.equal(full_even[3:-3], gathered))
+
+        # test different data type
+        conv = ht.convolve1D(signal.astype(ht.float), kernal_odd.astype(ht.float))
+        conv.balance_()
+        gathered = manipulations.resplit(conv, axis=None)
+        self.assertTrue(ht.equal(full_odd, gathered))
