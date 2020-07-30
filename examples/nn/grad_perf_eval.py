65849ef17915fc7305d013e4369060971409306c
Lehr, Fabrice von der <fabrice.lehr@dlr.de>
2020-07-30 16:28:12 +0200

    Added csv saving

--- examples/nn/grad_perf_eval.py
+++ examples/nn/grad_perf_eval.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 import argparse
+import csv
 from mpi4py import MPI
 import numpy as np
+import timeit
 import torch
 import torchvision.models as models
 import sys
@@ -12,7 +14,12 @@ import heat.nn.functional as F
 import heat.optim as optim
 from heat.utils.data.datatools import Dataset
 
-import timeit
+
+# file path for results csv
+CSV_PATH = "./grad_perf_eval.csv"
+
+# nn id mapping
+NN_ID_MAPPING = {1: "AlexNet", 2: "ResNet-101", 3: "VGG-16"}
 
 
 # extension of the Dataset class to provide labeled data
@@ -27,6 +34,52 @@ class LabeledDataset(Dataset):
         return img, target
 
 
+# writes measure results to csv file
+def save_results(nn_name, batch_size, img_size, epochs, blocking, repetitions, node_cnt, duration):
+    # create file if not exists
+    with open(CSV_PATH, mode="a+") as csv_file:
+        # go to beginning of the file
+        csv_file.seek(0)
+
+        # get number of rows
+        reader = csv.reader(csv_file, delimiter=",")
+        data = list(reader)
+        row_count = len(data)
+
+        writer = csv.writer(csv_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        fieldnames = [
+            "id",
+            "nn_name",
+            "batch_size",
+            "img_size",
+            "epochs",
+            "blocking",
+            "repetitions",
+            "node_cnt",
+            "duration",
+        ]
+
+        # write field names row, if file has just been created
+        if row_count == 0:
+            writer.writerow(fieldnames)
+            row_count += 1
+
+        # write content row for given conditions and result
+        writer.writerow(
+            [
+                row_count,
+                nn_name,
+                batch_size,
+                img_size,
+                epochs,
+                blocking,
+                repetitions,
+                node_cnt,
+                duration,
+            ]
+        )
+
+
 # wrapper function to pass parameterized function to timeit
 def timeit_wrapper(func, *args, **kwargs):
     def wrapped():
@@ -39,12 +92,14 @@ def timeit_wrapper(func, *args, **kwargs):
 # dataset has shape sample_cnt x 3 x img_size x img_size
 def generate_synthetic_data(mu, sample_cnt, img_size):
     data = ht.zeros((len(mu) * sample_cnt, 3, img_size, img_size), dtype=ht.float32)
-    target = ht.zeros((len(mu) * sample_cnt, 1), dtype=ht.float64)
+    target = ht.zeros((len(mu) * sample_cnt, 1), dtype=ht.float32)
+
     for i in range(len(mu)):
         data[sample_cnt * i : sample_cnt * (i + 1), :] = (
             ht.clip(ht.random.randn(sample_cnt, 3, img_size, img_size) + mu[i], 0, 255) / 255
         )
         target[sample_cnt * i : sample_cnt * (i + 1), :] = ht.ones((sample_cnt, 1)) * i
+
     permutation = np.random.permutation(data.shape[0])
     return ht.array(data[permutation], ndmin=4, split=0), ht.array(target[permutation])
 
@@ -64,10 +119,11 @@ def train_epoch(args, model, device, train_loader, optimizer):
     return True
 
 
-def train(args, model, device, train_loader, optimizer):
+def train(args, model, device, train_loader, optimizer, rank):
     for epoch in range(1, args.epochs + 1):
         continue_training = train_epoch(args, model, device, train_loader, optimizer)
-        print("Epoch ", epoch, " of ", args.epochs, " finished.")
+        if rank == 0:
+            print("Epoch ", epoch, " of ", args.epochs, " finished.")
         if not continue_training:
             break
 
@@ -117,7 +173,7 @@ def main():
         type=int,
         default=1,
         metavar="N",
-        help="network architecture: 1 - AlexNet, 2 - ResNet-101, 3 - VGG_16 (default: 1)",
+        help="network architecture: 1 - AlexNet, 2 - ResNet-101, 3 - VGG-16 (default: 1)",
     )
     parser.add_argument(
         "--repetitions",
@@ -142,35 +198,41 @@ def main():
 
     args = parser.parse_args()
     use_cuda = not args.cuda and torch.cuda.is_available()
-    nn_id = args.nn_id
     device = torch.device("cuda" if use_cuda else "cpu")
     kwargs = {"batch_size": args.batch_size}
 
+    # create synthetic data
     mu = np.arange(args.classes) * 25.5
     data, targets = generate_synthetic_data(mu, args.batch_size * node_cnt, args.img_size)
     dataset = LabeledDataset(data, targets)
-
     if rank == 0:
         print("Synthetic data has been generated.")
 
-    if nn_id == 1:
+    # setup local nn
+    if args.nn_id == 1:
         tmodel = models.AlexNet()
-    elif nn_id == 2:
+    elif args.nn_id == 2:
         tmodel = models.resnet101()
-    elif nn_id == 3:
+    elif args.nn_id == 3:
         tmodel = models.vgg16()
     else:
         print("Invalid NN id.")
         return
 
+    # setup local optimizer
     optimizer = optim.Adadelta(tmodel.parameters(), lr=args.lr)
+
+    # setup dp_nn and dp_optimizer
     dp_optim = ht.optim.DataParallelOptimizer(optimizer)
     model = ht.nn.DataParallel(
         tmodel, data.comm, dp_optim, blocking_parameter_updates=args.blocking
     )
 
+    # setup data loader
     train_loader = ht.utils.data.datatools.DataLoader(dataset.data, lcl_dataset=dataset, **kwargs)
-    timed_training = timeit_wrapper(train, args, model, device, train_loader, optimizer)
+
+    #
+    timed_training = timeit_wrapper(train, args, model, device, train_loader, optimizer, rank)
 
     # drop first run from measure
     timed_training()
@@ -189,6 +251,18 @@ def main():
     if rank == 0:
         print("Benchmark has been finished.")
         print("Time: ", glo_duration)
+
+        # write result to csv
+        save_results(
+            NN_ID_MAPPING[args.nn_id],
+            args.batch_size,
+            args.img_size,
+            args.epochs,
+            int(args.blocking),
+            args.repetitions,
+            node_cnt,
+            glo_duration,
+        )
 
 
 if __name__ == "__main__":
