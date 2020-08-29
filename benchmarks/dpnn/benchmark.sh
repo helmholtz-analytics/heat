d6e165851094d3460770a5f217f975dafbed2db4
Fabrice von der Lehr <vonderlehr1@hdfmll01.hdfml>
2020-08-29 14:49:33 +0200

    Reset benchmark start script

--- benchmarks/dpnn/benchmark.sh
+++ benchmarks/dpnn/benchmark.sh
@@ -1,11 +1,11 @@
 !/bin/bash
 
-#for t in 1 2 4
-#do  
-#	sbatch --nodes 1 --ntasks-per-node $t grad_perf_eval.sh
-#done
+for t in 1 2 4
+do  
+	sbatch --nodes 1 --ntasks-per-node $t grad_perf_eval.sh
+done
 
-for n in 8 #2 4 8 15
+for n in 2 4 8 15
 do
-	sbatch --nodes $n --tasks-per-node 1 grad_perf_eval.sh
+	sbatch --nodes $n --tasks-per-node 4 grad_perf_eval.sh
 done
