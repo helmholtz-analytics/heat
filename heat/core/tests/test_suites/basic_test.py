c3ab419cee6529d9768b661501a3fd615feec5ed
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2020-10-08 13:29:23 +0200

    change device earlier in assert_func_equal_tensor

--- heat/core/tests/test_suites/basic_test.py
+++ heat/core/tests/test_suites/basic_test.py
@@ -276,6 +276,7 @@ class TestCase(unittest.TestCase):
 
         if isinstance(tensor, np.ndarray):
             torch_tensor = torch.from_numpy(tensor.copy())
+            torch_tensor = torch_tensor.to(self.device.torch_device)
             np_array = tensor
         elif isinstance(tensor, torch.Tensor):
             torch_tensor = tensor
