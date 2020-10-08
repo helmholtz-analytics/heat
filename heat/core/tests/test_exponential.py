bd5903c35acb9b7773c2472a1e7473728fbb52a6
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2020-10-08 09:55:14 +0200

    change device directly in test_exp2

--- heat/core/tests/test_exponential.py
+++ heat/core/tests/test_exponential.py
@@ -97,6 +97,7 @@ class TestExponential(TestCase):
     def test_exp2(self):
         elements = 10
         tmp = np.exp2(torch.arange(elements, dtype=torch.float64))
+        tmp = tmp.to(self.device.torch_device)
         comparison = ht.array(tmp, device=self.device)
 
         # exponential of float32
