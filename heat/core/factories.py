ca5f688919d5d087721e35a54f1620ec8936b03d
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2020-10-14 10:24:05 +0200

    formatting

--- heat/core/factories.py
+++ heat/core/factories.py
@@ -327,7 +327,8 @@ def array(
     if str(obj.device) != device.torch_device:
         warnings.warn(
             "Array 'obj' is not on device '{}'. It will be copied to it.".format(device),
-            UserWarning, stacklevel=2
+            UserWarning,
+            stacklevel=2,
         )
         obj = obj.to(device.torch_device)
 
