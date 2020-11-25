5849104efefbc474ee8a2578772c97f6f7ee3cc3
coquelin77 <daniel.coquelin@gmail.com>
2020-11-25 10:24:50 +0100

    udpate optim init for python 3.6

--- heat/optim/__init__.py
+++ heat/optim/__init__.py
@@ -1,10 +1,12 @@
-from . import dp_optimizer
+from .lr_scheduler import *
+
+import sys
 import torch
 import unittest
-import sys
+
 
 if sys.version_info.minor >= 7:
-    from .lr_scheduler import *
+    from .dp_optimizer import *
 
     def __getattr__(name):
         # this will call the Heat optimizers if available,
@@ -23,10 +25,25 @@ if sys.version_info.minor >= 7:
 
 
 else:
-    # todo lr scheduler
-    from . import lr_scheduler
-    import torch.optim as tnn
+    from . import dp_optimizer
 
-    torch_all = tnn.__dict__
-    for n in torch_all.keys():
-        setattr(lr_scheduler, n, torch_all[n])
+    class Wrapper(object):
+        def __init__(self, wrapped):
+            self.wrapped = wrapped
+
+        def __getattr__(self, name):
+            # this will call the Heat optimizers if available,
+            # otherwise, it falls back to call a torch optimizer
+            if name in dp_optimizer.__all__:
+                return dp_optimizer.__getattribute__(name)
+
+            try:
+                return torch.optim.__getattribute__(name)
+            except AttributeError:
+                try:
+                    unittest.__getattribute__(name)
+                except AttributeError:
+                    if name is not None:
+                        raise AttributeError(f"module {name} not implemented in torch.optim")
+
+    sys.modules[__name__] = Wrapper(sys.modules[__name__])
