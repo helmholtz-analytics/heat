28d449a6df6f2e3b7dc19346105398d0806fed42
Kai Krajsek <k.krajsek@fz-juelich.de>
2021-03-13 17:06:19 +0100

    added optional communicator to HeatSyncBatchNorm Class

--- heat/nn/__init__.py
+++ heat/nn/__init__.py
@@ -6,7 +6,7 @@ from torch.autograd.function import Function
 import torch.nn.functional as F
 from torch.nn.modules.batchnorm import _BatchNorm
 import heat as ht
-
+from typing import Optional, Any
 
 
 if sys.version_info.minor >= 7:
@@ -62,9 +62,94 @@ else:
 
 
 class HeatSyncBatchNorm(_BatchNorm):
-    
-    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True):
+    r"""Applies Batch Normalization over a N-Dimensional input (a mini-batch of [N-2]D inputs
+    with additional channel dimension) as described in the paper
+    `Batch Normalization: Accelerating Deep Network Training by Reducing
+    Internal Covariate Shift <https://arxiv.org/abs/1502.03167>`__ .
+
+    .. math::
+
+        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta
+
+    The mean and standard-deviation are calculated per-dimension over all
+    mini-batches of the same process groups. :math:`\gamma` and :math:`\beta`
+    are learnable parameter vectors of size `C` (where `C` is the input size).
+    By default, the elements of :math:`\gamma` are sampled from
+    :math:`\mathcal{U}(0, 1)` and the elements of :math:`\beta` are set to 0.
+    The standard-deviation is calculated via the biased estimator, equivalent to
+    `torch.var(input, unbiased=False)`.
+
+    Also by default, during training this layer keeps running estimates of its
+    computed mean and variance, which are then used for normalization during
+    evaluation. The running estimates are kept with a default :attr:`momentum`
+    of 0.1.
+
+    If :attr:`track_running_stats` is set to ``False``, this layer then does not
+    keep running estimates, and batch statistics are instead used during
+    evaluation time as well.
+
+    .. note::
+        This :attr:`momentum` argument is different from one used in optimizer
+        classes and the conventional notion of momentum. Mathematically, the
+        update rule for running statistics here is
+        :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momentum} \times x_t`,
+        where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
+        new observed value.
+
+    Because the Batch Normalization is done for each channel in the ``C`` dimension, computing
+    statistics on ``(N, +)`` slices, it's common terminology to call this Volumetric Batch
+    Normalization or Spatio-temporal Batch Normalization.
+
+    Currently :class:`SyncBatchNorm` only supports
+    :class:`~torch.nn.DistributedDataParallel` (DDP) with single GPU per process. Use
+    :meth:`torch.nn.SyncBatchNorm.convert_sync_batchnorm()` to convert
+    :attr:`BatchNorm*D` layer to :class:`SyncBatchNorm` before wrapping
+    Network with DDP.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, +)`
+        eps: a value added to the denominator for numerical stability.
+            Default: ``1e-5``
+        momentum: the value used for the running_mean and running_var
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics, and initializes statistics
+            buffers :attr:`running_mean` and :attr:`running_var` as ``None``.
+            When these buffers are ``None``, this module always uses batch statistics.
+            in both training and eval modes. Default: ``True``
+        process_group: synchronization of stats happen within each process group
+            individually. Default behavior is synchronization across the whole
+            world
+
+    Shape:
+        - Input: :math:`(N, C, +)`
+        - Output: :math:`(N, C, +)` (same shape as input)
+
+    Examples::
+
+        >>> # With Learnable Parameters
+        >>> m = ht.nn.SyncBatchNorm(100)
+        >>> # creating process group (optional)
+        >>> input = torch.randn(20, 100, 35, 45, 10)
+        >>> output = m(input)
+    """       
+    def __init__(
+        self,
+        num_features: int,
+        eps: float = 1e-5,
+        momentum: float = 0.1,
+        affine: bool = True,
+        track_running_stats: bool = True,
+        comm: Optional[Any] = None
+    ) -> None:
         super().__init__(num_features, eps, momentum, affine, track_running_stats)
+        self.comm = comm
+
 
     def _check_input_dim(self, input):
         if input.dim() < 2:
@@ -111,12 +196,13 @@ class HeatSyncBatchNorm(_BatchNorm):
         running_mean = self.running_mean if not self.training or self.track_running_stats else None
         running_var = self.running_var if not self.training or self.track_running_stats else None
 
-
-
         need_sync = bn_training
         if need_sync:
-            need_sync = ht.MPI_WORLD.size > 1
-
+            if self.comm:
+                comm = self.comm
+            else:
+                comm = ht.MPICommunication()    
+            need_sync = comm.size > 1
 
         # fallback to framework BN when synchronization is not necessary
         if not need_sync:
@@ -128,23 +214,22 @@ class HeatSyncBatchNorm(_BatchNorm):
             assert bn_training
             return SyncBatchNorm.apply(
                 input, self.weight, self.bias, self.running_mean, self.running_var,
-                self.eps, exponential_average_factor)
+                self.eps, exponential_average_factor, comm)
 
 
 
 
 class SyncBatchNorm(Function):
     @staticmethod
-    def forward(self, input, weight, bias, running_mean, running_var, eps, momentum):
+    def forward(self, input, weight, bias, running_mean, running_var, eps, momentum, comm):
         input = input.contiguous()
-
+        self.comm = comm
         size = input.numel() // input.size(1)
-        count = torch.tensor([size]).to('cuda:0')
+        count = torch.tensor([size]).to(input.device)
         
         # calculate mean/invstd for input.
         mean, invstd = torch.batch_norm_stats(input, eps)
-        comm = ht.MPICommunication()
-
+         
         count_shape = count.shape
         count = count.unsqueeze(0)
         count_all = torch.zeros((ht.MPI_WORLD.size, ) + count_shape, device=count.device, dtype=torch.int64)
@@ -199,15 +284,16 @@ class SyncBatchNorm(Function):
 
         if self.needs_input_grad[0]:
             # synchronizing stats used to calculate input gradient.
-            comm=ht.MPICommunication() 
+            comm = self.comm
+
             sum_dy_reduced = torch.zeros_like(sum_dy,device=grad_output.device)
             comm.Allreduce(sum_dy, sum_dy_reduced, op=ht.MPI.SUM)
             
             sum_dy_xmu_reduced = torch.zeros_like(sum_dy_xmu,device=grad_output.device)
             comm.Allreduce(sum_dy_xmu, sum_dy_xmu_reduced, op=ht.MPI.SUM)
 
-            mean_dy = sum_dy_reduced / ht.MPI_WORLD.size 
-            mean_dy_xmu = sum_dy_xmu_reduced / ht.MPI_WORLD.size 
+            mean_dy = sum_dy_reduced / comm.size 
+            mean_dy_xmu = sum_dy_xmu_reduced / comm.size 
 
             # backward pass for gradient calculation
             grad_input = torch.batch_norm_backward_elemt(
