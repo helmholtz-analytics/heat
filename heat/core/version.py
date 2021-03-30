ca2a3a06cdafe6db6818c074d1f54181b96792dc
Markus Goetz <markus.goetz@kit.edu>
2021-03-30 10:12:12 +0200

    Fixed version.py rendering

--- heat/core/version.py
+++ heat/core/version.py
@@ -1,11 +1,16 @@
 """This module contains HeAT's version information."""
 
-major = 0
-minor = 5
-micro = 1
+major = 1
+"""Indicates HeAT's main version."""
+minor = 0
+"""Indicates feature extension."""
+micro = 0
+"""Indicates revisions for bugfixes."""
 extension = None
+"""Indicates special builds, e.g. for specific hardware."""
 
 if not extension:
     __version__ = "{}.{}.{}".format(major, minor, micro)
+    """The combined version string, consisting out of major, minor, micro and possibly extension."""
 else:
     __version__ = "{}.{}.{}-{}".format(major, minor, micro, extension)
