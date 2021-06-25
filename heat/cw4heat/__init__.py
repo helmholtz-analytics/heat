a9b2059d76bb82b183403ac4f5198936d16e11af
Frank Schlimbach <frank.schlimbach@intel.com>
2021-06-25 07:56:40 -0500

    first draft of a controller-worker wrapper for heat

---
+++ heat/cw4heat/__init__.py
@@ -0,0 +1,256 @@
+# MIT License
+
+# Copyright (c) 2021 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+###############################################################################
+# This provides a wrapper around SPMD-based HeAT
+# (github.com/helmholtz-analytics/heat) to operate in controller-worker mode.
+
+# The goal is to provide a compliant implementation of the array API
+# (github.com/data-apis/arra-api).
+
+# Returned array (DNDArray) objects are handles/futures only. Their content is
+# available through __int__ etc., through __partitioned__ or heat(). Notice: this
+# allows for delayed execution and optimizations of the workflow/task-graph and
+# communication.
+
+# For a function/method of the array-API that is executed on the controller
+# process, this wrapper generates the equivalent source code to be executed on
+# the worker processes.  The code is then sent to each remote worker and
+# executed there.
+
+# It's up to the distribution layer (e.g. distributor) to make sure the code is
+# executed in the right order on each process/worker so that collective
+# communication in HeAT can operate correctly without dead-locks.
+
+# To allow workflow optimizations array dependences and to avoid
+# pickle-dependencies to the array inputs we separate scalar/non-array arguments
+# from array arguments. For this we assume that array arguments never occur
+# after non-array arguments.  Each function.task handles and passes array-typed
+# and non-array-types arguments separately.
+###############################################################################
+
+from . import distributor
+from .arrayapi import (
+    aa_attributes,
+    aa_tlfuncs,
+    aa_datatypes,
+    aa_constants,
+    aa_methods_s,
+    aa_methods_a,
+    aa_inplace_operators,
+    aa_reflected_operators,
+)
+
+# just in case we find another SPMD/MPI implementation of numpy...
+import heat as impl
+from heat import DNDarray as dndarray
+impl_str = "impl"
+dndarray_str = "impl.DNDarray"
+
+def init():
+    '''
+    Initialize distribution engine.
+    For now we assume all ranks (controller and workers) are started through mpirun,
+    workers will never leave distributor.start() and so this function.
+    Call this as the very first thing in your program. For now it is recommended
+    to start your program with
+
+    import heat.cw4heat as ht
+    ht.init()
+
+    Also call fini() before exiting.
+    '''
+    distributor.init()
+    distributor.start()
+
+
+def fini():
+    '''
+    Finalize/shutdown distribution engine.
+    When called on controller, workers will sys.exit from init().
+    '''
+    distributor.fini()
+
+
+class _Task:
+    'A work item, executing functions provided as code.'
+    def __init__(self, func, args, kwargs, unwrap='*'):
+        self._func = func
+        self._args = args
+        self._kwargs = kwargs
+        self._unwrap = unwrap
+
+    def run(self, deps):
+        if deps:
+            return eval(f"{self._func}({self._unwrap}deps, *self._args, **self._kwargs)")
+        else:
+            return eval(f"{self._func}(*self._args, **self._kwargs)")
+
+
+class _PropertyTask:
+    'A work item, executing class properties provided as code.'
+    def __init__(self, func):
+        self._func = func
+
+    def run(self, deps):
+        return eval(f"deps[0].{self._func}")
+
+
+def _submit(name, args, kwargs, unwrap='*'):
+    '''
+    Create a _Task and submit, return PManager/Future.
+    '''
+    scalar_args = tuple(x for x in args if not isinstance(x, DDParray))
+    deps = [x._handle.getId() for x in args if isinstance(x, DDParray)]
+    return distributor.submitPP(_Task(name, scalar_args, kwargs, unwrap=unwrap), deps)
+
+
+def _submitProperty(name, self):
+    '''
+    Create a _PropertyTask (property) and submit, return PManager/Future.
+    '''
+    t = _PropertyTask(name)
+    try:
+        res = distributor.submitPP(t, [self._handle.getId()])
+    except:
+        assert False
+    return res
+
+
+# setitem has scalar arg key before array arg value
+# we need to provide a function accepting the inverse order
+def _setitem_normalized(self, value, key):
+    self.__setitem__(key, value)
+
+
+#######################################################################
+# Our array is just a wrapper. Actual array is stored as a handle to
+# allow delayed execution.
+#######################################################################
+class DDParray:
+    '''
+    Shallow wrapper class representing a distributed array.
+    It will be filled dynamically from lists extracted from the array-API.
+    All functionality is delegated to the underlying implementation,
+    executed in tasks.
+    '''
+
+    #######################################################################
+    # first define methods/properties which need special care.
+    #######################################################################
+
+    def __init__(self, handle):
+        'Do not use this array. Use creator functions instead.'
+        self._handle = handle
+
+    def heat(self):
+        '''
+        Return heat native array.
+        With delayed execution, triggers computation as needed and blocks until array is available.
+        '''
+        return self._handle.get()
+
+    def __getitem__(self, key):
+        'Return item/slice as array.'
+        return DDParray(_submit(f'{dndarray_str}.__getitem__', (self, key), {}))
+
+    # bring args in the order we can process and feed into normal process
+    # using global normalized version
+    def __setitem__(self, key, value):
+        'set item/slice to given value'
+        _submit(f'_setitem_normalized', (self, value, key), {})
+
+    @property
+    def T(self):
+        return DDParray(_submitProperty('T', self))
+
+
+    #######################################################################
+    # Now we add methods/properties through the standard process.
+    #######################################################################
+
+    # dynamically generate class methods from list of methods in array-API
+    # we simply make lambdas which submit appropriate Tasks
+    # FIXME: aa_inplace_operators,others?
+    fixme_afuncs = ['squeeze', 'astype', 'balance',]
+    for method in aa_methods_a + aa_reflected_operators + fixme_afuncs:
+        if method not in ['__getitem__', '__setitem__'] and hasattr(dndarray, method):
+            exec(f"{method} = lambda self, *args, **kwargs: DDParray(_submit('{dndarray_str}.{method}', (self, *args), kwargs))")
+
+    for method in aa_methods_s:
+        if hasattr(dndarray, method):
+            exec(f"{method} = lambda self, *args, **kwargs: _submit('{dndarray_str}.{method}', (self, *args), kwargs).get()")
+
+    for attr in aa_attributes:
+        if attr != 'T' and hasattr(dndarray, attr):
+            exec(f"{attr} = property(lambda self: self._handle.get().{attr})")
+
+    def __getattr__(self, attr):
+        # attributes are special
+        if not attr in aa_attributes:
+            raise Exception(f"unknown method/attribute {attr} requested")
+
+
+#######################################################################
+# first define top-level functions which need special care.
+#######################################################################
+
+# np.concatenate accepts a list of arrays (not individual arrays)
+# so we let the task not unwrap the list of deps
+def concatenate(*args, **kwargs):
+    return DDParray(_submit(f'{impl_str}.concatenate', *args, kwargs, unwrap=''))
+
+
+#######################################################################
+# first define top-level functions through the standard process.
+#######################################################################
+#   - creating arrays
+#   - elementswise operations
+#   - statistical operations
+# (lists taken from list of methods in array-API)
+# Again, we simply make lambdas which submit appropriate Tasks
+
+fixme_funcs = ['load_csv']
+for func in aa_tlfuncs + fixme_funcs:
+    exec(f"{func} = lambda *args, **kwargs: DDParray(_submit('{impl_str}.{func}', args, kwargs))")
+
+
+def concatenate(*args, **kwargs):
+    return DDParray(_submit(f'{impl_str}.concatenate', *args, kwargs, unwrap=''))
+
+
+# Here we data types and constants
+for attr in aa_datatypes + aa_constants:
+    if hasattr(impl, attr):
+        exec(f"{attr} = {impl_str}.{attr}")
+    else:
+        print(f"{impl.__name__} has no {attr}")
+
+
+#######################################################################
+# quick hack to provide random features
+#######################################################################
+class random:
+    for method, obj in impl.random.__dict__.items():
+        if callable(obj):
+            exec(f"{method} = staticmethod(lambda *args, **kwargs: DDParray(_submit('{impl_str}.random.{method}', args, kwargs)))")
---
+++ heat/cw4heat/distributor.py
@@ -0,0 +1,175 @@
+# MIT License
+
+# Copyright (c) 2021 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+###############################################################################
+# Distribution engine.
+#   - schedules same tasks on all workers
+#   - handles dependences seperately
+# This currently is a very simple eagerly executing machinery.
+# We can make this better over time. A low hanging fruit seems might
+# be to delay distribution until go() is called. This would allow aggregating
+# multiple distribution messages into one.
+#
+# Dependent objects have a unique identifier, assigned when a handle to it is
+# created. We assume that all workers execute handle-creation in identical order.
+# Such dependences are assumed to be global entities, e.g. each worker holds
+# a handle/reference to it (e.g. like a heat.DNDarray). The local handles
+# exist on each, stored in a worker-local dictionary. Thsi allows identifying
+# dependences through simple integers.
+#
+# Notice, mpi4py does not provide ibcast, so we cannot overlap. This makes the
+# above aggregation particularly promising. Another option woujld be to write
+# this in C/C++ and use ibcast.
+###############################################################################
+
+
+import sys
+from mpi4py import MPI
+_comm = MPI.COMM_WORLD
+
+# define identifiers
+END = 0
+TASK = 1
+GO = 2
+
+
+def init():
+    'Init distributor'
+    pass
+
+
+def start():
+    '''
+    Start distribution engine.
+    Controller inits and returns.
+    Workers enter recv-loop and exit program when fini si called.
+    '''
+    if _comm.rank != 0:
+        done = False
+        header = None
+        rtask = None
+        while(not done):
+            # wait in bcast for work
+            header = _comm.bcast(header, 0)
+            # then see what we need to do
+            if header[0] == END:
+                done = True
+                break
+            elif header[0] == TASK:
+                header[1].submit()
+            elif header[0] == GO:
+                # no delayed execution for now -> nothing to do
+                pass
+            else:
+                raise Exception("Worker received unknown tag")
+        sys.exit()
+
+        
+def fini():
+    'Control sends end-tag. Workers will sys.exit'
+    header = [END]
+    header = _comm.bcast(header, 0)
+
+
+def go():
+    'Trigger execution of all tasks that are still in flight'
+    header = [GO]
+    header = _comm.bcast(header, 0)
+
+
+def submitPP(task, deps, in_order=True):
+    '''
+    Submit a process-parallel task and return a handle/future.
+    '''
+    rtask = _RemoteTask(task, deps)
+    header = [TASK, rtask]
+    _, rtask = _comm.bcast(header, 0)
+    return rtask.submit()
+
+
+class Handle:
+    '''
+    A future representing an object that will be available eventually.
+    get() will return None as long as the value is not available.
+    '''
+
+    # this defines the next free and globally unique identifier
+    _nextId = 1
+
+    def __init__(self):
+        '''
+        Initialize handle.
+        We assume all workers create handles to objects in identical order.
+        This allows us to assign a simple integers as the unqique id.
+        '''
+        self._obj = None
+        self._id = Handle._nextId
+        Handle._nextId += 1
+
+    def set(self, obj):
+        'Make object available.'
+        self._obj = obj
+
+    def getId(self):
+        'Return future/handle id'
+        return self._id
+
+    def get(self):
+        'Return object or None'
+        return self._obj
+        
+
+class _RemoteTask:
+    '''
+    A task which is executed remotely on a worker.
+    It accepts a task with a run-method that it will execute at some point.
+    It also accepts dependences explicitly and so allows to create
+    task-graphs etc.
+
+    We keep a static dictionary mapping globally unique identifiers to dependent
+    global objects (like heat.DNDarrays). This keeps the objects alive and allows
+    communicating through simple integers.
+    '''
+
+    def __init__(self, task, deps, inorder=True):
+        self._depIds = deps
+        self._task = task
+        self._inorder = inorder
+
+    # here we store objects that are input dependences to tasks
+    s_pms = {}
+
+    def submit(self):
+        '''
+        Submit task to local task scheduler.
+        For now we execute eagerly, this is much simpler to implement.
+        Later, we might consider lazy evaluation, task-graph-optimizations etc.
+        FIXME: We currently assign a new id and store the result even when there is no result
+        or the result is not a global object.
+        '''
+        deps = [_RemoteTask.s_pms[i] for i in self._depIds]
+        res = self._task.run(deps)
+        hndl = Handle()
+        hndl.set(res)
+        _RemoteTask.s_pms[hndl.getId()] = res
+        return hndl
