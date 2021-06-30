034f3fcadbca42a02f21e092e09a646b28243e9f
coquelin77 <daniel.coquelin@gmail.com>
2021-06-30 16:23:05 +0200

    added DALI back for dataloading

--- examples/nn/daso-imagenet-cosine.py
+++ examples/nn/daso-imagenet-cosine.py
@@ -29,6 +29,16 @@ def print0(*args, **kwargs):
 import torch.utils.data.distributed
 from torchvision import datasets, transforms, models
 
+try:
+    from nvidia.dali.plugin.pytorch import DALIClassificationIterator, LastBatchPolicy
+    from nvidia.dali.pipeline import pipeline_def
+    import nvidia.dali.types as types
+    import nvidia.dali.fn as fn
+except ImportError:
+    raise ImportError(
+        "Please install DALI from https://www.github.com/NVIDIA/DALI to run this example."
+    )
+
 
 def parse():
     model_names = sorted(
@@ -195,6 +205,73 @@ def parse():
     )
     args = parser.parse_args()
     return args
+
+
+@pipeline_def
+def create_dali_pipeline(
+    data_dir,
+    crop,
+    size,
+    shard_id=ht.MPI_WORLD.rank,
+    num_shards=ht.MPI_WORLD.size,
+    dali_cpu=False,
+    is_training=True,
+):
+    shard_id = ht.MPI_WORLD.rank
+    num_shards = ht.MPI_WORLD.size
+    images, labels = fn.readers.file(
+        file_root=data_dir,
+        shard_id=shard_id,
+        num_shards=num_shards,
+        random_shuffle=is_training,
+        pad_last_batch=True,
+        name="Reader",
+    )
+    dali_device = "cpu" if dali_cpu else "gpu"
+    decoder_device = "cpu" if dali_cpu else "mixed"
+    device_memory_padding = 211025920 if decoder_device == "mixed" else 0
+    host_memory_padding = 140544512 if decoder_device == "mixed" else 0
+    if is_training:
+        images = fn.decoders.image_random_crop(
+            images,
+            device=decoder_device,
+            output_type=types.RGB,
+            device_memory_padding=device_memory_padding,
+            host_memory_padding=host_memory_padding,
+            random_aspect_ratio=[0.8, 1.25],
+            random_area=[0.1, 1.0],
+            num_attempts=100,
+        )
+        images = fn.resize(
+            images,
+            device=dali_device,
+            resize_x=crop,
+            resize_y=crop,
+            interp_type=types.INTERP_TRIANGULAR,
+        )
+        mirror = fn.random.coin_flip(probability=0.5)
+    else:
+        images = fn.decoders.image(images, device=decoder_device, output_type=types.RGB)
+        images = fn.resize(
+            images,
+            device=dali_device,
+            size=size,
+            mode="not_smaller",
+            interp_type=types.INTERP_TRIANGULAR,
+        )
+        mirror = False
+
+    images = fn.crop_mirror_normalize(
+        images.gpu(),
+        dtype=types.FLOAT,
+        output_layout="CHW",
+        crop=(crop, crop),
+        mean=[0.485 * 255, 0.456 * 255, 0.406 * 255],
+        std=[0.229 * 255, 0.224 * 255, 0.225 * 255],
+        mirror=mirror,
+    )
+    labels = labels.gpu()
+    return images, labels
 
 
 # item() is a recent addition, so this helps with backward compatibility. (from DALI)
@@ -407,51 +484,89 @@ def main():
         crop_size = 224  # should this be 256?
         val_size = 256
 
-    train_dataset = datasets.ImageFolder(
-        args.train,
-        transform=transforms.Compose(
-            [
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]
-        ),
-    )
-    train_sampler = torch.utils.data.distributed.DistributedSampler(
-        train_dataset, num_replicas=args.world_size, rank=rank
-    )
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
+        # train_dataset = datasets.ImageFolder(
+        #     args.train,
+        #     transform=transforms.Compose(
+        #         [
+        #             transforms.RandomResizedCrop(224),
+        #             transforms.RandomHorizontalFlip(),
+        #             transforms.ToTensor(),
+        #             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        #         ]
+        #     ),
+        # )
+        # train_sampler = torch.utils.data.distributed.DistributedSampler(
+        #     train_dataset, num_replicas=args.world_size, rank=rank
+        # )
+        # train_loader = torch.utils.data.DataLoader(
+        #     train_dataset,
+        #     batch_size=args.batch_size,
+        #     sampler=train_sampler,
+        #     num_workers=2,
+        #     pin_memory=True,
+        #     multiprocessing_context="fork",  # helps with infiniband issues?? (from hvd)
+        # )
+        #
+        # val_dataset = datasets.ImageFolder(
+        #     args.validate,
+        #     transform=transforms.Compose(
+        #         [
+        #             transforms.Resize(256),
+        #             transforms.CenterCrop(224),
+        #             transforms.ToTensor(),
+        #             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        #         ]
+        #     ),
+        # )
+        # val_sampler = torch.utils.data.distributed.DistributedSampler(
+        #     val_dataset, num_replicas=args.world_size, rank=rank
+        # )
+        # val_loader = torch.utils.data.DataLoader(
+        #     val_dataset,
+        #     batch_size=args.batch_size,
+        #     sampler=val_sampler,
+        #     num_workers=2,
+        #     pin_memory=True,
+        #     multiprocessing_context="fork",
+        #     # 'forkserver',  # helps with infiniband issues?? (from hvd)
+        # )
+
+        data_dir = "/hkfs/work/workspace/scratch/qv2382-heat/imagenet-raw/ILSVRC/Data/CLS-LOC/"
+
+    pipe = create_dali_pipeline(
         batch_size=args.batch_size,
-        sampler=train_sampler,
-        num_workers=2,
-        pin_memory=True,
-        multiprocessing_context="fork",  # helps with infiniband issues?? (from hvd)
+        num_threads=args.workers,
+        device_id=args.local_rank,
+        seed=12 + args.local_rank,
+        data_dir=data_dir + "train/",
+        crop=crop_size,
+        size=val_size,
+        dali_cpu=args.dali_cpu,
+        # shard_id=args.local_rank,
+        # num_shards=args.world_size,
+        is_training=True,
+    )
+    pipe.build()
+    train_loader = DALIClassificationIterator(
+        pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL
     )
 
-    val_dataset = datasets.ImageFolder(
-        args.validate,
-        transform=transforms.Compose(
-            [
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]
-        ),
-    )
-    val_sampler = torch.utils.data.distributed.DistributedSampler(
-        val_dataset, num_replicas=args.world_size, rank=rank
-    )
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset,
+    pipe = create_dali_pipeline(
         batch_size=args.batch_size,
-        sampler=val_sampler,
-        num_workers=2,
-        pin_memory=True,
-        multiprocessing_context="fork",
-        # 'forkserver',  # helps with infiniband issues?? (from hvd)
+        num_threads=args.workers,
+        device_id=args.local_rank,
+        seed=12 + args.local_rank,
+        data_dir=data_dir + "val/",
+        crop=crop_size,
+        size=val_size,
+        dali_cpu=args.dali_cpu,
+        # shard_id=args.local_rank,
+        # num_shards=args.world_size,
+        is_training=False,
+    )
+    pipe.build()
+    val_loader = DALIClassificationIterator(
+        pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL
     )
 
     if args.evaluate:
