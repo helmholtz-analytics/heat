c3881cadb86c5a870f18e93d92ef00f2fd65e46f
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2021-09-17 11:43:59 +0200

    fix test

--- heat/core/tests/test_arithmetics.py
+++ heat/core/tests/test_arithmetics.py
@@ -39,10 +39,11 @@ class TestArithmetics(TestCase):
         b = ht.array([1, 2], split=0)
         c = ht.add(a, b)
         self.assertTrue(ht.equal(c, ht.array([2, 3])))
-        if c.comm.rank < 2:
-            self.assertEqual(c.larray.size()[0], 1)
-        else:
-            self.assertEqual(c.larray.size()[0], 0)
+        if c.comm.size > 1:
+            if c.comm.rank < 2:
+                self.assertEqual(c.larray.size()[0], 1)
+            else:
+                self.assertEqual(c.larray.size()[0], 0)
 
         with self.assertRaises(ValueError):
             ht.add(self.a_tensor, self.another_vector)
