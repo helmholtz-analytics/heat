1f519078578b1532c8642301ad8f222727dc017d
Claudia Comito <c.comito@fz-juelich.de>
2021-10-04 15:06:24 +0200

    ht.diff edge case only one element along `axis`

--- heat/core/arithmetics.py
+++ heat/core/arithmetics.py
@@ -378,6 +378,19 @@ def diff(
             ret = ret[tuple(axis_slice)] - ret[tuple(axis_slice_end)]
         return ret
 
+    if a.shape[axis] == 1:
+        ret_lshape = list(a.lshape)
+        ret_lshape[axis] = 0
+        ret = torch.empty(ret_lshape, dtype=a.larray.dtype, device=a.larray.device)
+        if a.split != axis:
+            ret_gshape = ret_lshape
+            ret_gshape[a.split] = a.gshape[a.split]
+            return DNDarray(ret, tuple(ret_gshape), a.dtype, a.split, a.device, a.comm, a.balanced)
+
+        return DNDarray(
+            ret, tuple(ret_lshape), a.dtype, split=None, device=a.device, comm=a.comm, balanced=True
+        )
+
     size = a.comm.size
     rank = a.comm.rank
     ret = a.copy()
