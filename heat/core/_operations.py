use None-indexing, fix broadcast_shapes

heat/core/_operations.py
@@ -93,16 +93,21 @@ def __binary_op(
         try:
             t2 = factories.array(t2, device=t1.device, comm=t1.comm)
         except (ValueError, TypeError):
-            raise TypeError("Data type not supported, input was {}".format(type(t1)))
+            raise TypeError("Data type not supported, input was {}".format(type(t2)))
+    sanitation.sanitize_in(t1)
+    sanitation.sanitize_in(t2)
 
     # Make inputs have the same dimensionality
     output_shape = stride_tricks.broadcast_shape(t1.shape, t2.shape)
     # Broadcasting allows additional empty dimensions on the left side
     # TODO simplify this once newaxis-indexing is supported to get rid of the loops
-    while len(t1.shape) < len(output_shape):
-        t1 = t1.expand_dims(axis=0)
-    while len(t2.shape) < len(output_shape):
-        t2 = t2.expand_dims(axis=0)
+    # while len(t1.shape) < len(output_shape):
+    #     t1 = t1.expand_dims(axis=0)
+    # while len(t2.shape) < len(output_shape):
+    #     t2 = t2.expand_dims(axis=0)
+    t1 = t1[tuple([None] * (len(output_shape) - t1.ndim))]
+    t2 = t2[tuple([None] * (len(output_shape) - t2.ndim))]
+
     # if t1.split is not None and t2.split is not None and t1.split != t2.split:
     #     # if t1 and t2 both split, split has to be the same after (shape)bcast
     #     raise NotImplementedError(
@@ -234,7 +239,6 @@ def __binary_op(
     #     )
     # else:  # local process is not empty
     result = operation(t1.larray.type(promoted_type), t2.larray.type(promoted_type), **fn_kwargs)
-
     if not isinstance(result, torch.Tensor):
         result = torch.tensor(result, device=output_device.torch_device)
 

heat/core/stride_tricks.py
@@ -4,14 +4,14 @@ A collection of functions used for inferring or correction things before major c
 
 import itertools
 import numpy as np
+import torch
 
 from typing import Tuple, Union
 
 
 def broadcast_shape(shape_a: Tuple[int, ...], shape_b: Tuple[int, ...]) -> Tuple[int, ...]:
     """
-    Infers, if possible, the broadcast output shape of two operands a and b. Inspired by stackoverflow post:
-    https://stackoverflow.com/questions/24743753/test-if-an-array-is-broadcastable-to-a-shape
+    Infers, if possible, the broadcast output shape of two operands a and b.
 
     Parameters
     ----------
@@ -41,17 +41,7 @@ def broadcast_shape(shape_a: Tuple[int, ...], shape_b: Tuple[int, ...]) -> Tuple
         "operands could not be broadcast, input shapes {} {}".format(shape_a, shape_b)
     ValueError: operands could not be broadcast, input shapes (2, 1) (8, 4, 3)
     """
-    it = itertools.zip_longest(shape_a[::-1], shape_b[::-1], fillvalue=1)
-    resulting_shape = max(len(shape_a), len(shape_b)) * [None]
-    for i, (a, b) in enumerate(it):
-        if a == 1 or b == 1 or a == b:
-            resulting_shape[i] = max(a, b)
-        else:
-            raise ValueError(
-                "operands could not be broadcast, input shapes {} {}".format(shape_a, shape_b)
-            )
-
-    return tuple(resulting_shape[::-1])
+    return tuple(torch.broadcast_shapes(shape_a, shape_b))
 
 
 def sanitize_axis(