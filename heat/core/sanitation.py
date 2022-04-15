eb96bf85d7cbc181441fc43623efcc9e8a5771af
Dhruv454000 <dhruvkunjadiya55@gmail.com>
2022-04-15 12:30:43 +0530

    check for scalar

--- heat/core/sanitation.py
+++ heat/core/sanitation.py
@@ -381,8 +381,10 @@ def scalar_to_1d(x: DNDarray) -> DNDarray:
     x : DNDarray
         with `x.ndim = 0`
     """
-    if not isinstance(x, DNDarray):
-        raise ValueError("input needs to be a ht.DNDarray, but was {}".format(type(x)))
+    if x.ndim != 0:
+        raise ValueError(
+            "Input needs to be a scalar DNDarray,but was found to be {}d DNDarray".format(x.ndim)
+        )
     a = x.larray.unsqueeze(0)
     return DNDarray(
         a,
