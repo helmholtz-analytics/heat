31f6117854ad097dbee17bbf636f9cd4601a3241
kk <k.krajsek@fz-juelich.de>
2022-04-27 13:33:12 +0200

    added convolve2d to heat/core/signal.py

--- heat/core/signal.py
+++ heat/core/signal.py
@@ -10,7 +10,7 @@ from .manipulations import pad
 from .factories import array
 import torch.nn.functional as fc
 
-__all__ = ["convolve"]
+__all__ = ["convolve", "convolve2d"]
 
 
 def genpad(a, signal, pad, split, boundary, fillvalue):
@@ -173,4 +173,143 @@ def convolve(a: DNDarray, v: DNDarray, mode: str = "full") -> DNDarray:
         a.device,
         a.comm,
         balanced=False,
+    ).astype(a.dtype.torch_type())
+
+def convolve2d(a, v, mode="full", boundary='fill', fillvalue=0):
+    """
+    Returns the discrete, linear convolution of two two-dimensional HeAT tensors.
+
+    Parameters
+    ----------
+    a : (N,) ht.tensor
+        Two-dimensional signal HeAT tensor
+    v : (M,) ht.tensor
+        Two-dimensional filter weight HeAT tensor.
+    mode : {'full', 'valid', 'same'}, optional
+        'full':
+          By default, mode is 'full'. This returns the convolution at
+          each point of overlap, with an output shape of (N+M-1,). At
+          the end-points of the convolution, the signals do not overlap
+          completely, and boundary effects may be seen.
+        'same':
+          Mode 'same' returns output  of length 'N'. Boundary
+          effects are still visible. This mode is not supported for
+          even sized filter weights
+        'valid':
+          Mode 'valid' returns output of length 'N-M+1'. The
+          convolution product is only given for points where the signals
+          overlap completely. Values outside the signal boundary have no
+          effect.
+
+    Returns
+    -------
+    out : ht.tensor
+        Discrete, linear convolution of 'a' and 'v'.
+
+    Note : There is  differences to the numpy convolve function:
+        The inputs are not swapped if v is larger than a. The reason is that v needs to be
+        non-splitted. This should not influence performance. If the filter weight is larger
+        than fitting into memory, using the FFT for convolution is recommended.
+
+    Examples
+    --------
+    Note how the convolution operator flips the second array
+    before "sliding" the two across one another:
+    >>> a = ht.ones(10)
+    >>> v = ht.arange(3).astype(ht.float)
+    >>> ht.convolve1D(a, v, mode='full')
+    tensor([0., 1., 3., 3., 3., 3., 2.])
+
+    Only return the middle values of the convolution.
+    Contains boundary effects, where zeros are taken
+    into account:
+    >>> ht.convolve1D(a, v, mode='same')
+    tensor([1., 3., 3., 3., 3.])
+
+    Compute only positions where signal and filter weights
+    completely overlap:
+    >>> ht.convolve1D(a, v, mode='valid')
+    tensor([3., 3., 3.])
+    """
+    if not isinstance(a, dndarray.DNDarray) or not isinstance(v, dndarray.DNDarray):
+        raise TypeError("Signal and filter weight must be of type ht.tensor")
+    if v.split is not None:
+        raise TypeError("Distributed filter weights are not supported")
+    if len(a.shape) != 2 or len(v.shape) != 2:
+        raise ValueError("Only 2 dimensional input tensors are allowed")
+    if a.shape[0] <= v.shape[0] or a.shape[1] <= v.shape[1]:
+        raise ValueError("Filter size must not be larger than signal size")
+    if a.dtype is not v.dtype:
+        raise TypeError("Signal and filter weight must be of same type")
+    if mode == "same" and v.shape[0] % 2 == 0  and v.shape[1] % 2 == 0:
+        raise ValueError("Mode 'same' cannot be use with even sized kernal")
+
+    # compute halo size
+    if a.split == 0 or a.split == None:
+        halo_size = v.shape[0] // 2 
+    else:
+        halo_size = v.shape[1] // 2 
+
+    # fetch halos and store them in a.halo_next/a.halo_prev
+    a.get_halo(halo_size)
+
+    # apply halos to local array
+    signal = a.array_with_halos.clone()
+
+    # check if a local chunk is smaller than the filter size 
+    if a.is_distributed() and signal.size()[0] < v.shape[0]:
+        raise ValueError("Local chunk size is smaller than filter size, this is not supported yet")
+
+    if mode == "full":
+        pad_0 = v.shape[0]-1
+        pad_1 = v.shape[1]-1
+        gshape_0 = v.shape[0] + a.shape[0] - 1
+        gshape_1 = v.shape[1] + a.shape[1] - 1
+        pad = list((pad_0, pad_0, pad_1, pad_1))
+        gshape = list((gshape_0, gshape_1))
+
+    elif mode == "same":
+        pad = list((halo_size,)*4)
+        gshape = a.shape[0]
+
+    elif mode == "valid":
+        pad = list((0,)*4)
+        gshape_0 = a.shape[0] - v.shape[0] + 1
+        gshape_1 = a.shape[1] - v.shape[1] + 1
+        gshape = list((gshape_0, gshape_1))
+
+    else:
+        raise ValueError("Only {'full', 'valid', 'same'} are allowed for mode")
+
+
+    signal.unsqueeze_(0)
+    signal.unsqueeze_(0)
+
+
+    # add padding to the borders according to mode
+    signal = genpad(a, signal, pad, a.split, boundary, fillvalue)
+
+    
+    # make signal and filter weight 4D for Pytorch conv2d function
+    #signal.unsqueeze_(0)
+    #signal.unsqueeze_(0)
+
+    # flip filter for convolution as PyTorch conv2d computes correlation
+    weight = torch.flip(v._DNDarray__array.clone(), [0, 1])
+
+    weight.unsqueeze_(0)
+    weight.unsqueeze_(0)
+
+    # apply torch convolution operator
+    signal_filtered = fc.conv2d(signal, weight)
+
+    # unpack 3D result into 1D
+    signal_filtered = signal_filtered[0, 0, :]
+
+    # if kernel shape along split axis is even we need to get rid of duplicated values
+    if a.comm.rank != 0 and v.shape[0] % 2 == 0:
+        signal_filtered = signal_filtered[1:]
+
+    return dndarray.DNDarray(
+        signal_filtered.contiguous(), (gshape,), signal_filtered.dtype, a.split, a.device, a.comm, a.balanced
     ).astype(a.dtype.torch_type())
