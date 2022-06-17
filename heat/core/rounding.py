Fix bug causing precision loss in `abs`
In cases involving large numbers where the `dtype` parameter was
unchanged, precision loss occurred due to intermediate casting
to float32 (in `__local_op`) before casting back to the original
`dtype`.

Fixed by not doing the intermediate casting.

heat/core/rounding.py
@@ -52,7 +52,7 @@ def abs(
     if dtype is not None and not issubclass(dtype, dtype):
         raise TypeError("dtype must be a heat data type")
 
-    absolute_values = _operations.__local_op(torch.abs, x, out)
+    absolute_values = _operations.__local_op(torch.abs, x, out, no_cast=True)
     if dtype is not None:
         absolute_values.larray = absolute_values.larray.type(dtype.torch_type())
         absolute_values._DNDarray__dtype = dtype
@@ -181,7 +181,11 @@ def fabs(x: DNDarray, out: Optional[DNDarray] = None) -> DNDarray:
         If not provided or ``None``, a freshly-allocated array is returned.
 
     """
-    return abs(x, out, dtype=None)
+    if isinstance(x, DNDarray):
+        dtype = types.promote_types(x.dtype, types.float32)
+    else:
+        dtype = None
+    return abs(x, out, dtype=dtype)
 
 
 DNDarray.fabs: Callable[[DNDarray, Optional[DNDarray]], DNDarray] = lambda self, out=None: fabs(