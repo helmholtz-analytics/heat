aeb97e49d81e9b738736cc67be1af39a4a27b5c6
neosunhan <neosunhan@gmail.com>
2022-06-18 15:28:52 +0800

    Implement `greater_equal`

--- heat/array_api/_array_object.py
+++ heat/array_api/_array_object.py
@@ -281,7 +281,7 @@ class Array:
         other = self._check_allowed_dtypes(other, "numeric", "__ge__")
         if other is NotImplemented:
             return other
-        # self, other = self._normalize_two_args(self, other)
+        self, other = self._normalize_two_args(self, other)
         res = self._array.__ge__(other._array)
         return self.__class__._new(res)
 
--- heat/array_api/_elementwise_functions.py
+++ heat/array_api/_elementwise_functions.py
@@ -219,6 +219,28 @@ def floor_divide(x1: Array, x2: Array, /) -> Array:
     return Array._new(ht.floor_divide(x1._array, x2._array))
 
 
+def greater_equal(x1: Array, x2: Array, /) -> Array:
+    """
+    Computes the truth value of ``x1_i >= x2_i`` for each element ``x1_i`` of
+    the input array ``x1`` with the respective element ``x2_i`` of the input
+    array ``x2``.
+
+    Parameters
+    ----------
+    x1 : Array
+        First input array. Must have a numeric data type.
+    x2 : Array
+        Second input array. Must be compatible with ``x1`` and have a numeric
+        data type.
+    """
+    if x1.dtype not in _numeric_dtypes or x2.dtype not in _numeric_dtypes:
+        raise TypeError("Only numeric dtypes are allowed in greater_equal")
+    # Call result type here just to raise on disallowed type combinations
+    _result_type(x1.dtype, x2.dtype)
+    x1, x2 = Array._normalize_two_args(x1, x2)
+    return Array._new(ht.greater_equal(x1._array, x2._array))
+
+
 def isfinite(x: Array, /) -> Array:
     """
     Tests each element ``x_i`` of the input array ``x`` to determine if finite
