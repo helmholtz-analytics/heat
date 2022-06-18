0c93d3063ff6430e13a83cedc5fbf517a7aeada2
neosunhan <neosunhan@gmail.com>
2022-06-18 16:03:33 +0800

    Implement `divide`

--- heat/array_api/_elementwise_functions.py
+++ heat/array_api/_elementwise_functions.py
@@ -4,6 +4,7 @@ from ._dtypes import (
     _numeric_dtypes,
     _integer_dtypes,
     _integer_or_boolean_dtypes,
+    _floating_dtypes,
     _result_type,
 )
 from ._array_object import Array
@@ -175,6 +176,27 @@ def bitwise_xor(x1: Array, x2: Array, /) -> Array:
     _result_type(x1.dtype, x2.dtype)
     x1, x2 = Array._normalize_two_args(x1, x2)
     return Array._new(ht.bitwise_xor(x1._array, x2._array))
+
+
+def divide(x1: Array, x2: Array, /) -> Array:
+    """
+    Calculates the division for each element ``x1_i`` of the input array ``x1``
+    with the respective element ``x2_i`` of the input array ``x2``.
+
+    Parameters
+    ----------
+    x1 : Array
+        Dividend input array. Must have a numeric data type.
+    x2 : Array
+        Divisor input array. Must be compatible with ``x1`` and have a numeric
+        data type.
+    """
+    if x1.dtype not in _floating_dtypes or x2.dtype not in _floating_dtypes:
+        raise TypeError("Only floating-point dtypes are allowed in divide")
+    # Call result type here just to raise on disallowed type combinations
+    _result_type(x1.dtype, x2.dtype)
+    x1, x2 = Array._normalize_two_args(x1, x2)
+    return Array._new(ht.divide(x1._array, x2._array))
 
 
 def equal(x1: Array, x2: Array, /) -> Array:
