Fix bug causing precision loss in `arange`

heat/core/factories.py
@@ -139,10 +139,8 @@ def arange(
     # compose the local tensor
     start += offset * step
     stop = start + lshape[0] * step
-    data = torch.arange(start, stop, step, device=device.torch_device)
-
     htype = types.canonical_heat_type(dtype)
-    data = data.type(htype.torch_type())
+    data = torch.arange(start, stop, step, dtype=htype.torch_type(), device=device.torch_device)
 
     return DNDarray(data, gshape, htype, split, device, comm, balanced)
 