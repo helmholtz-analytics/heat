16678482553394c8bd17956469de88edb8c3bca1
AmirTouilPU <mtouil@princeton.edu>
2022-07-11 20:30:38 +0100

    coo_sparse

--- heat/core/coo_matrix.py
+++ heat/core/coo_matrix.py
@@ -1,10 +1,10 @@
-"""Missing docstring in public module"""
 from __future__ import annotations
 
 import math
 import numpy as np
 import torch
 import warnings
+import heat as ht
 
 from inspect import stack
 from mpi4py import MPI
@@ -12,16 +12,20 @@ from pathlib import Path
 from typing import List, Union, Tuple, TypeVar, Optional
 from .dndarray import DNDarray
 
-__all__ = ["CooMatrix"]
+__all__ = ["coo_matrix"]
 
 Communication = TypeVar("Communication")
 
+def indices(gshape, obj, split, comm) -> coo_matrix:
+    start, _, _ = comm.chunk(gshape, split=split)
+    t_indices = obj.coalesce().indices()
+    t_indices[split] += start
+    global_indices = ht.array(t_indices, is_split=1)
+    return global_indices   
 
-class CooMatrix:
-    """Missing docstring in public class"""
-
+class coo_matrix():
     def __init__(
-        self,
+        self, 
         array: torch.sparse_coo_tensor,
         gshape: Tuple[int, ...],
         dtype: datatype,
@@ -33,20 +37,33 @@ class CooMatrix:
     ):
         self.__gshape = gshape
         self.__dtype = dtype
-        self.__ndim = array.ndim
         self.__split = split
         self.__device = device
         self.__comm = comm
         self.__balanced = balanced
         self.__lnnz = array._nnz()
         self.__gnnz = gnnz
-        # TODO: indices need to include explicit zeros
-        self.__indices = array.nonzero(as_tuple=True)
-        print(self.__indices)
-        print("here")
-        self.__data = array[self.__indices]
-        print(self.__data)
-        self.has_canonical_format = True
+        self.__indices = indices(gshape,array,split, comm)
+        #TODO: indices need to include explicit zeros       
+        # create 
+        # .coalesce() 
+        # self.__indices = array.nonzero(as_tuple=True)
+        # print(self.__indices)
+        # print("here")
+        # self.__data = array[self.__indices]
+        # print(self.__data)
+        self.has_canonical_format = True 
+
+    @property
+    def indices(self) -> coo_matrix:
+        return self.__indices
+
+    @property
+    def ndim(self) -> int:
+        """
+        Number of dimensions of the ``coo_array``
+        """
+        return len(self.__gshape)
 
 
 from . import complex_math
