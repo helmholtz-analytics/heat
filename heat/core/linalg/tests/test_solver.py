9b82c9ec2a0113cdc5da5924f0eee0ca3fd48801
Claudia Comito <c.comito@fz-juelich.de>
2022-11-10 06:09:43 +0100

    Add correctness test for Lanczos

--- heat/core/linalg/tests/test_solver.py
+++ heat/core/linalg/tests/test_solver.py
@@ -31,43 +31,27 @@ class TestSolver(TestCase):
             ht.linalg.cg(A, b, A)
 
     def test_lanczos(self):
-        # define time and space domains
-        x = np.linspace(-10, 10, 100)
-        t = np.linspace(0, 6 * np.pi, 800)
-        # dt = t[2] - t[1]
-        Xm, Tm = np.meshgrid(x, t)
+        # define positive definite matrix (n,n), split = 0
+        n = 10
+        A = ht.random.randn(n, n, dtype=ht.float64, split=0)
+        B = A @ A.T
+        # Lanczos decomposition with iterations m = n
+        V, T = ht.lanczos(B, m=n)
+        # V must be unitary
+        V_inv = ht.linalg.inv(V)
+        self.assertTrue(ht.allclose(V_inv, V.T))
+        # V T V.T must be = B, V transposed = V inverse
+        lanczos_B = V @ T @ V_inv
+        self.assertTrue(ht.allclose(lanczos_B, B))
 
-        # create three spatiotemporal patterns
-        r = 3
-        f1 = np.multiply(20 - 0.2 * np.power(Xm, 2), np.exp((2.3j) * Tm))
-        f2 = np.multiply(Xm, np.exp(0.6j * Tm))
-        f3 = np.multiply(
-            5 * np.multiply(1 / np.cosh(Xm / 2), np.tanh(Xm / 2)), 2 * np.exp((0.1 + 2.8j) * Tm)
-        )
-
-        # combine signals and make data matrix
-        D = (f1 + f2 + f3).T
-
-        # generarte correlation matrix
-        CorrMatrix = D.real @ D.real.T
-
-        # Manual SVD on Correlation Matrix
-        SigSquare, U = np.linalg.eig(CorrMatrix)
-        idxs = SigSquare.argsort()[::-1]
-        SigSquare_, U_ = SigSquare[idxs][:r], U[:, idxs][:, :r]
-
-        # with Lanczos
-        ht_CorrMatrix = ht.array(CorrMatrix, dtype=ht.float64, split=0)
-        ht_V, ht_T = ht.lanczos(ht_CorrMatrix, m=6)
-        np_T = ht_T.numpy()
-        np_V = ht_V.numpy()
-        lanczos_vals, lanczos_vecs = np.linalg.eig(np_T)
-        lanczos_idxs = lanczos_vals.argsort()[::-1]
-        lanczos_vals_ = lanczos_vals[lanczos_idxs][:r]
-        lanczos_vecs_ = lanczos_vecs[:, lanczos_idxs][:, :r]
-
-        eigen_vecs_ = np_V @ lanczos_vecs_
-        eigen_vecs_ = eigen_vecs_[:, :r]
-
-        self.assertTrue(np.allclose(SigSquare_, lanczos_vals_, atol=1e-7))
-        self.assertTrue(np.allclose(U_.real, eigen_vecs_, atol=1e-7))
+        # define positive definite matrix (n,n), split = 1
+        A = ht.random.randn(n, n, dtype=ht.float64, split=1)
+        B = A @ A.T
+        # Lanczos decomposition with iterations m = n
+        V, T = ht.lanczos(B, m=n)
+        # V must be unitary
+        V_inv = ht.linalg.inv(V)
+        self.assertTrue(ht.allclose(V_inv, V.T))
+        # V T V.T must be = B, V transposed = V inverse
+        lanczos_B = V @ T @ V_inv
+        self.assertTrue(ht.allclose(lanczos_B, B))
