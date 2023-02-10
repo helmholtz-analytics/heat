bf10b29106e1dde6f4f70e847154546a47ba4829
Hoppe <mrhf92@gmail.com>
2023-02-10 16:24:11 +0100

    corrected a small error when building the average of the measured times

--- performance_monitoring/template/my_script.py
+++ performance_monitoring/template/my_script.py
@@ -77,7 +77,7 @@ def do_one_measurement(key):
 for k in range(num_measurements):
     for s in range(num_samples):
         measurements[k] += do_one_measurement(measurement_keys[k])
-    measurements[k] /= s
+    measurements[k] /= num_measurements
 
 # print and save the data
 if MPI.COMM_WORLD.rank == 0:
