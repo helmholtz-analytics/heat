eed7ffb66994583256b724f26d747fd7dfc50283
Sai Suraj <ce20b031@iittp.ac.in>
2023-05-08 01:11:16 +0530

    Reformatted some code in a few files to make it more cleaner.

--- benchmarks/cb/linalg.py
+++ benchmarks/cb/linalg.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 import heat as ht
+import itertools
 from perun.decorator import monitor
 
 
@@ -19,10 +20,9 @@ def matmul_cpu_split_1(n: int = 3000):
 
 @monitor()
 def qr_cpu(n: int = 2000):
-    for t in range(1, 3):
-        for sp in range(2):
-            a = ht.random.random((n, n), split=sp)
-            qr = a.qr(tiles_per_proc=t)
+    for t, sp in itertools.product(range(1, 3), range(2)):
+        a = ht.random.random((n, n), split=sp)
+        qr = a.qr(tiles_per_proc=t)
 
 
 @monitor()
