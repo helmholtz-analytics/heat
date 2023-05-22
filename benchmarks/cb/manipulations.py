b0862605f4edf3fb23e17cb5b85b45c27d683820
Hoppe <mrhf92@gmail.com>
2023-05-22 14:57:03 +0200

    some changes in the continuous benchmarks for linalg: I tried to separate the creation of test data from the actual tests

--- benchmarks/cb/manipulations.py
+++ benchmarks/cb/manipulations.py
@@ -1,14 +1,15 @@
 # flake8: noqa
 import heat as ht
-from perun.decorator import monitor
+from perun import monitor
 
 
 @monitor()
-def reshape_cpu():
+def reshape():
     sizes = [10000, 20000, 40000]
     for size in sizes:
         st = ht.zeros((1000, size), split=1)
         a = ht.reshape(st, (10000000, -1), new_split=1)
 
 
-reshape_cpu()
+def run_manipulation_benchmarks():
+    reshape()
