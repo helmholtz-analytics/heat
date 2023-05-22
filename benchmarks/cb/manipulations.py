cc4faa293ca03c5ade982132f077e8205620d4ca
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2023-05-22 15:34:48 +0200

    only time reshape function, not array creation

--- benchmarks/cb/manipulations.py
+++ benchmarks/cb/manipulations.py
@@ -4,12 +4,14 @@ from perun import monitor
 
 
 @monitor()
-def reshape():
-    sizes = [10000, 20000, 40000]
-    for size in sizes:
-        st = ht.zeros((1000, size), split=1)
-        a = ht.reshape(st, (10000000, -1), new_split=1)
-
+def reshape(arrays):
+    for array in arrays:
+        a = ht.reshape(array, (10000000, -1), new_split=1)
 
 def run_manipulation_benchmarks():
-    reshape()
+    sizes = [10000, 20000, 40000]
+    arrays = []
+    for size in sizes:
+        arrays.append(ht.zeros((1000, size), split=1))
+
+    reshape(arrays)
