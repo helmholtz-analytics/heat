06e20b3654ab26402ae357c63dbee92a9631ce88
Fabian Hoppe <112093564+mrfh92@users.noreply.github.com>
2023-06-02 10:55:27 +0200

    Update heat/core/linalg/svd.py
    Co-authored-by: Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>

--- heat/core/linalg/svd.py
+++ heat/core/linalg/svd.py
@@ -12,5 +12,5 @@ def svd(A: DNDarray) -> Tuple[DNDarray, DNDarray, DNDarray]:
     The intended functionality is similar to `numpy.linalg.svd`, but of-course allowing for distributed-memory parallelization and GPU-support.
     """
     raise NotImplementedError(
-        "Currently, a 'full' (i.e. non-trucated and non-approximate) SVD is not available in Heat. Please consider using `heat.linalg.hsvd_rank` or `heat.linalg.hsvd_rtol` for computing an approximate, truncated (w.r.t. rank or relative tolerance) SVD instead. Alternatively, you may also check on github whether the implementation of the full SVD is meanwhile available in a development branch of Heat."
+        " Memory-distributed 'full' (i.e. non-trucated and non-approximate) SVD not implemented yet. Consider using `heat.linalg.hsvd` for an approximate, truncated  SVD instead."
     )
