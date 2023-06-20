e3bd1d227f884b9d47b1bbed589f40ad5983d468
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2023-06-20 09:07:05 +0200

    Implement MPS-friendly choice of dtypes

--- heat/core/linalg/tests/test_solver.py
+++ heat/core/linalg/tests/test_solver.py
@@ -31,40 +31,6 @@ class TestSolver(TestCase):
             ht.linalg.cg(A, b, A)
 
     def test_lanczos(self):
-        # define positive definite matrix (n,n), split = 0
-        n = 100
-        A = ht.random.randn(n, n, dtype=ht.float64, split=0)
-        B = A @ A.T
-        # Lanczos decomposition with iterations m = n
-        V, T = ht.lanczos(B, m=n)
-        self.assertTrue(V.dtype is B.dtype)
-        self.assertTrue(T.dtype is B.dtype)
-        # V must be unitary
-        V_inv = ht.linalg.inv(V)
-        self.assertTrue(ht.allclose(V_inv, V.T))
-        # V T V.T must be = B, V transposed = V inverse
-        lanczos_B = V @ T @ V_inv
-        self.assertTrue(ht.allclose(lanczos_B, B))
-
-        # complex128, output buffers
-        A = (
-            ht.random.rand(n, n, dtype=ht.float64, split=0)
-            + ht.random.rand(n, n, dtype=ht.float64, split=0) * 1j
-        )
-        A_conj = ht.conj(A)
-        B = A @ A_conj.T
-        m = n
-        V_out = ht.zeros((n, m), dtype=B.dtype, split=B.split, device=B.device, comm=B.comm)
-        T_out = ht.zeros((m, m), dtype=ht.float64, device=B.device, comm=B.comm)
-        # Lanczos decomposition with iterations m = n
-        ht.lanczos(B, m=m, V_out=V_out, T_out=T_out)
-        # V must be unitary
-        V_inv = ht.linalg.inv(V_out)
-        self.assertTrue(ht.allclose(V_inv, ht.conj(V_out).T))
-        # V T V* must be = B, V conjugate transpose = V inverse
-        lanczos_B = V_out @ T_out @ V_inv
-        self.assertTrue(ht.allclose(lanczos_B, B))
-
         # single precision tolerance
         if (
             int(torch.__version__.split(".")[0]) == 1
@@ -74,6 +40,48 @@ class TestSolver(TestCase):
             tolerance = 1e-3
         else:
             tolerance = 1e-4
+
+        is_mps = (
+            ht.get_device().device_type.startswith("gpu")
+            and torch.backends.mps.is_built()
+            and torch.backends.mps.is_available()
+        )
+        dtype, atol = (ht.float32, tolerance) if is_mps else (ht.float64, 1e-12)
+
+        # define positive definite matrix (n,n), split = 0
+        n = 100
+        A = ht.random.randn(n, n, dtype=dtype, split=0)
+        B = A @ A.T
+        # Lanczos decomposition with iterations m = n
+        V, T = ht.lanczos(B, m=n)
+        self.assertTrue(V.dtype is B.dtype)
+        self.assertTrue(T.dtype is B.dtype)
+        # V must be unitary
+        V_inv = ht.linalg.inv(V)
+        self.assertTrue(ht.allclose(V_inv, V.T, atol=atol))
+        # V T V.T must be = B, V transposed = V inverse
+        lanczos_B = V @ T @ V_inv
+        self.assertTrue(ht.allclose(lanczos_B, B, atol=atol))
+
+        # complex128, output buffers
+        if not is_mps:
+            A = (
+                ht.random.rand(n, n, dtype=ht.float64, split=0)
+                + ht.random.rand(n, n, dtype=ht.float64, split=0) * 1j
+            )
+            A_conj = ht.conj(A)
+            B = A @ A_conj.T
+            m = n
+            V_out = ht.zeros((n, m), dtype=B.dtype, split=B.split, device=B.device, comm=B.comm)
+            T_out = ht.zeros((m, m), dtype=ht.float64, device=B.device, comm=B.comm)
+            # Lanczos decomposition with iterations m = n
+            ht.lanczos(B, m=m, V_out=V_out, T_out=T_out)
+            # V must be unitary
+            V_inv = ht.linalg.inv(V_out)
+            self.assertTrue(ht.allclose(V_inv, ht.conj(V_out).T))
+            # V T V* must be = B, V conjugate transpose = V inverse
+            lanczos_B = V_out @ T_out @ V_inv
+            self.assertTrue(ht.allclose(lanczos_B, B))
 
         # float32, pre_defined v0, split mismatch
         A = ht.random.randn(n, n, dtype=ht.float32, split=0)
@@ -86,44 +94,45 @@ class TestSolver(TestCase):
         self.assertTrue(T.dtype is B.dtype)
         # V must be unitary
         V_inv = ht.linalg.inv(V)
-        self.assertTrue(ht.allclose(V_inv, V.T, atol=tolerance))
+        self.assertTrue(ht.allclose(V_inv, V.T, atol=atol))
         # V T V.T must be = B, V transposed = V inverse
         lanczos_B = V @ T @ V_inv
-        self.assertTrue(ht.allclose(lanczos_B, B, atol=tolerance))
+        self.assertTrue(ht.allclose(lanczos_B, B, atol=atol))
 
         # complex64
-        A = (
-            ht.random.randn(n, n, dtype=ht.float32, split=0)
-            + ht.random.randn(n, n, dtype=ht.float32, split=0) * 1j
-        )
-        A_conj = ht.conj(A)
-        B = A @ A_conj.T
-        # Lanczos decomposition with iterations m = n
-        V, T = ht.lanczos(B, m=n)
-        # V must be unitary
-        # V T V* must be = B, V conjugate transpose = V inverse
-        V_conj = ht.conj(V)
-        lanczos_B = V @ T @ V_conj.T
-        self.assertTrue(ht.allclose(lanczos_B, B, atol=tolerance))
+        if not is_mps:
+            A = (
+                ht.random.randn(n, n, dtype=ht.float32, split=0)
+                + ht.random.randn(n, n, dtype=ht.float32, split=0) * 1j
+            )
+            A_conj = ht.conj(A)
+            B = A @ A_conj.T
+            # Lanczos decomposition with iterations m = n
+            V, T = ht.lanczos(B, m=n)
+            # V must be unitary
+            # V T V* must be = B, V conjugate transpose = V inverse
+            V_conj = ht.conj(V)
+            lanczos_B = V @ T @ V_conj.T
+            self.assertTrue(ht.allclose(lanczos_B, B, atol=tolerance))
 
         # non-distributed
-        A = ht.random.randn(n, n, dtype=ht.float64, split=None)
+        A = ht.random.randn(n, n, dtype=dtype, split=None)
         B = A @ A.T
         # Lanczos decomposition with iterations m = n
         m = n
         V_out = ht.zeros((n, m), dtype=B.dtype, split=B.split, device=B.device, comm=B.comm)
-        T_out = ht.zeros((m, m), dtype=ht.float64, device=B.device, comm=B.comm)
+        T_out = ht.zeros((m, m), dtype=dtype, device=B.device, comm=B.comm)
         ht.lanczos(B, m=m, V_out=V_out, T_out=T_out)
         self.assertTrue(V_out.dtype is B.dtype)
         self.assertTrue(T_out.dtype is B.real.dtype)
         # V must be unitary
         V_inv = ht.linalg.inv(V_out)
-        self.assertTrue(ht.allclose(V_inv, V_out.T))
+        self.assertTrue(ht.allclose(V_inv, V_out.T, atol=atol))
         # without output buffers
         V, T = ht.lanczos(B, m=m)
         # V T V.T must be = B, V transposed = V inverse
         lanczos_B = V @ T @ V.T
-        self.assertTrue(ht.allclose(lanczos_B, B))
+        self.assertTrue(ht.allclose(lanczos_B, B, atol=atol))
 
         with self.assertRaises(TypeError):
             V, T = ht.lanczos(B, m="3")
