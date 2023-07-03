ccb3b5b579dee26b1cb63728a58c9a41318fe552
Hoppe <mrhf92@gmail.com>
2023-07-03 12:58:42 +0200

    ...

--- heat/core/printing.py
+++ heat/core/printing.py
@@ -269,14 +269,13 @@ def _torch_data(dndarray, summarize) -> DNDarray:
                     )
         # exchange data
         exchange_sizes = dndarray.comm.gather(torch.tensor(data.shape))
-        print(dndarray.comm.rank, exchange_sizes)
         if dndarray.comm.rank == 0:
             recv_size = exchange_sizes[0]
             recv_size[dndarray.split] = sum([s[dndarray.split] for s in exchange_sizes])
             recv_buf = torch.zeros(tuple(recv_size), dtype=data.dtype, device=data.device)
         else:
-            recv_buf = torch.zeros(1)
-        dndarray.comm.Gather(data, recv_buf)
+            recv_buf = None
+        dndarray.comm.Gather(data, recv_buf, axis=dndarray.split, recv_axis=dndarray.split)
         print(dndarray.comm.rank, recv_buf)
 
         received = dndarray.comm.gather(data)
