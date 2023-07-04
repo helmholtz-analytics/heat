c95fee25e299066d9f0ea1547c253fed511682ab
Hoppe <mrhf92@gmail.com>
2023-07-04 14:57:20 +0200

    merged main...

--- heat/core/dxarray.py
+++ heat/core/dxarray.py
@@ -5,21 +5,21 @@ Implements a distributed counterpart of xarray built on top of Heats DNDarray cl
 import heat as ht
 
 
-class Dxarray:
+class DXarray:
     """
     Distributed counterpart of xarray.
 
     Parameters
     --------------
     dataarray: DNDarray
-        entries of the xarray
+        data entries of the DXarray
     coords: dictionary
         coordinates
-        entries of the dictionary have the form `dim`:`coords_of_dim` for each `dim` in `dims`, where `coords_of_dim` can either be a list of coordinate labels ("logical coordinates") or an Dxarray of same shape as the original one, also split along the same split axis ("physical coordinates")
+        entries of the dictionary have the form `dim`:`coords_of_dim` for each `dim` in `dims`, where `coords_of_dim` can either be a list of coordinate labels ("logical coordinates") or an DXarray of same shape as the original one, also split along the same split axis ("physical coordinates")
     dims: List
-        names of the dimensions
+        names of the dimensions of the DXarray
     split: Union[int,None]
-        split dimension of the Dxarray (analogous to split dimension of DNDarray)
+        split dimension of the DXarray (analogous to split dimension of DNDarray)
     """
 
     # TODO: @properties ...
