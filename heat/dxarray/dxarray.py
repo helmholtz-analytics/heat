afe9005d0fa3bafe64d469fee68f9f84153d8bb3
Hoppe <mrhf92@gmail.com>
2023-07-06 12:17:15 +0200

    some little progress in implementation of DXarray

--- heat/dxarray/dxarray.py
+++ heat/dxarray/dxarray.py
@@ -33,7 +33,7 @@ class DXarray:
         self,
         values: ht.DNDarray,
         dims: Union[list, None],
-        coords: dict,
+        coords: Union[dict, None],
         name: Union[str, None] = None,
         attrs: dict = {},
     ):
@@ -41,6 +41,8 @@ class DXarray:
         self.__name = name
         self.__attrs = attrs
 
+        # check if names of dims are given (and whether their number fits the number of dims of the values array)
+        # if no names are provided, introduce generic names "dim_N", N = 0,1,...
         if dims is not None:
             assert len(self.__dims) == self.__values.ndim
             self.__dims = dims
@@ -48,23 +50,58 @@ class DXarray:
             self.__dims = ["dim_%d" % k for k in range(self.__values.ndim)]
         self.__dims = dims
 
+        # set attribute split: use dimension name instead of idx since we are in class DXarray instead of DNDarray
+        self.__split = self.__dim_idx_to_name(values.split)
+
+        # check consistency of the coordinates provided
+        if coords is not None:
+            # go through all entries in the dictionary coords
+            for coord_item in coords.items():
+                coord_item_dims = coord_item[0]
+                coord_item_coords = coord_item[1]
+                # first case: "classical" coordinates for a single dimension, sometimes referred to "logical coordinates"
+                if isinstance(coord_item_dims, str):
+                    # here, the coordinates must be given by a one-dimensional DNDarray...
+                    assert isinstance(coord_item_coords, ht.DNDarray)
+                    assert coord_item_coords.ndim == 1
+                    # ... with matching device and communicator, ...
+                    assert coord_item_coords.device == self.__values.device
+                    assert coord_item_coords.comm == self.__values.comm
+                    # ... correct shape, and ...
+                    assert (
+                        coord_item_coords.gshape[0]
+                        == self.__values.gshape[self.__dim_name_to_idx(coord_item_dims)]
+                    )
+                    # ... that is split if and only if the coordinates refer to the split dimension of the DXarray
+                    if coord_item_dims == self.__split:
+                        assert coord_item_coords.split == 0
+                    else:
+                        assert coord_item_coords.split is None
+                # second case: "physical coordinates" - two or more dimensions are "merged" together and equipped with a coordinate array
+                # that cannot be expressed as meshgrid of 1d coordinate arrays
+                elif isinstance(coord_item_dims, tuple(int)):
+                    # now, the coordinates must be given as a DXarray...
+                    assert isinstance(coord_item_coords, ht.DXarray)
+                    # ... with matching dimension names, ...
+                    assert coord_item_coords.dims == list(coord_item_dims)
+                    # ... shape, ...
+                    assert (
+                        coord_item_coords.values.gshape
+                        == self.__values.gshape[self.__dim_name_to_idx(list(coord_item_dims))]
+                    )
+                    # ... device and communicator, ...
+                    assert coord_item_coords.device == self.__values.device
+                    assert coord_item_coords.comm == self.__values.comm
+                    # ... and split dimension.
+                    if self.__split in coord_item_dims:
+                        assert coord_item_coords.split == self.__split
+                    else:
+                        assert coord_item_coords.split is None
+
+        # after the consistency checks, set the remaining attributes of the DXarray
         self.__coords = coords
-        self.__split = values.split
         self.__device = values.device
         self.__comm = values.comm
-
-        for coord_item in coords.items():
-            if coord_item[1] is not None:
-                assert (
-                    isinstance(coord_item[1], ht.DNDarray)
-                    and coord_item[1].device == self.__values.device
-                    and coord_item[1].comm == self.__values.comm
-                )
-                if coord_item[1].split is not None:
-                    0 == 1
-                    # ensure correct split dim...
-        # TODO: we need to introduce some additional consistency checks: compare communicator, devices, and split-dimension of DNDarrays contained in coords and of values...
-        #     physical coordinate arrays must be split along same dim as values...
 
     @property
     def values(self) -> ht.DNDarray:
@@ -143,3 +180,37 @@ class DXarray:
         Set attributes of DXarray
         """
         self.__attrs = attributes
+
+    def __dim_name_to_idx(self, names: Union[str, tuple, list, None]):
+        """
+        Converts a string (or tuple of strings) referring to dimensions of the DXarray to the corresponding numeric index (tuple of indices) of these dimensions.
+        Inverse of :meth:`__dim_idx_to_name`.
+        """
+        if names is None:
+            return None
+        elif isinstance(names, str):
+            return self.__dims.index(names)
+        elif isinstance(names, tuple):
+            names_list = list(names)
+            return tuple([self.__dims.index(names) for name in names_list])
+        elif isinstance(names, list):
+            return tuple([self.__dims.index(names) for name in names])
+        else:
+            raise TypeError("Input must be None, string, list of strings, or tuple of strings.")
+
+    def __dim_idx_to_name(self, idxs: Union[int, tuple, list, None]):
+        """
+        Converts an numeric index (or tuple of such indices) referring to the dimensions of the DXarray to the corresponding name string (or tuple of name strings).
+        Inverse of :meth:`__dim_name_to_idx`.
+        """
+        if idxs is None:
+            return None
+        elif isinstance(self, idxs):
+            return self.__dims[idxs]
+        elif isinstance(idxs, tuple):
+            idxs_list = list(idxs)
+            return tuple([self.__dims[idx] for idx in idxs_list])
+        elif isinstance(idxs, list):
+            return tuple([self.__dims[idx] for idx in idxs])
+        else:
+            raise TypeError("Input must be None, int, list of ints, or tuple of ints.")
