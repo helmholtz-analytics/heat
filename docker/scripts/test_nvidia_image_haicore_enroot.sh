e0467082a5e9ce4509db25a05dff1ab5b43314ad
Juan Pedro GHM <juan.muriedas@kit.edu>
2023-07-13 11:27:21 +0200

    chore: docker helper scripts

---
+++ docker/scripts/test_nvidia_image_haicore_enroot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clear environment, else mpi4py will fail to install.
+ml purge
+
+SBATCH_PARAMS=(
+	--partition 	   normal
+	--time      	   00:10:00
+	--nodes     	   1
+	--tasks-per-node   1
+	--gres		   gpu:1
+	--container-image  ~/containers/nvidia+pytorch+23.05-py3.sqsh
+	--container-writable
+	--container-mounts /etc/slurm/task_prolog.hk:/etc/slurm/task_prolog.hk,/scratch:/scratch
+	--container-mount-home
+)
+
+sbatch "${SBATCH_PARAMS[@]}" ./install_print_test.sh
