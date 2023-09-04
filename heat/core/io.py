a75f4016bf32a8aa3ebe78b5c69e17a7e757e91e
Hoppe <mrhf92@gmail.com>
2023-09-04 11:52:12 +0200

    removed some bugs due to the changes above

--- heat/core/io.py
+++ heat/core/io.py
@@ -74,7 +74,7 @@ else:
             Name of the dataset to be read.
         dtype : datatype, optional
             Data type of the resulting array.
-        load_fraction : float (between 0. and 1.), default is 1.
+        load_fraction : float between 0. (excluded) and 1. (included), default is 1.
             if 1. (default), the whole dataset is loaded from the file specified in path
             else, the dataset is loaded partially, with the fraction of the dataset (along the split axis) specified by load_fraction
             If split is None, load_fraction is automatically set to 1., i.e. the whole dataset is loaded.
@@ -113,17 +113,13 @@ else:
             raise TypeError("dataset must be str, not {}".format(type(dataset)))
         elif split is not None and not isinstance(split, int):
             raise TypeError(f"split must be None or int, not {type(split)}")
-        if split is None:
-            load_fraction = 1.0  # if no split is set, load the whole dataset
+
+        if not isinstance(load_fraction, float):
+            raise TypeError(f"load_fraction must be float, but is {type(load_fraction)}")
         else:
-            if isinstance(load_fraction, float):
-                if load_fraction <= 0.0 or load_fraction >= 1.0:
-                    raise ValueError(
-                        f"load_fraction must be strictly between 0. and 1., but is {load_fraction}."
-                    )
-            else:
-                raise TypeError(
-                    f"load_fraction must be None or float, but is {type(load_fraction)}"
+            if split is not None and (load_fraction <= 0.0 or load_fraction > 1.0):
+                raise ValueError(
+                    f"load_fraction must be between 0. (excluded) and 1. (included), but is {load_fraction}."
                 )
 
         # infer the type and communicator for the loaded array
@@ -136,9 +132,10 @@ else:
         with h5py.File(path, "r") as handle:
             data = handle[dataset]
             gshape = data.shape
-            gshape = np.array(gshape)
-            gshape[split] = int(gshape[split] * load_fraction)
-            gshape = tuple(gshape)
+            if split is not None:
+                gshape = np.array(gshape)
+                gshape[split] = int(gshape[split] * load_fraction)
+                gshape = tuple(gshape)
             dims = len(gshape)
             split = sanitize_axis(gshape, split)
             _, _, indices = comm.chunk(gshape, split)
