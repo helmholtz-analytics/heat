05af2441dd285efe6635fdd56cde92cb1daf3b56
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2023-09-14 10:44:56 +0200

    add fft tests first draft

---
+++ heat/fft/tests/test_fft.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+
+import heat as ht
+from heat.core.tests.test_suites.basic_test import TestCase
+
+
+class TestFFT(TestCase):
+    def test_fft(self):
+        pass
+
+    def test_ifft(self):
+        pass
+
+    def test_rfft(self):
+        pass
+
+    def test_irfft(self):
+        pass
+
+    def test_fft2(self):
+        pass
+
+    def test_ifft2(self):
+        pass
+
+    def test_rfft2(self):
+        pass
+
+    def test_irfft2(self):
+        pass
+
+    def test_fftn(self):
+        pass
+
+    def test_ifftn(self):
+        pass
+
+    def test_rfftn(self):
+        pass
+
+    def test_irfftn(self):
+        pass
