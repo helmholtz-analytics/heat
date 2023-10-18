02b0a296d4c21f760e063b7575337050a4e8a090
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2023-10-18 17:24:47 +0200

    replace == with allclose for 2D FFTs

--- heat/fft/tests/test_fft.py
+++ heat/fft/tests/test_fft.py
@@ -79,7 +79,7 @@ class TestFFT(TestCase):
         np_y = np.fft.fft2(x.numpy(), axes=axes)
         self.assertEqual(y.shape, np_y.shape)
         self.assertTrue(y.split == 0)
-        self.assert_array_equal(y, np_y)
+        self.assertTrue(ht.allclose(y, np_y))
 
         # exceptions
         x = ht.arange(10, split=0)
