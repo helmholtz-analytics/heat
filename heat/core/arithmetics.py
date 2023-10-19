8697b15f3348baeb072b14318541a6c858b639c8
Scheib <ls.login.sl@gmail.com>
2023-10-19 15:02:38 +0200

    Bug fix

--- heat/core/arithmetics.py
+++ heat/core/arithmetics.py
@@ -700,7 +700,6 @@ def iadd(t1: DNDarray, t2: Union[DNDarray, float]) -> DNDarray:
     >>> s
     2.0
     """
-
     if not isinstance(t1, DNDarray):
         raise TypeError("Input 1 must be a DNDarray and input 2 either a DNDarray or a scalar.")
 
@@ -787,7 +786,6 @@ def isub(t1: DNDarray, t2: Union[DNDarray, float]) -> DNDarray:
     >>> s
     2.0
     """
-
     if not isinstance(t1, DNDarray):
         raise TypeError("Input 1 must be a DNDarray and input 2 either a DNDarray or a scalar.")
 
