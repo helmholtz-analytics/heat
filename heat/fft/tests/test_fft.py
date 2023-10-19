1cd62f7bfb7c89884ec067089b047f7369c2df7a
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2023-10-19 15:25:43 +0200

    skip comm-intensive tests on gpu

--- heat/fft/tests/test_fft.py
+++ heat/fft/tests/test_fft.py
@@ -38,25 +38,27 @@ class TestFFT(TestCase):
         self.assertTrue(y.split == 0)
         self.assert_array_equal(y, np_y)
 
-        # FFT along distributed axis, n not None
-        n = 8
-        y = ht.fft.fft(x, axis=0, n=n)
-        np_y = np.fft.fft(x.numpy(), axis=0, n=n)
-        self.assertIsInstance(y, ht.DNDarray)
-        self.assertEqual(y.shape, np_y.shape)
-        self.assertTrue(y.split == 0)
-        self.assert_array_equal(y, np_y)
+        # on GPU, test only on less than 4 processes
+        if x.device == ht.cpu or x.comm.size < 4:
+            # FFT along distributed axis, n not None
+            n = 8
+            y = ht.fft.fft(x, axis=0, n=n)
+            np_y = np.fft.fft(x.numpy(), axis=0, n=n)
+            self.assertIsInstance(y, ht.DNDarray)
+            self.assertEqual(y.shape, np_y.shape)
+            self.assertTrue(y.split == 0)
+            self.assert_array_equal(y, np_y)
 
-        # complex input
-        x = x + 1j * ht.random.randn(10, 8, 6, dtype=ht.float64, split=0)
-        # FFT along last axis (distributed)
-        x.resplit_(axis=2)
-        y = ht.fft.fft(x, n=n)
-        np_y = np.fft.fft(x.numpy(), n=n)
-        self.assertIsInstance(y, ht.DNDarray)
-        self.assertEqual(y.shape, np_y.shape)
-        self.assertTrue(y.split == 2)
-        self.assert_array_equal(y, np_y)
+            # complex input
+            x = x + 1j * ht.random.randn(10, 8, 6, dtype=ht.float64, split=0)
+            # FFT along last axis (distributed)
+            x.resplit_(axis=2)
+            y = ht.fft.fft(x, n=n)
+            np_y = np.fft.fft(x.numpy(), n=n)
+            self.assertIsInstance(y, ht.DNDarray)
+            self.assertEqual(y.shape, np_y.shape)
+            self.assertTrue(y.split == 2)
+            self.assert_array_equal(y, np_y)
 
         # exceptions
         # wrong input type
@@ -81,15 +83,16 @@ class TestFFT(TestCase):
         backwards = ht.fft.ifft2(y)
         self.assertTrue(ht.allclose(backwards, x))
 
-        # 2D FFT along split axes
-        x = ht.random.randn(10, 6, 6, split=0, dtype=ht.float64)
-        axes = (0, 1)
-        y = ht.fft.fft2(x, axes=axes)
-        np_y = np.fft.fft2(x.numpy(), axes=axes)
-        self.assertTrue(y.split == 0)
-        self.assert_array_equal(y, np_y)
-        backwards = ht.fft.ifft2(y, axes=axes)
-        self.assertTrue(ht.allclose(backwards, x))
+        if x.device == ht.cpu or x.comm.size < 4:
+            # 2D FFT along split axes
+            x = ht.random.randn(10, 6, 6, split=0, dtype=ht.float64)
+            axes = (0, 1)
+            y = ht.fft.fft2(x, axes=axes)
+            np_y = np.fft.fft2(x.numpy(), axes=axes)
+            self.assertTrue(y.split == 0)
+            self.assert_array_equal(y, np_y)
+            backwards = ht.fft.ifft2(y, axes=axes)
+            self.assertTrue(ht.allclose(backwards, x))
 
         # exceptions
         x = ht.arange(10, split=0)
@@ -126,14 +129,16 @@ class TestFFT(TestCase):
         self.assertTrue(y.split == 0)
         self.assert_array_equal(y, np_y)
 
-        # FFT along distributed axis
-        x.resplit_(axis=1)
-        y = ht.fft.fftn(x, axes=(0, 1), s=(10, 8))
-        np_y = np.fft.fftn(x.numpy(), axes=(0, 1), s=(10, 8))
-        self.assertIsInstance(y, ht.DNDarray)
-        self.assertEqual(y.shape, np_y.shape)
-        self.assertTrue(y.split == 1)
-        self.assert_array_equal(y, np_y)
+        # on GPU, test only on less than 4 processes
+        if x.device == ht.cpu or x.comm.size < 4:
+            # FFT along distributed axis
+            x.resplit_(axis=1)
+            y = ht.fft.fftn(x, axes=(0, 1), s=(10, 8))
+            np_y = np.fft.fftn(x.numpy(), axes=(0, 1), s=(10, 8))
+            self.assertIsInstance(y, ht.DNDarray)
+            self.assertEqual(y.shape, np_y.shape)
+            self.assertTrue(y.split == 1)
+            self.assert_array_equal(y, np_y)
 
         # exceptions
         # wrong input type
@@ -161,32 +166,29 @@ class TestFFT(TestCase):
         reconstructed_x = ht.fft.hfft(inv_fft[:3])
         self.assertEqual(reconstructed_x.shape, (3, n))
 
-    def test_ifft(self):
-        # 1D non-distributed
-        x = ht.random.randn(6, dtype=ht.float64)
-        x_fft = ht.fft.fft(x)
-        y = ht.fft.ifft(x_fft)
-        self.assertIsInstance(y, ht.DNDarray)
-        self.assertEqual(y.shape, x.shape)
-        self.assert_array_equal(y, x.numpy())
+    def test_hfftn_ihfftn(self):
+        # follows example in torch.fft.hfftn docs
+        x = ht.random.randn(10, 6, 6, dtype=ht.float64)
+        inv_fft = ht.fft.ifftn(x)
+        reconstructed_x = ht.fft.hfftn(inv_fft, s=x.shape)
+        self.assertTrue(ht.allclose(reconstructed_x, x))
 
-    def test_rfft(self):
-        pass
+    def test_rfft_irfft(self):
+        # n-D distributed
+        x = ht.random.randn(10, 8, 6, dtype=ht.float64, split=0)
+        # FFT along last axis
+        y = ht.fft.fft(x)
+        np_y = np.fft.fft(x.numpy())
+        self.assertTrue(y.split == 0)
+        self.assert_array_equal(y, np_y)
+        backwards = ht.fft.irfft(y, n=x.shape[-1])
+        self.assertTrue(ht.allclose(backwards, x))
 
-    def test_irfft(self):
-        pass
-
-    def test_ifft2(self):
-        pass
-
-    def test_rfft2(self):
-        pass
-
-    def test_irfft2(self):
-        pass
-
-    def test_ifftn(self):
-        pass
+        # exceptions
+        # complex input
+        x = x + 1j * ht.random.randn(10, 8, 6, dtype=ht.float64, split=0)
+        with self.assertRaises(TypeError):
+            ht.fft.rfft(x)
 
     def test_rfftn(self):
         pass
