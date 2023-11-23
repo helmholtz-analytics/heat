18ffbc69f8eb653287c087f452c8e35b28bc92f6
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2023-11-23 04:33:19 +0100

    update to  Pytorch 2.1.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "mpi4py>=3.0.0",
         "numpy>=1.20.0",
-        "torch>=1.8.0, <2.0.2",
+        "torch>=1.8.0, <2.1.2",
         "scipy>=0.14.0",
         "pillow>=6.0.0",
         "torchvision>=0.8.0",
