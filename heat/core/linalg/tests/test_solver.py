c0fe5b455c100064659602877f913fac04f33427
Hoppe <mrhf92@gmail.com>
2024-04-08 11:50:27 +0200

    ...

--- heat/core/linalg/tests/test_solver.py
+++ heat/core/linalg/tests/test_solver.py
@@ -41,7 +41,7 @@ class TestSolver(TestCase):
         self.assertTrue(T.dtype is B.dtype)
         # V must be unitary
         V_inv = ht.conj(V).T
-        self.assertTrue(ht.allclose(V_inv @ V, ht.eye(V.shape[0]))
+        self.assertTrue(ht.allclose(V_inv @ V, ht.eye(V.shape[0])))
         # V T V.T must be = B, V transposed = V inverse
         lanczos_B = V @ T @ V_inv
         self.assertTrue(ht.allclose(lanczos_B, B))
@@ -60,7 +60,7 @@ class TestSolver(TestCase):
         ht.lanczos(B, m=m, V_out=V_out, T_out=T_out)
         # V must be unitary
         V_inv = ht.conj(V_out).T
-        self.assertTrue(ht.allclose(V_inv @ V_out, ht.eye(V_out.shape[0]))
+        self.assertTrue(ht.allclose(V_inv @ V_out, ht.eye(V_out.shape[0])))
         # V T V* must be = B, V conjugate transpose = V inverse
         lanczos_B = V_out @ T_out @ V_inv
         self.assertTrue(ht.allclose(lanczos_B, B))
@@ -111,7 +111,7 @@ class TestSolver(TestCase):
         self.assertTrue(T_out.dtype is B.real.dtype)
         # V must be unitary
         V_inv = ht.conj(V_out).T
-        self.assertTrue(ht.allclose(V_inv @ V_out, ht.eye(V_out.shape[0]))
+        self.assertTrue(ht.allclose(V_inv @ V_out, ht.eye(V_out.shape[0])))
         # without output buffers
         V, T = ht.lanczos(B, m=m)
         # V T V.T must be = B, V transposed = V inverse
