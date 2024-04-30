874d397e58c7d198a164467b86dce1cab3150232
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2024-04-30 11:57:20 +0200

    pacify pre-commit

--- benchmarks/2020/distance_matrix/numpy-cpu.py
+++ benchmarks/2020/distance_matrix/numpy-cpu.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         start = time.perf_counter()
         dist = cdist(data, data)
         end = time.perf_counter()
-        print(f"\t{end-start}s")
+        print(f"\t{end - start}s")
