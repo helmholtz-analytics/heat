205074f8f876008b4393b4315854b7b7a71807ec
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2024-05-27 05:20:06 +0200

    debugging test_lasso

--- heat/regression/tests/test_lasso.py
+++ heat/regression/tests/test_lasso.py
@@ -37,6 +37,11 @@ class TestLasso(TestCase):
             )
 
             # normalize dataset
+            print(
+                "DEBUGGING: X.shape, ht.sqrt((ht.mean(X**2, axis=0)).shape = ",
+                X.shape,
+                ht.sqrt((ht.mean(X**2, axis=0)).shape),
+            )
             X = X / ht.sqrt((ht.mean(X**2, axis=0)))
             m, n = X.shape
             # HeAT lasso instance
