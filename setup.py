d47a04843ba5fcb26ee3205b176807c61f8f5e9e
mtar <18899420+mtar@users.noreply.github.com>
2024-08-12 06:39:36 +0000

    Support latest PyTorch release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         "mpi4py>=3.0.0, <4.0.0",
         "numpy>=1.22.0, <2",
-        "torch>=2.0.0, <2.3.2",
+        "torch>=2.0.0, <2.4.1",
         "scipy>=1.10.0",
         "pillow>=6.0.0",
         "torchvision>=0.15.2",
