f4b588ec05e885f4777c4e53fd61fdb98b3148ea
Hoppe <mrhf92@gmail.com>
2024-09-02 16:26:44 +0200

    started working on structure of DMD class

--- heat/decomposition/dmd.py
+++ heat/decomposition/dmd.py
@@ -3,7 +3,7 @@ Module implementing the Dynamic Mode Decomposition (DMD) algorithm.
 """
 
 import heat as ht
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, List
 
 try:
     from typing import Self
@@ -29,7 +29,7 @@ class DMD(ht.RegressionMixin, ht.BaseEstimator):
     svd_tol : float, optional
         The tolerance to which SVD shall be truncated. For `'full'` SVD, `svd_tol = None` together with `svd_rank = None` (default) will result in no truncation.
         For `svd_algorithm='hierarchical'`, either `svd_tol` (accuracy to truncate to) or `svd_rank` (rank to truncate to) must be specified.
-        For `svd_algorithm='randomized'`, `svd_tol` is meaningless.
+        For `svd_algorithm='randomized'`, `svd_tol` is meaningless and must be None.
 
     Attributes
     ----------
@@ -39,10 +39,22 @@ class DMD(ht.RegressionMixin, ht.BaseEstimator):
         The rank to which SVD shall be truncated.
     svd_tol : float
         The tolerance to which SVD shall be truncated.
+    rom_basis_ : DNDarray
+        The reduced order model basis.
+    rom_transfer_matrix_ : DNDarray
+        The reduced order model transfer matrix.
+    rom_eigenvalues_ : DNDarray
+        The reduced order model eigenvalues.
+    rom_eigenmodes_ : DNDarray
+        The reduced order model eigenmodes ("DMD modes")
 
     Notes
     ----------
-    ...
+    We follow the "exact DMD" method as described in [1], Sect. 2.2.
+
+    References
+    ----------
+    [1] J. L. Proctor, S. L. Brunton, and J. N. Kutz, "Dynamic Mode Decomposition with Control," SIAM Journal on Applied Dynamical Systems, vol. 15, no. 1, pp. 142-161, 2016.
     """
 
     def __init__(
@@ -51,6 +63,82 @@ class DMD(ht.RegressionMixin, ht.BaseEstimator):
         svd_rank: Optional[int] = None,
         svd_tol: Optional[float] = None,
     ):
+        # Check if the specified SVD algorithm is valid
+        if not isinstance(svd_algorithm, str):
+            raise TypeError(
+                f"Invalid type '{type(svd_algorithm)}' for 'svd_algorithm'. Must be a string."
+            )
+        # check if the specified SVD algorithm is valid
+        if svd_algorithm not in ["full", "hierarchical", "randomized"]:
+            raise ValueError(
+                f"Invalid SVD algorithm '{svd_algorithm}'. Must be one of 'full', 'hierarchical', 'randomized'."
+            )
+        # check if the respective algorithm got the right combination of non-None parameters
+        if svd_algorithm == "full" and svd_rank is not None and svd_tol is not None:
+            raise ValueError(
+                "For 'full' SVD, at most one of 'svd_rank' or 'svd_tol' may be specified."
+            )
+        if svd_algorithm == "hierarchical":
+            if svd_rank is None and svd_tol is None:
+                raise ValueError(
+                    "For 'hierarchical' SVD, exactly one of 'svd_rank' or 'svd_tol' must be specified, but none of them is specified."
+                )
+            if svd_rank is not None and svd_tol is not None:
+                raise ValueError(
+                    "For 'hierarchical' SVD, exactly one of 'svd_rank' or 'svd_tol' must be specified, but currently both are specified."
+                )
+        if svd_algorithm == "randomized":
+            if svd_rank is None:
+                raise ValueError("For 'randomized' SVD, 'svd_rank' must be specified.")
+            if svd_tol is not None:
+                raise ValueError("For 'randomized' SVD, 'svd_tol' must be None.")
+        # check correct data types of non-None parameters
+        if svd_rank is not None:
+            if not isinstance(svd_rank, int):
+                raise TypeError(
+                    f"Invalid type '{type(svd_rank)}' for 'svd_rank'. Must be an integer."
+                )
+            if svd_rank < 1:
+                raise ValueError(
+                    f"Invalid value '{svd_rank}' for 'svd_rank'. Must be a positive integer."
+                )
+        if svd_tol is not None:
+            if not isinstance(svd_tol, float):
+                raise TypeError(f"Invalid type '{type(svd_tol)}' for 'svd_tol'. Must be a float.")
+            if svd_tol <= 0:
+                raise ValueError(f"Invalid value '{svd_tol}' for 'svd_tol'. Must be non-negative.")
+        # set or initialize the attributes
+        self.svd_algorithm = svd_algorithm
         self.svd_rank = svd_rank
         self.svd_tol = svd_tol
-        self.svd_algorithm = svd_algorithm
+        self.rom_basis_ = None
+        self.rom_transfer_matrix_ = None
+        self.rom_eigenvalues_ = None
+        self.rom_eigenmodes_ = None
+        return self
+
+    def fit(self, X: ht.DNDarray) -> Self:
+        """
+        Fits the DMD model to the given data.
+
+        Parameters
+        ----------
+        X : DNDarray
+            The time series data to fit the DMD model to. Must be of shape (n_features, n_timesteps).
+        """
+        pass
+
+    def predict(self, X: ht.DNDarray, steps: Union[int, List, ht.DNDarray]) -> ht.DNDarray:
+        """
+        Predicts and returns the future states at the time steps provided in `steps` with initial condition(s) given by the input `X`.
+
+        Parameters
+        ----------
+        X : DNDarray
+            The initial condition(s) for the prediction. Must have the same number of features as the training data, but can be batched for multiple initial conditions,
+            e.g., X can be of shape (n_features,) or (n_initial_conditions, n_features).
+        steps : int, list, DNDarray
+            The time steps at which to predict the future states. If `steps` is an integer, the future state(s) after `steps` time steps are predicted.
+            If `steps` is a list or DNDarray, the future states at the time steps provided in the list or the DNDarray, respectively, are predicted.
+        """
+        pass
