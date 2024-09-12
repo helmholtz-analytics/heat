8ca15ddf2690679f1dc3a7b91406ebe1a2f53492
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2024-09-12 12:01:03 +0200

    remove ref to torch < 2

--- heat/core/devices.py
+++ heat/core/devices.py
@@ -133,11 +133,7 @@ if torch.cuda.device_count() > 0:
     # the GPU device should be exported as global symbol
     __all__.append("gpu")
 
-elif (
-    (int(torch.__version__.split(".")[0]) > 1 or int(torch.__version__.split(".")[1]) > 11)
-    and torch.backends.mps.is_built()
-    and torch.backends.mps.is_available()
-):
+elif torch.backends.mps.is_built() and torch.backends.mps.is_available():
     # Apple Metal Performance Shaders (MPS) available
     gpu_id = 0
     # create a new GPU device
