da63ce8005b4d0a53bcc7d9198900e727d5dc95e
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2024-09-30 05:49:41 +0200

    simplify is_mps

--- heat/core/relational.py
+++ heat/core/relational.py
@@ -166,7 +166,7 @@ def equal(x: Union[DNDarray, float, int], y: Union[DNDarray, float, int]) -> boo
                 y = y.balance()
 
     result_type = types.result_type(x, y)
-    is_mps = x.device.torch_device.startswith("mps") or y.device.torch_device.startswith("mps")
+    is_mps = x.larray.is_mps or y.larray.is_mps
     if is_mps and result_type is types.float64:
         result_type = types.float32
     x = x.astype(result_type)
