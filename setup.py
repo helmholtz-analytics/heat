e98aa8b5a464a3536e62696058176eb84d545464
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2024-10-16 13:36:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "torch>=2.0.0, <2.4.2",
         "scipy>=1.10.0",
         "pillow>=6.0.0",
-        "torchvision>=0.15.2, <0.19.1",
+        "torchvision>=0.15.2, <0.19.2",
     ],
     extras_require={
         "docutils": ["docutils>=0.16"],
