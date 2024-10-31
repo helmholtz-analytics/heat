884d179c8331cc1708477eb651600b1d4cfba1f1
Hoppe <mrhf92@gmail.com>
2024-10-31 11:24:23 +0100

    removed bug

--- test_ausprobieren.py
+++
@@ -1,7 +0,0 @@
-import heat as ht
-
-# Create a DNDarray with split=1 and dtype=ht.float64
-x = ht.random.randn(100, 100, split=1, dtype=ht.float64)
-
-# Call _estimate_largest_singularvalue with an invalid algorithm
-est = ht.linalg._estimate_largest_singularvalue(x, algorithm="invalid")
