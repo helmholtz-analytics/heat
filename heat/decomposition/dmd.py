d54f7b497bbf8432ae8c0efe8f1fd0e437bb546a
Hoppe <mrhf92@gmail.com>
2025-01-27 17:15:42 +0100

    bug fix in randomized SVD for DMD

--- heat/decomposition/dmd.py
+++ heat/decomposition/dmd.py
@@ -197,7 +197,7 @@ class DMD(ht.RegressionMixin, ht.BaseEstimator):
         else:
             # compute SVD via "randomized" SVD
             U, S, V = ht.linalg.rsvd(
-                X[:, :-1] if X.split == 0 else X[:, :-1].balance_(),
+                X[:, :-1],
                 self.svd_rank,
             )
             self.rom_basis_ = U
