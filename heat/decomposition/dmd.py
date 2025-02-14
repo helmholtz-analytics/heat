3148bbe8e728ef7a0ed2c876d638e977730e0fb9
Hoppe <mrhf92@gmail.com>
2025-02-14 16:25:53 +0100

    added DMDc fit

--- heat/decomposition/dmd.py
+++ heat/decomposition/dmd.py
@@ -319,3 +319,261 @@ class DMD(ht.RegressionMixin, ht.BaseEstimator):
             raise NotImplementedError(
                 "Predicting multiple time steps in one go is not supported for the given data layout. Please, use 'predict_next' instead, or open an issue on GitHub if you require this feature."
             )
+
+
+class DMDc(ht.RegressionMixin, ht.BaseEstimator):
+    """
+    Dynamic Mode Decomposition with Control (DMDc), plain vanilla version with SVD-based implementation.
+
+    The time series of states and controls must be provided as 2-D DNDarrays of shapes (n_state_features, n_timesteps) and (n_control_features, n_timesteps), respectively.
+    Please, note that this deviates from Heat's convention that data sets are handeled as 2-D arrays with the feature axis being the second axis.
+
+    Parameters
+    ----------
+    svd_solver : str, optional
+        Specifies the algorithm to use for the singular value decomposition (SVD). Options are 'full' (default), 'hierarchical', and 'randomized'.
+    svd_rank : int, optional
+        The rank to which SVD of the states shall be truncated. For `'full'` SVD, `svd_rank = None` together with `svd_tol = None` (default) will result in no truncation.
+        For `svd_solver='full'`, at most one of `svd_rank` or `svd_tol` may be specified.
+        For `svd_solver='hierarchical'`, either `svd_rank` (rank to truncate to) or `svd_tol` (tolerance to truncate to) must be specified.
+        For `svd_solver='randomized'`, `svd_rank` must be specified and determines the the rank to truncate to.
+    svd_tol : float, optional
+        The tolerance to which SVD of the states shall be truncated. For `'full'` SVD, `svd_tol = None` together with `svd_rank = None` (default) will result in no truncation.
+        For `svd_solver='hierarchical'`, either `svd_tol` (accuracy to truncate to) or `svd_rank` (rank to truncate to) must be specified.
+        For `svd_solver='randomized'`, `svd_tol` is meaningless and must be None.
+
+    Attributes
+    ----------
+    svd_solver : str
+        The algorithm used for the singular value decomposition (SVD).
+    svd_rank : int
+        The rank to which SVD shall be truncated.
+    svd_tol : float
+        The tolerance to which SVD shall be truncated.
+    rom_basis_ : DNDarray
+        The reduced order model basis.
+    rom_transfer_matrix_ : DNDarray
+        The reduced order model transfer matrix.
+    rom_control_matrix_ : DNDarray
+        The reduced order model control matrix.
+    rom_eigenvalues_ : DNDarray
+        The reduced order model eigenvalues.
+    rom_eigenmodes_ : DNDarray
+        The reduced order model eigenmodes ("DMD modes")
+
+    Notes
+    ----------
+    We follow the approach described in [1], Sects. 3.3 and 3.4.
+
+    References
+    ----------
+    [1] J. L. Proctor, S. L. Brunton, and J. N. Kutz, "Dynamic Mode Decomposition with Control," SIAM Journal on Applied Dynamical Systems, vol. 15, no. 1, pp. 142-161, 2016.
+    """
+
+    def __init__(
+        self,
+        svd_solver: Optional[str] = "full",
+        svd_rank: Optional[int] = None,
+        svd_tol: Optional[float] = None,
+        svd_rank_system: Optional[int] = None,
+        svd_tol_system: Optional[float] = None,
+    ):
+        # Check if the specified SVD algorithm is valid
+        if not isinstance(svd_solver, str):
+            raise TypeError(
+                f"Invalid type '{type(svd_solver)}' for 'svd_solver'. Must be a string."
+            )
+        # check if the specified SVD algorithm is valid
+        if svd_solver not in ["full", "hierarchical", "randomized"]:
+            raise ValueError(
+                f"Invalid SVD algorithm '{svd_solver}'. Must be one of 'full', 'hierarchical', 'randomized'."
+            )
+        # check if the respective algorithm got the right combination of non-None parameters
+        if svd_solver == "full" and svd_rank is not None and svd_tol is not None:
+            raise ValueError(
+                "For 'full' SVD, at most one of 'svd_rank' or 'svd_tol' may be specified."
+            )
+        if svd_solver == "hierarchical":
+            if svd_rank is None and svd_tol is None:
+                raise ValueError(
+                    "For 'hierarchical' SVD, exactly one of 'svd_rank' or 'svd_tol' must be specified, but none of them is specified."
+                )
+            if svd_rank is not None and svd_tol is not None:
+                raise ValueError(
+                    "For 'hierarchical' SVD, exactly one of 'svd_rank' or 'svd_tol' must be specified, but currently both are specified."
+                )
+        if svd_solver == "randomized":
+            if svd_rank is None:
+                raise ValueError("For 'randomized' SVD, 'svd_rank' must be specified.")
+            if svd_tol is not None:
+                raise ValueError("For 'randomized' SVD, 'svd_tol' must be None.")
+        # check correct data types of non-None parameters
+        if svd_rank is not None:
+            if not isinstance(svd_rank, int):
+                raise TypeError(
+                    f"Invalid type '{type(svd_rank)}' for 'svd_rank'. Must be an integer."
+                )
+            if svd_rank < 1:
+                raise ValueError(
+                    f"Invalid value '{svd_rank}' for 'svd_rank'. Must be a positive integer."
+                )
+        if svd_tol is not None:
+            if not isinstance(svd_tol, float):
+                raise TypeError(f"Invalid type '{type(svd_tol)}' for 'svd_tol'. Must be a float.")
+            if svd_tol <= 0:
+                raise ValueError(f"Invalid value '{svd_tol}' for 'svd_tol'. Must be non-negative.")
+        # set or initialize the attributes
+        self.svd_solver = svd_solver
+        self.svd_rank = svd_rank
+        self.svd_tol = svd_tol
+        self.rom_basis_ = None
+        self.rom_transfer_matrix_ = None
+        self.rom_control_matrix_ = None
+        self.rom_eigenvalues_ = None
+        self.rom_eigenmodes_ = None
+        self.dmdmodes_ = None
+        self.n_modes_ = None
+        self.n_modes_system_ = None
+
+    def fit(self, X: ht.DNDarray, C: ht.DNDarray) -> Self:
+        """
+        Fits the DMD model to the given data.
+
+        Parameters
+        ----------
+        X : DNDarray
+            The time series data of states to fit the DMD model to. Must be of shape (n_state_features, n_timesteps).
+        C : DNDarray
+            The time series of control inputs to fit the DMD model to. Must be of shape (n_control_features, n_timesteps).
+        """
+        ht.sanitize_in(X)
+        ht.sanitize_in(C)
+        # check if the input data is a 2-D DNDarray
+        if X.ndim != 2:
+            raise ValueError(
+                f"Invalid shape '{X.shape}' for input data 'X'. Must be a 2-D DNDarray of shape (n_state_features, n_timesteps)."
+            )
+        if C.ndim != 2:
+            raise ValueError(
+                f"Invalid shape '{C.shape}' for input data 'C'. Must be a 2-D DNDarray of shape (n_control_features, n_timesteps)."
+            )
+        # check if the input data has at least two time steps
+        if X.shape[1] < 2:
+            raise ValueError(
+                f"Invalid number of time steps '{X.shape[1]}' in input data 'X'. Must have at least two time steps."
+            )
+        if C.shape[1] < 2:
+            raise ValueError(
+                f"Invalid number of time steps '{C.shape[1]}' in input data 'C'. Must have at least two time steps."
+            )
+        # check if the input data has the same number of time steps
+        if X.shape[1] != C.shape[1]:
+            raise ValueError(
+                f"Invalid number of time steps {X.shape[1]} in input data 'X' and {C.shape[1]} in input data 'C'. Must have the same number of time steps."
+            )
+        Xplus = X[:, 1:]
+        Xplus.balance_()
+        Omega = ht.concatenate((X, C), axis=0)
+        # first step of DMDc: compute the SVD of the input data from first to second last time step
+        # as well as of the full system matrix
+        if self.svd_solver == "full" or not X.is_distributed():
+            U, S, V = ht.linalg.svd(Xplus, full_matrices=False)
+            Utilde, Stilde, Vtilde = ht.linalg.svd(Omega, full_matrices=False)
+            if self.svd_tol is not None:
+                # truncation w.r.t. prescribed bound on explained variance
+                # determine svd_rank accordingly
+                total_variance = (S**2).sum()
+                variance_threshold = (1 - self.svd_tol) * total_variance.larray.item()
+                variance_cumsum = (S**2).larray.cumsum(0)
+                self.n_modes_ = len(variance_cumsum[variance_cumsum <= variance_threshold]) + 1
+                total_variance_system = (Stilde**2).sum()
+                variance_threshold_system = (1 - self.svd_tol) * total_variance_system.larray.item()
+                variance_cumsum_system = (Stilde**2).larray.cumsum(0)
+                self.n_modes_system_ = (
+                    len(variance_cumsum_system[variance_cumsum_system <= variance_threshold_system])
+                    + 1
+                )
+            elif self.svd_rank is not None:
+                # truncation w.r.t. prescribed rank
+                self.n_modes_ = self.svd_rank
+                self.n_modes_system_ = self.svd_rank + C.shape[0]
+            else:
+                # no truncation
+                self.n_modes_ = S.shape[0]
+                self.n_modes_system = Stilde.shape[0]
+            self.rom_basis_ = U[:, : self.n_modes_]
+            V = V[:, : self.n_modes_]
+            S = S[: self.n_modes_]
+            Vtilde = Vtilde[:-1, : self.n_modes_system_]
+            Stilde = Stilde[: self.n_modes_system_]
+            Utilde1 = Utilde[: X.shape[0], : self.n_modes_system_]
+            Utilde2 = Utilde[X.shape[0] :, : self.n_modes_system_]
+        # compute SVD via "hierarchical" SVD
+        elif self.svd_solver == "hierarchical":
+            if self.svd_tol is not None:
+                # hierarchical SVD with prescribed upper bound on relative error
+                U, S, V, _ = ht.linalg.hsvd_rtol(
+                    Xplus,
+                    self.svd_tol,
+                    compute_sv=True,
+                    safetyshift=5,
+                )
+                Utilde, Stilde, Vtilde, _ = ht.linalg.hsvd_rtol(
+                    Omega,
+                    self.svd_tol,
+                    compute_sv=True,
+                    safetyshift=5,
+                )
+            else:
+                # hierarchical SVD with prescribed, fixed rank
+                U, S, V, _ = ht.linalg.hsvd_rank(
+                    Xplus,
+                    self.svd_rank,
+                    compute_sv=True,
+                    safetyshift=5,
+                )
+                Utilde, Stilde, Vtilde, _ = ht.linalg.hsvd_rank(
+                    Omega,
+                    self.svd_rank,
+                    compute_sv=True,
+                    safetyshift=5,
+                )
+            self.rom_basis_ = U
+            self.n_modes_ = U.shape[1]
+            self.n_modes_system_ = Utilde.shape[1]
+            Utilde1 = Utilde[: X.shape[0], :]
+            Utilde2 = Utilde[X.shape[0] :, :]
+            Vtilde = Vtilde[:-1, :]
+        else:
+            # compute SVD via "randomized" SVD
+            U, S, V = ht.linalg.rsvd(
+                Xplus,
+                self.svd_rank,
+            )
+            Utilde, Stilde, Vtilde = ht.linalg.rsvd(
+                Omega,
+                self.svd_rank,
+            )
+            self.rom_basis_ = U
+            self.n_modes_ = U.shape[1]
+            self.n_modes_system_ = Utilde.shape[1]
+            Utilde1 = Utilde[: X.shape[0], :]
+            Utilde2 = Utilde[X.shape[0] :, :]
+            Vtilde = Vtilde[:-1, :]
+
+        # second step of DMD: compute the reduced order model transfer matrix
+        # we need to assume that the the transfer matrix of the ROM is small enough to fit into memory of one process
+        self.rom_transfer_matrix_ = (
+            self.rom_basis_.T @ Xplus @ (Vtilde / Stilde) @ Utilde1.T @ self.rom_basis_
+        )
+        self.rom_control_matrix_ = self.rom_basis_.T @ Xplus @ (Vtilde / Stilde) @ Utilde2.T
+        self.rom_transfer_matrix_.resplit_(None)
+        self.rom_control_matrix_.resplit_(None)
+
+        # third step of DMD: compute the reduced order model eigenvalues and eigenmodes
+        eigvals_loc, eigvec_loc = torch.linalg.eig(self.rom_transfer_matrix_.larray)
+        self.rom_eigenvalues_ = ht.array(eigvals_loc, split=None)
+        self.rom_eigenmodes_ = ht.array(eigvec_loc, split=None)
+        self.dmdmodes_ = (
+            Xplus @ (Vtilde / Stilde) @ Utilde1.T @ self.rom_basis_ @ self.rom_eigenmodes_
+        )
