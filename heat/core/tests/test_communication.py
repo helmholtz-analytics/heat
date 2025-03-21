b535474a65c16b04ffae6c2e1c8cd486e6960797
Hoppe <mrhf92@gmail.com>
2025-03-21 09:14:00 +0100

    debugging

--- heat/core/tests/test_communication.py
+++ heat/core/tests/test_communication.py
@@ -2522,8 +2522,8 @@ class TestCommunication(TestCase):
 
     # the following test is only for up to three processes to save memory
     @unittest.skipIf(
-        ht.MPI_WORLD.size > 3 or "rocm" in torch.__version__,
-        "Only for up to three processes and not on the AMD runner",
+        ht.MPI_WORLD.size == 1 or ht.MPI_WORLD.size > 3 or "rocm" in torch.__version__,
+        "Only for two or three processes and not on the AMD runner",
     )
     def test_largecount_workaround_Allreduce(self):
         shape = (2**10, 2**11, 2**10)
