4db57b8ce96a3cd7428a47761307aa79e9c59c1b
Hoppe <mrhf92@gmail.com>
2025-04-08 10:12:21 +0200

    added MPICH
    (cherry picked from commit e44c43b2b51773c9781d37ee27496da03f33ce72)

--- heat/core/_config.py
+++ heat/core/_config.py
@@ -8,6 +8,7 @@ import mpi4py
 import subprocess
 import os
 import warnings
+import re
 
 PLATFORM = platform.platform()
 MPI_LIBRARY_VERSION = mpi4py.MPI.Get_library_version()
@@ -16,21 +17,33 @@ TORCH_CUDA_IS_AVAILABLE = torch.cuda.is_available()
 CUDA_IS_ACTUALLY_ROCM = "rocm" in TORCH_VERSION
 
 CUDA_AWARE_MPI = False
-# check whether there is CUDA-aware OpenMPI
+ROCM_AWARE_MPI = False
+
+# check whether there is CUDA- or ROCm-aware OpenMPI
 try:
     buffer = subprocess.check_output(["ompi_info", "--parsable", "--all"])
     CUDA_AWARE_MPI = b"mpi_built_with_cuda_support:value:true" in buffer
+    pattern = re.compile(r"^MPI extensions:.*", re.MULTILINE)
+    match = pattern.search(buffer)
+    ROCM_AWARE_MPI = "rocm" in match.group(0)
 except:  # noqa E722
     pass
+
 # do the same for MVAPICH
 CUDA_AWARE_MPI = CUDA_AWARE_MPI or os.environ.get("MV2_USE_CUDA") == "1"
-# do the same for MPICH
+CUDA_AWARE_MPI = ROCM_AWARE_MPI or os.environ.get("MV2_USE_ROCM") == "1"
+
+# do the same for MPICH, TODO: outdated?
 CUDA_AWARE_MPI = CUDA_AWARE_MPI or os.environ.get("MPIR_CVAR_ENABLE_HCOLL") == "1"
-# do the same for ParaStationMPI
+
+# Cray MPICH
+CUDA_AWARE_MPI = os.environ.get("MPICH_GPU_SUPPORT_ENABLED") == "1"
+ROCM_AWARE_MPI = os.environ.get("MPICH_GPU_SUPPORT_ENABLED") == "1"
+
+# do the same for ParaStationMPI, seems to have CUDA-support only
 CUDA_AWARE_MPI = CUDA_AWARE_MPI or os.environ.get("PSP_CUDA") == "1"
 
-ROCM_AWARE_MPI = False
-# TODO: do these checks for ROCM-aware MPI
+# Intel-MPI?
 
 # warn the user if CUDA/ROCm-aware MPI is not available, but PyTorch can use GPUs with CUDA/ROCm
 if TORCH_CUDA_IS_AVAILABLE:
