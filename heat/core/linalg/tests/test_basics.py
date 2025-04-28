332d0c0697c1fcb66afecc0454395d4fe8db714a
Marc-Jindra <m.jindra@fz-juelich.de>
2025-04-28 10:35:10 +0200

    Make unit tests compatible with NumPy 2.x (#1826)
    * changed row_stack to vstack for numpy >= 2.0.0
    
    * Changed the numpy version check to the numpy suggested method
    
    * Changed numpy version requirement
    
    * fixed DeprecationWarning from missing axes for np.fft.fftn
    
    * Fixed one __array_wrap__ DeprecationWarning
    
    * Stopped testing cross of vector axes with 2 elements for numpy >= 2.0
    
    * changed requirements to avoid errors with numpy >= 2
    
    * Using Python 3.10 for RecievePR
    
    * changed python-version to '3.10' because 3.10 was interpreted as 3.1
    
    * changed ci.yaml to exclude python 3.9
    
    * Fixed two DeprecationWarnings of np.cross() by adding a third dimension with zeros
    
    * Fixed the last np.cross() warning by performing the operation manually
    
    * changed dtype of np.cross() to float32
    
    ---------
    
    Co-authored-by: Fabian Hoppe <112093564+mrfh92@users.noreply.github.com>
    Co-authored-by: Juan Pedro Gutiérrez Hermosillo Muriedas <juanpedroghm@gmail.com>
    Co-authored-by: Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>

--- heat/core/linalg/tests/test_basics.py
+++ heat/core/linalg/tests/test_basics.py
@@ -111,16 +111,26 @@ class TestLinalgBasics(TestCase):
         # test vector axes with 2 elements
         b_2d = ht.array(np_b[:-1, :, :], split=1)
         cross_3d_2d = ht.cross(a, b_2d, axisa=1, axisb=0)
-        np_cross_3d_2d = np.cross(np_a, np_b[:-1, :, :], axisa=1, axisb=0)
+        np_cross_3d_2d = np.cross(
+            np_a,
+            np.concatenate([np_b[:-1, :, :], np.zeros((1, 40, 50))], axis=0, dtype=np.float32),
+            axisa=1,
+            axisb=0,
+        )
         self.assert_array_equal(cross_3d_2d, np_cross_3d_2d)
 
         a_2d = ht.array(np_a[:, :-1, :], split=0)
         cross_2d_3d = ht.cross(a_2d, b, axisa=1, axisb=0)
-        np_cross_2d_3d = np.cross(np_a[:, :-1, :], np_b, axisa=1, axisb=0)
+        np_cross_2d_3d = np.cross(
+            np.concatenate([np_a[:, :-1, :], np.zeros((40, 1, 50))], axis=1, dtype=np.float32),
+            np_b,
+            axisa=1,
+            axisb=0,
+        )
         self.assert_array_equal(cross_2d_3d, np_cross_2d_3d)
 
         cross_z_comp = ht.cross(a_2d, b_2d, axisa=1, axisb=0)
-        np_cross_z_comp = np.cross(np_a[:, :-1, :], np_b[:-1, :, :], axisa=1, axisb=0)
+        np_cross_z_comp = np_a[:, 0, ...] * np_b[1, ...] - np_a[:, 1, ...] * np_b[0, ...]
         self.assert_array_equal(cross_z_comp, np_cross_z_comp)
 
         a_wrong_split = ht.array(np_a[:, :-1, :], split=2)
