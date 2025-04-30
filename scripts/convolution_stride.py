c37b9b0246d2f9a09e05a5da1575f1673e2465ee
Carola Fischer <carola.fischer@kit.edu>
2025-04-30 10:41:12 +0200

    Remove: Remove script to test convolution with stride

--- scripts/convolution_stride.py
+++
@@ -1,8 +0,0 @@
-import heat as ht
-from heat import manipulations
-import numpy as np
-import torch
-
-
-print(ht.convolve(ht.array([1, 2]), ht.array([1, 1]), mode="valid", stride=2))
-print(ht.convolve(ht.array([1, 2, 3]), ht.array([1, 1]), mode="valid", stride=2))
