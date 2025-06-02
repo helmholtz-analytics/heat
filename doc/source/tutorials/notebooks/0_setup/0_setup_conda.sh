a1ddefd78646d620e53b30fa7813f7a00bd6f153
Till Korten <webmaster@korten.at>
2025-06-02 11:55:05 +0200

    exit 0_setup_conda.sh if environment cannot be activated
    otherwise the script might install into the base environment

--- doc/source/tutorials/notebooks/0_setup/0_setup_conda.sh
+++ doc/source/tutorials/notebooks/0_setup/0_setup_conda.sh
@@ -5,7 +5,7 @@
 
 ## 2. Setup conda environment
 conda create --name heat-env python=3.11
-conda activate heat-env
+conda activate heat-env || exit 1
 conda install -c conda-forge heat xarray jupyter scikit-learn ipyparallel
 
 ## 3. Setup kernel
