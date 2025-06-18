ecbd62d2a337a51a1efe96b1877438a73eac3199
Carola Fischer <carola.fischer@kit.edu>
2025-06-18 16:08:02 +0200

    Fix: test_signal.py batch convolution with stride
    - Stride was not passed as a single value but different values for
      different ranks
    - Solution: Do not randomly create stride values but use fixed values

--- heat/core/signal.py
+++ heat/core/signal.py
@@ -192,6 +192,7 @@ def convolve(a: DNDarray, v: DNDarray, mode: str = "full", stride: int = 1) -> D
         # all operations are local torch operations, only the last dimension is convolved
         local_a = a.larray
         local_v = v.larray
+
         # flip filter for convolution, as Pytorch conv1d computes correlations
         local_v = torch.flip(local_v, [-1])
         local_batch_dims = tuple(local_a.shape[:-1])
