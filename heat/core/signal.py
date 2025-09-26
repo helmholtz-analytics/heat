Fix: local_index computation for distributed signal and stride > 1
convolution2d_large_signal_and_kernel still throws error for distributed
kernels

heat/core/signal.py
@@ -856,7 +856,12 @@ def convolve2d(
             if a.comm.rank == 0:
                 local_index = 0
             else:
-                local_index = torch.sum(a.lshape_map[: a.comm.rank, a.split]).item() - halo_size
+                # lshape map does not know about padding, compute pad_offset for last rank
+                pad_offset = pad_size[a.split] if a.comm.rank == a.comm.size - 1 else 0
+
+                local_index = (
+                    torch.sum(a.lshape_map[: a.comm.rank, a.split]).item() + pad_offset - halo_size
+                )
                 local_index = local_index % stride[a.split]
 
                 print(
@@ -889,10 +894,16 @@ def convolve2d(
             signal_filtered = torch.tensor([[]], device=str(signal.device))
 
         # if kernel shape along split axis is even we need to get rid of duplicated values
-        if stride[a.split] == 1:
-            if a.comm.rank != 0 and a.split == 0 and v.shape[-2] % 2 == 0:
+        if (
+            a.is_distributed()
+            and stride[a.split] == 1
+            and a.comm.rank != 0
+            and v.shape[a.split] % 2 == 0
+        ):
+            print(f"Even kernel, so offset of 1 necessary. Split is: {stride[a.split]}")
+            if a.split == 0:
                 signal_filtered = signal_filtered[1:, :]
-            elif a.comm.rank != 0 and a.split == 1 and v.shape[-1] % 2 == 0:
+            elif a.split == 1:
                 signal_filtered = signal_filtered[:, 1:]
 
         result = DNDarray(

heat/core/tests/test_signal.py
@@ -1332,6 +1332,7 @@ class TestSignal(TestCase):
 
         mode = "full"
         strides = [(1, 2), (2, 1), (2, 2)]
+        strides = [(2,1)]
 
         dis_signal = ht.array(np_sig, split=0).astype(ht_dtype)
         signal = ht.array(np_sig).astype(ht_dtype)
@@ -1481,8 +1482,8 @@ class TestSignal(TestCase):
             ht_dtype = ht.float32 if self.is_mps else ht.int32
             np_type = np.float32 if self.is_mps else np.int32
 
-            np_a = np.random.randint(1000, size=(140, 250))
-            np_b = np.random.randint(1000, size=(39, 17))
+            np_a = np.random.randint(0,1000, size=(140, 250))
+            np_b = np.random.randint(0,3, size=(39, 17))
             random_stride = tuple(np.random.randint(1, high=100, size=2))
             for mode in ["full", "same", "valid"]:
                 strides = [(1,1), random_stride] if mode != "same" else [(1,1)]