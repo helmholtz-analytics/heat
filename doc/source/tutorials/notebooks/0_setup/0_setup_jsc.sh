675bb6529112a94ea5c7daa2fbceaf63946cc59e
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2025-11-19 15:37:11 +0100

    add modules for dev/perun kernel

--- doc/source/tutorials/notebooks/0_setup/0_setup_jsc.sh
+++ doc/source/tutorials/notebooks/0_setup/0_setup_jsc.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-module --force purge
+module purge
 module load Stages/2025
+module load GCC
+module load Python
+module load jupyter-server  # provides ipykernel
 
-ml GCC ParaStationMPI heat h5py
+module load ParaStationMPI mpi4py #MPI
+module load IPython ipyparallel/.9.0.0 # parallel usage on notebook
+module load PyTorch torchvision
+module load h5py zarr netCDF # I/O
+module load xarray scikit-learn matplotlib # interoperability
+module load SciPy-bundle Python-bundle-PyPI py-cpuinfo #psutil # perun deps
 
-ml IPython ipyparallel/.9.0.0
 
-# needed for interoperability examples
-ml xarray scikit-learn matplotlib
+
+
+# module -- purge
+# module load Stages/2025
+
+# ml GCC ParaStationMPI heat h5py
+
+# ml IPython ipyparallel/.9.0.0
+
+# # needed for interoperability examples
+# ml xarray scikit-learn matplotlib
