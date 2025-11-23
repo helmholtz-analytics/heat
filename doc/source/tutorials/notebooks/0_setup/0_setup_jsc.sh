78ef1b508fbd0d781c6f0315542230bd4b88817c
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2025-11-23 16:50:03 +0100

    separate heat from perun shell

--- doc/source/tutorials/notebooks/0_setup/0_setup_jsc.sh
+++ doc/source/tutorials/notebooks/0_setup/0_setup_jsc.sh
@@ -3,16 +3,11 @@
 module --force purge
 module load Stages/2025
 module load GCC
-module load Python
-module load jupyter-server  # provides ipykernel
 
-module load ParaStationMPI mpi4py #MPI
+module load ParaStationMPI mpi4py heat #MPI
 module load IPython ipyparallel/.9.0.0 # parallel usage on notebook
-module load PyTorch torchvision
 module load h5py zarr netcdf4-python # I/O
 module load xarray scikit-learn matplotlib # interoperability
-module load SciPy-bundle Python-bundle-PyPI py-cpuinfo # perun deps
-module load heat
 
 
 
