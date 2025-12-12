39cdea21382b8f8d294bfd0e93b11ee1b290461f
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2025-12-12 15:03:28 +0100

    Clean up commented module load lines in setup script
    Removed commented-out module load commands for clarity.

--- doc/source/tutorials/notebooks/0_setup/0_setup_jsc.sh
+++ doc/source/tutorials/notebooks/0_setup/0_setup_jsc.sh
@@ -8,15 +8,3 @@ module load ParaStationMPI mpi4py heat #MPI
 module load IPython ipyparallel/.9.0.0 # parallel usage on notebook
 module load h5py zarr netcdf4-python # I/O
 module load xarray scikit-learn matplotlib # interoperability
-
-
-
-# module -- purge
-# module load Stages/2025
-
-# ml GCC ParaStationMPI heat h5py
-
-# ml IPython ipyparallel/.9.0.0
-
-# # needed for interoperability examples
-# ml xarray scikit-learn matplotlib
