75df28af6c2415d9af9ceba3a9cd8acb956f5f44
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-01-15 11:36:54 +0100

    Added support for complex values in QR

--- heat/core/linalg/tests/test_qr.py
+++ heat/core/linalg/tests/test_qr.py
@@ -11,7 +11,7 @@ class TestQR(TestCase):
         if self.is_mps:
             dtypes = [ht.float32]
         else:
-            dtypes = [ht.float32, ht.float64]
+            dtypes = [ht.float32, ht.float64, ht.complex64, ht.complex128]
 
         for split in [1, None]:
             for mode in ["reduced", "r"]:
@@ -22,63 +22,57 @@ class TestQR(TestCase):
                     (40 * ht.MPI_WORLD.size - 1, 20 * ht.MPI_WORLD.size),
                 ]:
                     for dtype in dtypes:
-                        dtypetol = 1e-3 if dtype == ht.float32 else 1e-6
-                        mat = ht.random.randn(*shape, dtype=dtype, split=split)
-                        qr = ht.linalg.qr(mat, mode=mode)
+                        with self.subTest(f'{dtype=} {shape=} {mode=}'):
+                            dtypetol = 1e-3 if dtype in [ht.float32, ht.complex64] else 1e-6
+                            mat = ht.random.randn(*shape, dtype=dtype, split=split)
+                            qr = ht.linalg.qr(mat, mode=mode)
 
-                        if mode == "reduced":
-                            allclose = ht.allclose(qr.Q @ qr.R, mat, atol=dtypetol, rtol=dtypetol)
-                            if not allclose:
-                                diff = qr.Q @ qr.R - mat
-                                max_diff = ht.max(diff)
-                                #print(f"diff: {diff}")
-                                #print(f"max_diff: {max_diff}m")
-
-                            self.assertTrue(
-                                ht.allclose(qr.Q @ qr.R, mat, atol=dtypetol, rtol=dtypetol)
-                            )
-                            self.assertIsInstance(qr.Q, ht.DNDarray)
-
-                            # test if Q is orthogonal
-                            self.assertTrue(
-                                ht.allclose(
-                                    qr.Q.T @ qr.Q,
-                                    ht.eye(qr.Q.shape[1], dtype=dtype),
-                                    atol=dtypetol,
-                                    rtol=dtypetol,
+                            if mode == "reduced":
+                                self.assertTrue(
+                                    ht.allclose(qr.Q @ qr.R, mat, atol=dtypetol, rtol=dtypetol)
                                 )
-                            )
-                            # test correct shape of Q
-                            self.assertEqual(qr.Q.shape, (shape[0], min(shape)))
-                        else:
-                            self.assertIsNone(qr.Q)
+                                self.assertIsInstance(qr.Q, ht.DNDarray)
 
-                        # test correct type and shape of R
-                        self.assertIsInstance(qr.R, ht.DNDarray)
-                        self.assertEqual(qr.R.shape, (min(shape), shape[1]))
+                                # test if Q is orthogonal / hermitian
+                                self.assertTrue(
+                                    ht.allclose(
+                                        ht.conj(qr.Q).T @ qr.Q,
+                                        ht.eye(qr.Q.shape[1], dtype=dtype),
+                                        atol=dtypetol,
+                                        rtol=dtypetol,
+                                    )
+                                )
+                                # test correct shape of Q
+                                self.assertEqual(qr.Q.shape, (shape[0], min(shape)))
+                            else:
+                                self.assertIsNone(qr.Q)
 
-                        # compare with torch qr, due to different signs we can only compare absolute values
-                        mat_t = mat.resplit_(None).larray
-                        q_t, r_t = torch.linalg.qr(mat_t, mode=mode)
-                        r_ht = qr.R.resplit_(None).larray
-                        self.assertTrue(
-                            torch.allclose(
-                                torch.abs(r_t), torch.abs(r_ht), atol=dtypetol, rtol=dtypetol
-                            )
-                        )
-                        if mode == "reduced":
-                            q_ht = qr.Q.resplit_(None).larray
+                            # test correct type and shape of R
+                            self.assertIsInstance(qr.R, ht.DNDarray)
+                            self.assertEqual(qr.R.shape, (min(shape), shape[1]))
+
+                            # compare with torch qr, due to different signs we can only compare absolute values
+                            mat_t = mat.resplit_(None).larray
+                            q_t, r_t = torch.linalg.qr(mat_t, mode=mode)
+                            r_ht = qr.R.resplit_(None).larray
                             self.assertTrue(
                                 torch.allclose(
-                                    torch.abs(q_t), torch.abs(q_ht), atol=dtypetol, rtol=dtypetol
+                                    torch.abs(r_t), torch.abs(r_ht), atol=dtypetol, rtol=dtypetol
                                 )
                             )
+                            if mode == "reduced":
+                                q_ht = qr.Q.resplit_(None).larray
+                                self.assertTrue(
+                                    torch.allclose(
+                                        torch.abs(q_t), torch.abs(q_ht), atol=dtypetol, rtol=dtypetol
+                                    )
+                                )
 
     def test_qr_split0(self):
         if self.is_mps:
             dtypes = [ht.float32]
         else:
-            dtypes = [ht.float32, ht.float64]
+            dtypes = [ht.float32, ht.float64, ht.complex64, ht.complex128]
         split = 0
         for procs_to_merge in [0, 2, 3]:
             for mode in ["reduced", "r"]:
@@ -89,51 +83,52 @@ class TestQR(TestCase):
                     (40 * ht.MPI_WORLD.size - 1, 20 * ht.MPI_WORLD.size),
                 ]:
                     for dtype in dtypes:
-                        dtypetol = 1e-3 if dtype == ht.float32 else 1e-6
-                        mat = ht.random.randn(*shape, dtype=dtype, split=split)
+                        with self.subTest(f'{dtype=} {shape=} {mode=} {procs_to_merge=}'):
+                            dtypetol = 1e-3 if dtype in [ht.float32, ht.complex64] else 1e-6
+                            mat = ht.random.randn(*shape, dtype=dtype, split=split)
 
-                        qr = ht.linalg.qr(mat, mode=mode, procs_to_merge=procs_to_merge)
+                            qr = ht.linalg.qr(mat, mode=mode, procs_to_merge=procs_to_merge)
 
-                        if mode == "reduced":
-                            self.assertTrue(
-                                ht.allclose(qr.Q @ qr.R, mat, atol=dtypetol, rtol=dtypetol)
-                            )
-                            self.assertIsInstance(qr.Q, ht.DNDarray)
-
-                            # test if Q is orthogonal
-                            self.assertTrue(
-                                ht.allclose(
-                                    qr.Q.T @ qr.Q,
-                                    ht.eye(qr.Q.shape[1], dtype=dtype),
-                                    atol=dtypetol,
-                                    rtol=dtypetol,
+                            if mode == "reduced":
+                                self.assertTrue(
+                                    ht.allclose(qr.Q @ qr.R, mat, atol=dtypetol, rtol=dtypetol)
                                 )
-                            )
-                            # test correct shape of Q
-                            self.assertEqual(qr.Q.shape, (shape[0], min(shape)))
-                        else:
-                            self.assertIsNone(qr.Q)
+                                self.assertIsInstance(qr.Q, ht.DNDarray)
 
-                        # test correct type and shape of R
-                        self.assertIsInstance(qr.R, ht.DNDarray)
-                        self.assertEqual(qr.R.shape, (min(shape), shape[1]))
+                                # test if Q is orthogonal / hermitian
+                                self.assertTrue(
+                                    ht.allclose(
+                                        ht.conj(qr.Q).T @ qr.Q,
+                                        ht.eye(qr.Q.shape[1], dtype=dtype),
+                                        atol=dtypetol,
+                                        rtol=dtypetol,
+                                    )
+                                )
+                                # test correct shape of Q
+                                self.assertEqual(qr.Q.shape, (shape[0], min(shape)))
+                            else:
+                                self.assertIsNone(qr.Q)
 
-                        # compare with torch qr, due to different signs we can only compare absolute values
-                        mat_t = mat.resplit_(None).larray
-                        q_t, r_t = torch.linalg.qr(mat_t, mode=mode)
-                        r_ht = qr.R.resplit_(None).larray
-                        self.assertTrue(
-                            torch.allclose(
-                                torch.abs(r_t), torch.abs(r_ht), atol=dtypetol, rtol=dtypetol
-                            )
-                        )
-                        if mode == "reduced":
-                            q_ht = qr.Q.resplit_(None).larray
+                            # test correct type and shape of R
+                            self.assertIsInstance(qr.R, ht.DNDarray)
+                            self.assertEqual(qr.R.shape, (min(shape), shape[1]))
+
+                            # compare with torch qr, due to different signs we can only compare absolute values
+                            mat_t = mat.resplit_(None).larray
+                            q_t, r_t = torch.linalg.qr(mat_t, mode=mode)
+                            r_ht = qr.R.resplit_(None).larray
                             self.assertTrue(
                                 torch.allclose(
-                                    torch.abs(q_t), torch.abs(q_ht), atol=dtypetol, rtol=dtypetol
+                                    torch.abs(r_t), torch.abs(r_ht), atol=dtypetol, rtol=dtypetol
                                 )
                             )
+                            if mode == "reduced":
+                                q_ht = qr.Q.resplit_(None).larray
+                                self.assertTrue(
+                                    torch.allclose(
+                                        torch.abs(q_t), torch.abs(q_ht), atol=dtypetol, rtol=dtypetol
+                                    )
+                                )
 
     def test_batched_qr_splitNone(self):
         # two batch dimensions, float64 data type, "split = None" (split batch axis)
