9f32bda8d4c2aec7913ad876008a027b328fbafd
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2026-02-21 16:11:27 +0100

    debugging test_io.py

--- heat/core/tests/test_io.py
+++ heat/core/tests/test_io.py
@@ -990,6 +990,8 @@ class TestIO(TestCase):
 
         import zarr
 
+        ht.MPI_WORLD.Barrier()
+        
         # Write out a nested Zarr store
         original_data = np.arange(np.prod(self.ZARR_SHAPE)).reshape(self.ZARR_SHAPE)
         nested_group_name = "MAIN_0"
