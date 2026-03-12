d59f554b791dba44077b844a26b335176cd7d8e7
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2026-03-12 05:55:15 +0100

    minor edits

--- benchmarks/cb/regression.py
+++ benchmarks/cb/regression.py
@@ -18,13 +18,14 @@ def lasso_predict(estimator, X):
 def run_regression_benchmarks():
     # Benchmark parameters
     n_data_points = 20000
-    n_features = 5
+    n_features = 10
     max_iter = 100
 
     # Distributed Samples (split=0)
     X = ht.random.randn(n_data_points, n_features, split=0)
-    true_w = ht.random.randn(n_features, 1)
-    y = (X @ true_w) + 0.1 * ht.random.randn(n_data_points, 1, split=0)
+    ground_truth = ht.random.randn(n_features, 1)
+    # define y = X @ ground_truth + noise
+    y = (X @ ground_truth) + 0.1 * ht.random.randn(n_data_points, 1, split=0)
 
     lasso = ht.regression.Lasso(max_iter=max_iter, tol=1e-6)
 
