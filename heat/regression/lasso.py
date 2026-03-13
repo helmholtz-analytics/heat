baf34cad84329818c5fd893e0809d31506e14887
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2026-03-13 05:42:19 +0100

    raise NotImplementedError on 1 proc as well

--- heat/regression/lasso.py
+++ heat/regression/lasso.py
@@ -136,7 +136,7 @@ class Lasso(ht.RegressionMixin, ht.BaseEstimator):
             raise ValueError(f"y.ndim must <= 2, currently: {y.ndim}")
         if x.ndim != 2:
             raise ValueError(f"X.ndim must == 2, currently: {x.ndim}")
-        if x.is_distributed() and x.split != 0 or y.is_distributed() and y.split != 0:
+        if x.split is not None and x.split != 0 or y.split is not None and y.split != 0:
             raise NotImplementedError(
                 f"Distribution along the feature axis is not supported, currently x.split = {x.split}, y.split = {y.split}."
             )
