a443bc472fd285bd9a1a28138b3803accd34c329
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2026-03-16 09:52:04 +0100

    remove dl tutorials from notebooks dir

--- doc/source/tutorials/notebooks/heat_dl_tutorial/alloc.sh
+++
@@ -1,1 +0,0 @@
-salloc --partition=dc-gpu-devel --nodes=1 --account=training2546  --time=01:30:00
--- doc/source/tutorials/notebooks/heat_dl_tutorial/set_masteraddr.sh
+++
@@ -1,4 +0,0 @@
-MASTER_ADDR=$(scontrol show hostnames | head -n 1)
-MASTER_ADDR="${MASTER_ADDR}i"
-export MASTER_ADDR=$(nslookup "$MASTER_ADDR" | grep -oP '(?<=Address: ).*')
-export MASTER_PORT=6000
