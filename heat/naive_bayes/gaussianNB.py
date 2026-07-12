fcf00a8363e466ab01b91e9b4861f212a4cec182
Matthias Liesenfeld <116307294+maliesen@users.noreply.github.com>
2026-07-12 16:57:07 +0200

    Make it run in parallel
    - save class_count with split=None so all ranks can access it
    - get the index/mask for the global x with respect to the global y, not y.larray

--- heat/naive_bayes/gaussianNB.py
+++ heat/naive_bayes/gaussianNB.py
@@ -298,7 +298,7 @@ class GaussianNB(ht.ClassificationMixin, ht.BaseEstimator):
             else:
                 class_count_dtype = ht.types.promote_types(x.dtype, ht.float)
             self.class_count_ = ht.zeros(
-                (x.comm.size, n_classes), dtype=class_count_dtype, device=x.device, split=0
+                (n_classes), dtype=class_count_dtype, device=x.device, split=None
             )
             # Initialise the class prior
             # Take into account the priors
@@ -348,11 +348,11 @@ class GaussianNB(ht.ClassificationMixin, ht.BaseEstimator):
             else:
                 classes_ext = torch.cat((classes.larray, y_i.larray.unsqueeze(0)))
                 i = torch.argsort(classes_ext)[-1].item()
-            where_y_i = torch.where(y.larray == y_i)[0]
-            X_i = x[where_y_i, :]
+            eq_y_i = y == y_i.item()
+            X_i = x[eq_y_i, :]
 
             if sample_weight is not None:
-                sw_i = sample_weight[where_y_i]
+                sw_i = sample_weight[eq_y_i]
                 if 0 not in sw_i.shape:
                     N_i = sw_i.sum().item()
                 else:
@@ -363,7 +363,7 @@ class GaussianNB(ht.ClassificationMixin, ht.BaseEstimator):
                 N_i = X_i.shape[0]
 
             new_theta, new_sigma = self.__update_mean_variance(
-                self.class_count_.larray[:, i].item(),
+                self.class_count_.larray[i].item(),
                 self.theta_[i, :],
                 self.sigma_[i, :],
                 X_i,
@@ -371,16 +371,14 @@ class GaussianNB(ht.ClassificationMixin, ht.BaseEstimator):
             )
             self.theta_[i, :] = new_theta
             self.sigma_[i, :] = new_sigma
-            self.class_count_.larray[:, i] += N_i
+            self.class_count_.larray[i] += N_i
 
         self.sigma_[:, :] += self.epsilon_
 
         # Update only if no priors are provided
         if self.priors is None:
-            # distributed class_count_: sum along distribution axis
-            self.class_count_ = self.class_count_.sum(axis=0, keepdims=True)
             # Empirical prior, with sample_weight taken into account
-            self.class_prior_ = (self.class_count_ / self.class_count_.sum()).squeeze(0)
+            self.class_prior_ = self.class_count_ / self.class_count_.sum()
 
         return self
 
