12bd457384bcd66000c8cf3642d4ff74c884d1d1
Berkant Palazoglu <berkantpalazoglu03@gmail.com>
2026-08-03 09:45:36 +0200

    - Changed chosen device to the one described by the environmental variable.

--- tests/core/test_sorting.py
+++ tests/core/test_sorting.py
@@ -1,19 +1,18 @@
 import numpy as np
 import torch
 import pytest
-
+import os
 import heat as ht
 
 
 class TestSorting:
-    @pytest.mark.parametrize("device", ["cpu", "gpu"])
+    def __init__(self, *args, **kwargs):
+        self.device = os.environ.get("HEAT_TEST_USE_DEVICE", "cpu")
+
     @pytest.mark.parametrize("split", [None, 0, 1, 2])
     @pytest.mark.parametrize("descending", [None, True, False])
     @pytest.mark.parametrize("axis", [None, 0, 1, 2])
-    def test_sort(self, axis, descending, split, device):
-        if not torch.cuda.is_available() and device == "gpu":
-            pytest.skip("No gpu available for testing.")
-
+    def test_sort(self, axis, descending, split):
         kwargs = {"axis": axis}
 
         if np.lib.NumpyVersion(np.__version__) < np.lib.NumpyVersion('2.5.0'):
@@ -21,7 +20,7 @@ class TestSorting:
         else:
             kwargs["descending"] = descending
 
-        data = ht.random.rand(2, 3, 4, split=split)
+        data = ht.random.rand(2, 3, 4, split=split, device=self.device)
         result, idx = ht.sort(data, return_sort_indices=True, **kwargs)
         exp = np.sort(data.numpy(), **kwargs)
         exp_idx = np.argsort(data.numpy(), **kwargs)
@@ -29,14 +28,10 @@ class TestSorting:
         assert np.allclose(result.numpy(), exp)
         assert np.allclose(idx.numpy(), exp_idx)
 
-    @pytest.mark.parametrize("device", ["cpu", "gpu"])
     @pytest.mark.parametrize("split", [None, 0, 1, 2])
     @pytest.mark.parametrize("descending", [None, True, False])
     @pytest.mark.parametrize("axis", [None, 0, 1, 2])
-    def test_argsort_random(self, axis, descending, split, device):
-        if not torch.cuda.is_available() and device == "gpu":
-            pytest.skip("No gpu available for testing.")
-
+    def test_argsort_random(self, axis, descending, split):
         kwargs = {"axis": axis}
 
         if np.lib.NumpyVersion(np.__version__) < np.lib.NumpyVersion('2.5.0'):
@@ -44,16 +39,12 @@ class TestSorting:
         else:
             kwargs["descending"] = descending
 
-        data = ht.random.rand(2, 3, 4, split=split, device=device)
+        data = ht.random.rand(2, 3, 4, split=split, device=self.device)
         result_indices = ht.argsort(data, **kwargs)
         exp_indices = np.argsort(data.numpy(), **kwargs)
         assert np.allclose(result_indices.numpy(), exp_indices)
 
-    @pytest.mark.parametrize("device", ["cpu", "gpu"])
-    def test_argsort_specific(self, device):
-        if not torch.cuda.is_available() and device == "gpu":
-            pytest.skip("No gpu available for testing.")
-
+    def test_argsort_specific(self):
         size = ht.MPI_WORLD.size
         rank = ht.MPI_WORLD.rank
 
@@ -65,17 +56,17 @@ class TestSorting:
                 [[8, 4, 7], [0, 8, 9]],
             ],
             dtype=torch.int32,
-            device=device,
+            device=self.device,
         )
 
         data = ht.array(tensor, split=0)
         if torch.cuda.is_available() and data.device == ht.gpu and size < 4:
             indices_axis_zero = torch.tensor(
-                [[0, 2, 2], [3, 2, 0]], dtype=torch.int32, device=device
+                [[0, 2, 2], [3, 2, 0]], dtype=torch.int32, device=self.device
             )
         else:
             indices_axis_zero = torch.tensor(
-                [[0, 2, 2], [3, 0, 0]], dtype=torch.int32, device=device
+                [[0, 2, 2], [3, 0, 0]], dtype=torch.int32, device=self.device
             )
         result_indices = ht.argsort(data, axis=0)
         first_indices = result_indices[0].larray
@@ -84,7 +75,7 @@ class TestSorting:
 
         data = ht.array(tensor, split=1)
         indices_axis_one = torch.tensor(
-            [[0, 1, 1]], dtype=torch.int32, device=device
+            [[0, 1, 1]], dtype=torch.int32, device=self.device
         )
         result_indices = ht.argsort(data, axis=1)
         first_indices = result_indices[0].larray[:1]
@@ -93,7 +84,7 @@ class TestSorting:
 
         data = ht.array(tensor, split=2)
         indices_axis_two = torch.tensor(
-            [[0], [1]], dtype=torch.int32, device=device
+            [[0], [1]], dtype=torch.int32, device=self.device
         )
         result_indices = ht.argsort(data, axis=2)
         first_indices = result_indices[0].larray[:, :1]
@@ -108,7 +99,7 @@ class TestSorting:
 
         rank = ht.MPI_WORLD.rank
         ht.random.seed(1)
-        data = ht.random.randn(100, 1, split=0, device=device)
+        data = ht.random.randn(100, 1, split=0, device=self.device)
         indices = ht.argsort(data, axis=0)
         result = ht.resplit(data, None)[indices]
         arr = ht.resplit(result.flatten(), axis=None)
