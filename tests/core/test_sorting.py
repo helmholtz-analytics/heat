91802fd173fa7235cf11143d64d342b8d15a4efe
Berkant Palazoglu <berkantpalazoglu03@gmail.com>
2026-08-04 09:32:09 +0200

    removed specific test for `argsort`

--- tests/core/test_sorting.py
+++ tests/core/test_sorting.py
@@ -17,7 +17,7 @@ class TestSorting:
         kwargs = {"axis": axis}
 
         if descending in [True, False]:
-            if np.lib.NumpyVersion(np.__version__) < np.lib.NumpyVersion('2.5.0'):
+            if np.lib.NumpyVersion(np.__version__) < np.lib.NumpyVersion("2.5.0"):
                 pytest.skip(f"NumPy {np.__version__} does not support the 'descending' keyword.")
             else:
                 kwargs["descending"] = descending
@@ -37,7 +37,7 @@ class TestSorting:
         kwargs = {"axis": axis}
 
         if descending in [True, False]:
-            if np.lib.NumpyVersion(np.__version__) < np.lib.NumpyVersion('2.5.0'):
+            if np.lib.NumpyVersion(np.__version__) < np.lib.NumpyVersion("2.5.0"):
                 pytest.skip(f"NumPy {np.__version__} does not support the 'descending' keyword.")
             else:
                 kwargs["descending"] = descending
@@ -46,65 +46,3 @@ class TestSorting:
         result_indices = ht.argsort(data, **kwargs)
         exp_indices = np.argsort(data.numpy(), **kwargs)
         assert np.allclose(result_indices.numpy(), exp_indices)
-
-    def test_argsort_specific(self):
-        size = ht.MPI_WORLD.size
-        rank = ht.MPI_WORLD.rank
-
-        tensor = torch.tensor(
-            [
-                [[2, 8, 5], [7, 2, 3]],
-                [[6, 5, 2], [1, 8, 7]],
-                [[9, 3, 0], [1, 2, 4]],
-                [[8, 4, 7], [0, 8, 9]],
-            ],
-            dtype=torch.int32,
-            device=ht.get_device().torch_device,
-        )
-
-        data = ht.array(tensor, split=0)
-        if torch.cuda.is_available() and data.device == ht.gpu and size < 4:
-            indices_axis_zero = torch.tensor(
-                [[0, 2, 2], [3, 2, 0]], dtype=torch.int32, device=ht.get_device().torch_device
-            )
-        else:
-            indices_axis_zero = torch.tensor(
-                [[0, 2, 2], [3, 0, 0]], dtype=torch.int32, device=ht.get_device().torch_device
-            )
-        result_indices = ht.argsort(data, axis=0)
-        first_indices = result_indices[0].larray
-        if rank == 0:
-            assert torch.equal(first_indices, indices_axis_zero)
-
-        data = ht.array(tensor, split=1)
-        indices_axis_one = torch.tensor(
-            [[0, 1, 1]], dtype=torch.int32, device=ht.get_device().torch_device
-        )
-        result_indices = ht.argsort(data, axis=1)
-        first_indices = result_indices[0].larray[:1]
-        if rank == 0:
-            assert torch.equal(first_indices, indices_axis_one)
-
-        data = ht.array(tensor, split=2)
-        indices_axis_two = torch.tensor(
-            [[0], [1]], dtype=torch.int32, device=ht.get_device().torch_device
-        )
-        result_indices = ht.argsort(data, axis=2)
-        first_indices = result_indices[0].larray[:, :1]
-        if rank == 0:
-            assert torch.equal(first_indices, indices_axis_two)
-
-        # test exceptions
-        with pytest.raises(ValueError):
-            ht.argsort(data, axis=3)
-        with pytest.raises(TypeError):
-            ht.argsort(data, axis="1")
-
-        rank = ht.MPI_WORLD.rank
-        ht.random.seed(1)
-        data = ht.random.randn(100, 1, split=0)
-        indices = ht.argsort(data, axis=0)
-        result = ht.resplit(data, None)[indices]
-        arr = ht.resplit(result.flatten(), axis=None)
-
-        assert (arr.larray[:-1] <= arr.larray[1:]).all()
