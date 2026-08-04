3eef2743743f5a34c6bd56365c67bc209bf31a1d
Berkant Palazoglu <berkantpalazoglu03@gmail.com>
2026-08-04 14:01:00 +0200

    Added info for one dimensional data.

--- heat/core/manipulations.py
+++ heat/core/manipulations.py
@@ -2917,6 +2917,8 @@ def vectorized_sort(
     columns first, which acts as the primary sort key, with subsequent
     columns acting as secondary, tertiary, etc., keys.
 
+    Behaves like `sort` for one dimensional data.
+
     Parameters
     ----------
     a : DNDarray
