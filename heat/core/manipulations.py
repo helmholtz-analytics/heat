91fe22e9e1a725b89be36e3c184be466af030b29
Berkant <51971304+Berkant03@users.noreply.github.com>
2026-09-08 17:37:15 +0200

    Update heat/core/manipulations.py
    Co-authored-by: Thomas Saupe <39156931+brownbaerchen@users.noreply.github.com>

--- heat/core/manipulations.py
+++ heat/core/manipulations.py
@@ -3216,6 +3216,8 @@ def reorder(
         raise ValueError(f"'axis' must be integer, not {type(axis)}.")
     if not (-a.ndim <= axis < a.ndim):
         raise ValueError(f"{axis=} does not exist for array with {a.ndim} dimensions.")
+    if not indices.shape == (a.gshape[axis],):
+        raise ValueError(f'indices of shape {indices.shape} cannot be used to index array of shape {a.shape} along axis {axis}')
 
     if axis < 0:
         axis += a.ndim
