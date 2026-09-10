b6ca8ec8e6964a637a35c390b5501a036855dc89
Berkant Palazoglu <b.palazoglu@fz-juelich.de>
2026-09-10 14:23:32 +0200

    fix test case for 1 process

--- tests/core/test_sorting.py
+++ tests/core/test_sorting.py
@@ -198,6 +198,6 @@ class TestSorting:
         assert a.device == res.device
 
         if axis is None:
-            assert res.split == (0 if res.is_distributed() else None)
+            assert res.split == (0 if a.split is not None else None)
         else:
             assert res.split == a.split
